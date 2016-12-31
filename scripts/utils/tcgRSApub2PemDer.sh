58a6fbf96184cb9a5fb18901d29747e45611d4e9
Imran Desai <imran.desai@intel.com>
2016-12-31 12:37:12 -0700

    Fixing a typo in the script

--- scripts/utils/tcgRSApub2PemDer.sh
+++ scripts/utils/tcgRSApub2PemDer.sh
@@ -35,6 +35,6 @@ echo 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA' | base64 -d > header.bin
 echo '02 03' | xxd -r -p >mid-header.bin
 echo '01 00 01' | xxd -r -p >exponent.bin
 dd if=$1 of=modulus.bin bs=1 count=256 skip=102
-cat header.bin modulus.bin mid-header.bin exponent.bin > $1.der
+cat header.bin modulus.bin mid-header.bin exponent.bin > $1.cer
 openssl rsa -in $1.cer -inform DER -pubin > $1.pem
 rm header.bin modulus.bin mid-header.bin exponent.bin
