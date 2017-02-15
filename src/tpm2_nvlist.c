78202cebc030ef3d5a8e83633adc9cbca37a1281
William Roberts <william.c.roberts@intel.com>
2017-02-15 09:52:41 -0800

    tpm2_nvlist: pprt to common infrastructure
    Also align with coding standard.
    
    Fixes: #156
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- src/tpm2_nvlist.c
+++ src/tpm2_nvlist.c
@@ -29,149 +29,82 @@
 // THE POSSIBILITY OF SUCH DAMAGE.
 //**********************************************************************;
 
-#include <stdarg.h>
-
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <limits.h>
-#include <ctype.h>
-#include <getopt.h>
 
 #include <sapi/tpm20.h>
-#include <tcti/tcti_socket.h>
 #include "changeEndian.h"
-#include "common.h"
 
-int debugLevel = 0;
+#include "log.h"
+#include "main.h"
+#include "options.h"
 
-int NVReadPublic(TPMI_RH_NV_INDEX nvIndex)
-{
-    UINT32 rval;
-    TPM2B_NAME nvName = { { sizeof(TPM2B_NAME)-2, } };
-    TPM2B_NV_PUBLIC nvPublic = { { 0, } };
+static bool nv_read_public(TSS2_SYS_CONTEXT *sapi_context,
+        TPMI_RH_NV_INDEX nv_index) {
 
-    rval = Tss2_Sys_NV_ReadPublic( sysContext, nvIndex, 0, &nvPublic, &nvName, 0 );
-    if(rval != TPM_RC_SUCCESS)
-    {
-        printf("\nNVReadPublic Failed ! ErrorCode: 0x%0x\n\n", rval);
-        return -1;
+    TPM2B_NAME nv_name = {
+            { sizeof(TPM2B_NAME)-2, }
+    };
+
+    TPM2B_NV_PUBLIC nv_public = {
+            { 0, }
+    };
+
+    TPM_RC rval = Tss2_Sys_NV_ReadPublic(sapi_context, nv_index, 0, &nv_public,
+            &nv_name, 0);
+    if (rval != TPM_RC_SUCCESS) {
+        LOG_ERR("NVReadPublic Failed ! ErrorCode: 0x%0x\n", rval);
+        return false;
     }
 
     printf("  {\n");
-    printf("\tHash algorithm(nameAlg):%d\n ", nvPublic.t.nvPublic.nameAlg);
-    printf("\tThe Index attributes(attributes):0x%x\n ", nvPublic.t.nvPublic.attributes.val);
-    printf("\tThe size of the data area(dataSize):%d\n ", nvPublic.t.nvPublic.dataSize);
+    printf("\tHash algorithm(nameAlg):%d\n ", nv_public.t.nvPublic.nameAlg);
+    printf("\tThe Index attributes(attributes):0x%x\n ",
+            nv_public.t.nvPublic.attributes.val);
+    printf("\tThe size of the data area(dataSize):%d\n ",
+            nv_public.t.nvPublic.dataSize);
     printf("  }\n");
 
-    return 0;
+    return true;
 }
 
-int nvList()
-{
+static bool nv_list(TSS2_SYS_CONTEXT *sapi_context) {
+
     TPMI_YES_NO moreData;
     TPMS_CAPABILITY_DATA capabilityData;
-    UINT32 rval;
-
-    rval = Tss2_Sys_GetCapability( sysContext, 0, TPM_CAP_HANDLES, CHANGE_ENDIAN_DWORD(TPM_HT_NV_INDEX),
-                                   TPM_PT_NV_INDEX_MAX, &moreData, &capabilityData, 0 );
-    if(rval != TPM_RC_SUCCESS)
-    {
-        printf("\n......GetCapability:Get NV Index list Error. TPM Error:0x%x......\n", rval);
-        return -1;
+    TPM_RC rval = Tss2_Sys_GetCapability(sapi_context, 0, TPM_CAP_HANDLES,
+            CHANGE_ENDIAN_DWORD(TPM_HT_NV_INDEX),
+            TPM_PT_NV_INDEX_MAX, &moreData, &capabilityData, 0);
+    if (rval != TPM_RC_SUCCESS) {
+        LOG_ERR("GetCapability:Get NV Index list Error. TPM Error:0x%x", rval);
+        return false;
     }
 
-    printf( "%d NV indexes defined.\n", capabilityData.data.handles.count);
+    printf("%d NV indexes defined.\n", capabilityData.data.handles.count);
+
     UINT32 i;
-    for( i=0; i < capabilityData.data.handles.count; i++ )
-    {
-        printf("\n  %d. NV Index: 0x%x\n", i, capabilityData.data.handles.handle[i]);
-        if(NVReadPublic(capabilityData.data.handles.handle[i]))
-            return -2;
+    for (i = 0; i < capabilityData.data.handles.count; i++) {
+        printf("\n  %d. NV Index: 0x%x\n", i,
+                capabilityData.data.handles.handle[i]);
+        bool result = nv_read_public(sapi_context,
+                capabilityData.data.handles.handle[i]);
+        if (!result) {
+            return false;
+        }
     }
     printf("\n");
 
-    return 0;
+    return true;
 }
 
-void showHelp(const char *name)
-{
-    printf("Usage: %s [-h/--help]\n"
-           "   or: %s [-v/--version]\n"
-           "   or: %s\n"
-           "   or: %s [-p/--port <port>] [-d/--dbg <dbgLevel>]\n"
-           "\nwhere:\n\n"
-           "   -h/--help                       display this help and exit.\n"
-           "   -v/--version                    display version information and exit.\n"
-           "   -p/--port <port>                specifies the port number, default %d, optional\n"
-           "   -d/--dbg <dbgLevel>             specifies level of debug messages, optional:\n"
-           "                                     0 (high level test results)\n"
-           "                                     1 (test app send/receive byte streams)\n"
-           "                                     2 (resource manager send/receive byte streams)\n"
-           "                                     3 (resource manager tables)\n"
-           , name, name, name, name, DEFAULT_RESMGR_TPM_PORT);
-}
+int execute_tool(int argc, char *argv[], char *envp[], common_opts_t *opts,
+        TSS2_SYS_CONTEXT *sapi_context) {
 
-int main(int argc, char *argv[])
-{
-    char hostName[200] = DEFAULT_HOSTNAME;
-    int port = DEFAULT_RESMGR_TPM_PORT;
-    int opt = -1;
-    int returnVal = 0;
+    (void) argc;
+    (void) argv;
+    (void) envp;
+    (void) opts;
 
-    struct option sOptions[] =
-    {
-        { "port"     , required_argument, NULL, 'p' },
-        { "dbg"      , required_argument, NULL, 'd' },
-        { "help"     , no_argument,       NULL, 'h' },
-        { "version"  , no_argument,       NULL, 'v' },
-        { 0          , 0,                    0,  0  },
-    };
-
-    if( argc > (int)(2*sizeof(sOptions)/sizeof(struct option)) )
-    {
-        showArgMismatch(argv[0]);
-        return -1;
-    }
-
-    while( ( opt = getopt_long(argc, argv, "p:d:hv", sOptions, NULL) ) != -1 )
-    {
-        switch(opt)
-        {
-        case 'h':
-        case '?':
-            showHelp(argv[0]);
-            return 0;
-        case 'v':
-            showVersion(argv[0]);
-            return 0;
-        case 'p':
-            if( getPort(optarg, &port) )
-            {
-                printf("Incorrect port number.\n");
-                return -2;
-            }
-            break;
-        case 'd':
-            if( getDebugLevel(optarg, &debugLevel) )
-            {
-                printf("Incorrect debug level.\n");
-                return -3;
-            }
-            break;
-        default:
-            break;
-        }
-    };
-
-    prepareTest(hostName, port, debugLevel);
-
-    returnVal = nvList();
-
-    finishTest();
-
-    if(returnVal)
-        return -4;
-
-    return 0;
+    return nv_list(sapi_context) != 0;
 }
