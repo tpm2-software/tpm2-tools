99e3329ff96208fb78a35edbd910859f8aaf81bb
William Roberts <william.c.roberts@intel.com>
2017-03-06 12:54:43 -0800

    Revert "Adding tool for creating simple assertion policies"
    This reverts commit 7ae80404e3197a97e98ed642961895df4ae46145.

--- lib/tpm_hash.h
+++ lib/tpm_hash.h
@@ -3,13 +3,7 @@
 
 #include <sapi/tpm20.h>
 
-UINT32 tpm_hash(TSS2_SYS_CONTEXT *sapi_context, TPMI_ALG_HASH hashAlg,
-	UINT16 size, BYTE *data, TPM2B_DIGEST *result);
-
-int tpm_hash_compute_data(TSS2_SYS_CONTEXT *sapi_context, BYTE *buffer,
-	UINT16 length, TPMI_ALG_HASH halg, TPM2B_DIGEST *result);
-
-UINT32 tpm_hash_sequence(TSS2_SYS_CONTEXT *sapi_context, TPMI_ALG_HASH hash_alg,
-	size_t num_buffers, TPM2B_DIGEST *buffer_list, TPM2B_DIGEST *result);
+UINT32 tpm_hash(TSS2_SYS_CONTEXT *sapi_context, TPMI_ALG_HASH hashAlg, UINT16 size, BYTE *data, TPM2B_DIGEST *result);
+int tpm_hash_compute_data(TSS2_SYS_CONTEXT *sapi_context, BYTE *buffer, UINT16 length, TPMI_ALG_HASH halg, TPM2B_DIGEST *result);
 
 #endif /* SRC_TPM_HASH_H_ */
