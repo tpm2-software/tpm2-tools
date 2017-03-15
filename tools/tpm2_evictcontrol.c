31581c9390576f47e51d47bf73cf654d54847f2a
Alex Devries <alex.devries@windriver.com>
2017-03-15 13:35:58 -0700

    tpm2_evictcontrol: fix bug in password handling
    Passing -P to tpm2_evictcontrol exits immediately because of mishandling the
    return value of password_util_copy_password.
    
    Fixes #249
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- tools/tpm2_evictcontrol.c
+++ tools/tpm2_evictcontrol.c
@@ -155,7 +155,7 @@ static bool init(int argc, char *argv[], tpm_evictcontrol_ctx *ctx) {
         case 'P': {
             bool result = password_util_copy_password(optarg, "authenticating",
                     &ctx->session_data.hmac);
-            if (result) {
+            if (!result) {
                 return false;
             }
             flags.P = 1;
