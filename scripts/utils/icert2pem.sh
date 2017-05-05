07314c163013ac2a95151b974df551a9ca49eb70
William Roberts <william.c.roberts@intel.com>
2017-05-05 13:54:40 -0700

    icert2pem.sh: add help and usage information
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- scripts/utils/icert2pem.sh
+++ scripts/utils/icert2pem.sh
@@ -31,7 +31,38 @@
 #;**********************************************************************;
 #!/bin/sh
 
-certBasename=$( echo $(basename $1) | cut -f 1 -d '.')
+usage() {
+cat <<EOF
+Converts an Intel certificate from DER encoding to PEM encoding, writing the
+result to stdout.
+
+Usage: $0 [options] FILE
+
+Options:
+  -h    print this help text.
+EOF
+  exit 0
+}
+
+while getopts ":h" opt; do
+  case $opt in
+    h)
+      usage
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [ "$#" -ne 1 ]; then
+    (>&2 echo "Error: expected 1 certificate file paramater, got: $#")
+    exit 1
+fi
+
 sed 's/-/+/g;s/_/\//g;s/%3D/=/g;s/^{.*certificate":"//g;s/"}$//g;' $1 |
     base64 --decode |
-    openssl x509 -inform DER -out $certBasename.pem -outform PEM
+    openssl x509 -inform DER -outform PEM
+
+exit 0
