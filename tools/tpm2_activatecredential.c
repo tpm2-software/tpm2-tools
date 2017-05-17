8c9d96f18c0983e6656b79dcab88811e38df2cf0
William Roberts <william.c.roberts@intel.com>
2017-05-17 09:12:56 -0700

    tpm2_activatecredential: remove casts to UINT8
    The sessionAttribute bitfield was being casted to a UINT8 to
    initialize to 0. This is britial if the bitfield is ever
    changed to be larger than 8 bits, and a hack in general.
    
    Fix it by initializing everytihng to 0 and removing these.
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- tools/tpm2_activatecredential.c
+++ tools/tpm2_activatecredential.c
@@ -142,8 +142,6 @@ static bool activate_credential_and_output(TSS2_SYS_CONTEXT *sapi_context,
 
     ctx->password.sessionHandle = TPM_RS_PW;
     ctx->endorse_password.sessionHandle = TPM_RS_PW;
-    *((UINT8 *) ((void *) &ctx->password.sessionAttributes)) = 0;
-    *((UINT8 *) ((void *) &ctx->endorse_password.sessionAttributes)) = 0;
 
     TPMS_AUTH_COMMAND *cmd_session_array_password[2] = {
         &ctx->password,
