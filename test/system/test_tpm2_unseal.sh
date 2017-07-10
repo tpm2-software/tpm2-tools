f7256305eafaf96d11e60f0627a71294401e2b9d
Javier Martinez Canillas <javierm@redhat.com>
2017-07-11 01:17:35 +0200

    tpm2_unseal: remove commented out code leftovers
    The test has some code that is commented out that were added on the
    original commit. Remove them since seems to just be leftovers.
    
    Signed-off-by: Javier Martinez Canillas <javierm@redhat.com>

--- test/system/test_tpm2_unseal.sh
+++ test/system/test_tpm2_unseal.sh
@@ -56,27 +56,21 @@ if [ $? != 0 ];then
 echo "createprimary fail, please check the environment or parameters!"
 exit 1
 fi
-#./tpm2_create -g 0x000B -G 0x0008 -o opu9.out -O opr9.out -c context.p9.out -I secret.data
+
 tpm2_create -g $alg_create_obj -G $alg_create_key -o $file_unseal_key_pub -O $file_unseal_key_priv  -I $file_input_data -c $file_primary_key_ctx
 if [ $? != 0 ];then
 echo "create fail, please check the environment or parameters!"
 exit 1
 fi
-#./tpm2_load -c context.p9.out  -u opu9.out -r opr9.out -n name.load9.out -C context_load_out9.out 
+
 tpm2_load -c $file_primary_key_ctx  -u $file_unseal_key_pub  -r $file_unseal_key_priv -n $file_unseal_key_name -C $file_unseal_key_ctx
 if [ $? != 0 ];then
 echo "load fail, please check the environment or parameters!"
 exit 1
 fi
 
-#tpm2_unseal -c context_load_out1.out -o usl.data.out
 tpm2_unseal -c $file_unseal_key_ctx -o $file_unseal_output_data
 if [ $? != 0 ];then
 echo "unseal fail, please check the environment or parameters!"
 exit 1
 fi
-
-###handle test blocked 
-##tpm2_evictcontrol -A p -c $file_unseal_key_ctx  -S 0x81010015 --Fail to evict
-##tpm2_evictcontrol -A o -c context_load_out4  -S 0x81010015 (0x285)
-##tpm2_unseal -H 0x81010015 -o usl_handle
