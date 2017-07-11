e8f03b65ef61182b57af7e78621bacbe97afbc16
Javier Martinez Canillas <javierm@redhat.com>
2017-07-11 02:32:07 +0200

    test_tpm2_unseal: extend test to cover unsealing objects using a PCR policy
    The test currently only tests creating and unsealing objects without a PCR
    policy, extend the test to also cover the case when a PCR policy is used.
    
    This also tests tpm2_create ability to get the data to be sealed from the
    stdin and for the tpm2_unseal tool to write the unsealed data to stdout.
    
    Signed-off-by: Javier Martinez Canillas <javierm@redhat.com>

--- test/system/test_tpm2_unseal.sh
+++ test/system/test_tpm2_unseal.sh
@@ -34,20 +34,29 @@ alg_primary_obj=0x0004
 alg_primary_key=0x0001
 alg_create_obj=0x000B
 alg_create_key=0x0008
+alg_pcr_policy=0x0004
 
+pcr_index=0
+
+obj_attr=0x492 # fixedTPM, fixedParent, adminWithPolicy, noDA
+
+file_pcr_value=pcr.bin
 file_input_data=secret.data
+file_policy=policy.data
 file_primary_key_ctx=context.p_"$alg_primary_obj"_"$alg_primary_key"
 file_unseal_key_pub=opu_"$alg_create_obj"_"$alg_create_key"
 file_unseal_key_priv=opr_"$alg_create_obj"_"$alg_create_key"
 file_unseal_key_ctx=ctx_load_out_"$alg_primary_obj"_"$alg_primary_key"-"$alg_create_obj"_"$alg_create_key"
 file_unseal_key_name=name.load_"$alg_primary_obj"_"$alg_primary_key"-"$alg_create_obj"_"$alg_create_key"
 file_unseal_output_data=usl_"$file_unseal_key_ctx"
-  
+
+secret="12345678"
+
 rm $file_primary_key_ctx $file_unseal_key_pub $file_unseal_key_priv $file_unseal_key_ctx $file_unseal_key_name $file_unseal_output_data -rf
 
 if [ ! -e "$file_input_data" ]   
   then    
-echo "12345678" > $file_input_data
+echo $secret > $file_input_data
 fi 
 
 tpm2_takeownership -c
@@ -77,6 +86,45 @@ fi
 
 cmp -s $file_unseal_output_data $file_input_data
 if [ $? != 0 ]; then
+    echo "unseal fail, output differs from sealed data!"
+    exit 1
+fi
+
+# Test using a PCR policy for auth
+
+rm $file_unseal_key_pub $file_unseal_key_priv $file_unseal_key_name
+
+tpm2_listpcrs -L ${alg_pcr_policy}:${pcr_index} -o $file_pcr_value
+if [ $? != 0 ];then
+    echo "create raw pcr output fail, please check the environment or parameters!"
+    exit 1
+fi
+
+tpm2_createpolicy -P -L ${alg_pcr_policy}:${pcr_index} -F $file_pcr_value -f $file_policy
+if [ $? != 0 ];then
+    echo "create policy fail, please check the environment or parameters!"
+    exit 1
+fi
+
+tpm2_create -g $alg_create_obj -G $alg_create_key -o $file_unseal_key_pub -O $file_unseal_key_priv -I- -c $file_primary_key_ctx -L $file_policy -A $obj_attr <<< $secret
+if [ $? != 0 ];then
+    echo "create object with policy fail, please check the environment or parameters!"
+    exit 1
+fi
+
+tpm2_load -c $file_primary_key_ctx  -u $file_unseal_key_pub  -r $file_unseal_key_priv -n $file_unseal_key_name -C $file_unseal_key_ctx
+if [ $? != 0 ];then
+    echo "load fail, please check the environment or parameters!"
+    exit 1
+fi
+
+unsealed=`tpm2_unseal -c $file_unseal_key_ctx -L ${alg_pcr_policy}:${pcr_index} -F $file_pcr_value`
+if [ $? != 0 ];then
+    echo "unseal fail, please check the environment or parameters!"
+    exit 1
+fi
+
+if [ "$unsealed" != "$secret" ];then
     echo "unseal fail, output differs from sealed data!"
     exit 1
 fi
