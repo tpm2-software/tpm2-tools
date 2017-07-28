b2c32c630501c5ac1559e1651963e5d94b4a4c64
William Roberts <william.c.roberts@intel.com>
2017-07-28 06:58:36 -0700

    tpm2_nvlist: fix byte order in output
    Listing the attribute value, a UINT32, the bytes were not
    converted from big endian (tpm endianess) to host endianess.
    
    Thus, unless you converted them, the attribute value in hex
    was wrong.
    
    Fixes: #383
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- tools/tpm2_nvlist.c
+++ tools/tpm2_nvlist.c
@@ -51,7 +51,7 @@ static void print_nv_public(TPM2B_NV_PUBLIC *nv_public) {
     printf("  {\n");
     printf("\tHash algorithm(nameAlg):%d\n ", nv_public->t.nvPublic.nameAlg);
     printf("\tattributes: %s(0x%x)\n ", attrs,
-            nv_public->t.nvPublic.attributes.val);
+            tpm2_util_ntoh_32(nv_public->t.nvPublic.attributes.val));
     printf("\tThe size of the data area(dataSize):%d\n ",
             nv_public->t.nvPublic.dataSize);
     printf("\tAuthorization Policy for R/W/D: ");
