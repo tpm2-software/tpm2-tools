c24e901bbbcb8cead6c50d82ed3d8f155bbb5671
Javier Martinez Canillas <javierm@redhat.com>
2017-08-01 17:05:30 +0200

    tpm2_certify: fix tool exit code
    The tpm2_certify execute_tool() is wrongly returning a false value on
    error, while the main program takes 0 to be a success value. But also
    the function is expected to return an int a not a bool value, so fix
    both issues.
    
    Signed-off-by: Javier Martinez Canillas <javierm@redhat.com>

--- tools/tpm2_certify.c
+++ tools/tpm2_certify.c
@@ -394,7 +394,7 @@ int execute_tool(int argc, char *argv[], char *envp[], common_opts_t *opts,
 
     bool result = init(argc, argv, &ctx);
     if (!result) {
-        return false;
+        return 1;
     }
 
     return certify_and_save_data(&ctx) != true;
