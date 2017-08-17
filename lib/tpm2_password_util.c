11bd769936164f3508414325090c8cb0d00a523c
William Roberts <william.c.roberts@intel.com>
2017-08-17 08:56:55 -0700

    tpm2_password_util: drop unused routines
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- lib/tpm2_password_util.c
+++ lib/tpm2_password_util.c
@@ -12,8 +12,6 @@
 #define STR_PREFIX "str:"
 #define STR_PREFIX_LEN sizeof(STR_PREFIX) - 1
 
-#define PASSWORD_MAX (sizeof(((TPM2B_DIGEST *)NULL)->t.buffer))
-
 bool tpm2_password_util_from_optarg(const char *password, TPM2B_AUTH *dest) {
 
     bool is_hex = !strncmp(password, HEX_PREFIX, HEX_PREFIX_LEN);
@@ -50,49 +48,5 @@ bool tpm2_password_util_from_optarg(const char *password, TPM2B_AUTH *dest) {
         return false;
     }
 
-    return true;
-}
-
-bool tpm2_password_util_fromhex(TPM2B_AUTH *password, bool is_hex, const char *description,
-        TPM2B_AUTH *auth) {
-
-    if (is_hex) {
-        auth->t.size = sizeof(auth) - 2;
-        /* this routine is safe on overlapping memory areas */
-        if (tpm2_util_hex_to_byte_structure((char *)password->t.buffer, &auth->t.size, auth->t.buffer)
-                != 0) {
-            LOG_ERR("Failed to convert hex format password for %s.",
-                    description);
-            return false;
-        }
-        /*
-         * we only claim sanity on same memory, not overlapping, but well use
-         * memove anyways at the expense of speed.
-         */
-    } else if (password != auth) {
-        memmove(auth, password, sizeof(*auth));
-    }
-    return true;
-}
-
-bool tpm2_password_util_copy_password(const char *password, const char *description, TPM2B_AUTH *dest) {
-
-    if (!password) {
-        LOG_ERR("Please input the %s password!", description);
-        return false;
-    }
-
-    if (!dest || !description) {
-        return false;
-    }
-
-    size_t len = strlen(password);
-    if (len >= PASSWORD_MAX) {
-        LOG_ERR("Over-length password for %s. Got %zu expected less than %zu!", description, len, PASSWORD_MAX);
-        return false;
-    }
-
-    dest->t.size = len;
-    snprintf((char *)dest->t.buffer, PASSWORD_MAX, "%s", password);
     return true;
 }
--- lib/tpm2_password_util.h
+++ lib/tpm2_password_util.h
@@ -4,40 +4,6 @@
 #include <sapi/tpm20.h>
 
 /**
- * Copies a password stored in a TPM2B_AUTH structure, converting from hex if necessary, into
- * another TPM2B_AUTh structure. Source password and auth structures can be the same pointer.
- * @param password
- *  The source password.
- * @param is_hex
- *  True if the password contained in password is hex encoded.
- * @param description
- *  The description of the key used for error reporting.
- * @param auth
- *  The destination auth structure to copy the key into.
- * @return
- *  True on success and False on failure.
- */
-bool tpm2_password_util_fromhex(TPM2B_AUTH *password, bool is_hex, const char *description,
-        TPM2B_AUTH *auth);
-
-/**
- * Copies a C string password into a TPM2B_AUTH structure. It logs an error on failure.
- *
- * Note: Use of a TPM2B_AUTH structure is for proper size allocation reporting and having
- * a size parameter to avoid duplicate strlen() calls.
- *
- * @param password
- *  The C string password to copy.
- * @param description
- *  A description of the password being copied for error reporting purposes.
- * @param dest
- *  The destination TPM2B_AUTH structure.
- * @return
- *  True on success, False on error.
- */
-bool tpm2_password_util_copy_password(const char *password, const char *description, TPM2B_AUTH *dest);
-
-/**
  * Convert a password argument to a valid TPM2B_AUTH structure. Passwords can
  * be specified in two forms: string and hex-string and are identified by a
  * prefix of str: and hex: respectively. No prefix assumes the str form.
