76c1770ee5e0fe57578c3c42a746679641836382
William Roberts <william.c.roberts@intel.com>
2017-08-24 16:48:08 -0700

    test_tpm2_hmac: clean up testing file
    Set up an on error and on exit handler for cleaning up
    test artifacts as well as handling error messages
    automatically.
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- test/system/test_tpm2_hmac.sh
+++ test/system/test_tpm2_hmac.sh
@@ -47,65 +47,52 @@ file_hmac_output=hmac_"$file_hmac_key_ctx"
 
 file_input_data=secret.data
 
-fail()
-{
-	    echo "$1 test fail, please check the environment or parameters!"
-        exit 1
+onerror() {
+    echo "$BASH_COMMAND on line ${BASH_LINENO[0]} failed: $?"
+    exit 1
 }
-Pass()
-{
-	    echo ""$1" pass" >>test_getpubak_pass.log
+trap onerror ERR
+
+cleanup() {
+  rm -f $file_primary_key_ctx $file_hmac_key_pub $file_hmac_key_priv \
+        $file_hmac_key_name $file_hmac_output
+  if [ "$1" == "all" ]; then
+    rm -f $file_hmac_key_ctx $file_input_data
+  fi
 }
 
-rm $file_primary_key_ctx $file_hmac_key_pub $file_hmac_key_priv $file_hmac_key_name $file_hmac_key_ctx  $file_hmac_output  -rf
+onexit() {
+  cleanup "all"
+}
+trap onexit EXIT
 
-if [ ! -e "$file_input_data" ]   
-  then    
+cleanup
+
 echo "12345678" > $file_input_data
-fi 
 
 tpm2_takeownership -c
 
 tpm2_createprimary -A e -g $alg_primary_obj -G $alg_primary_key -C $file_primary_key_ctx
-if [ $? != 0 ];then
-	 fail createprimary 
-fi
+
 tpm2_create -g $alg_create_obj -G $alg_create_key -o $file_hmac_key_pub -O $file_hmac_key_priv  -c $file_primary_key_ctx
-if [ $? != 0 ];then
-	fail create 
-fi
 
 tpm2_load -c $file_primary_key_ctx  -u $file_hmac_key_pub  -r $file_hmac_key_priv -n $file_hmac_key_name -C $file_hmac_key_ctx
-if [ $? != 0 ];then
-	fail load   
-fi
 
-tpm2_hmac  -c $file_hmac_key_ctx  -g $halg -I $file_input_data -o $file_hmac_output 
-if [ $? != 0 ];then
-	fail hmac 
-fi
+tpm2_hmac -c $file_hmac_key_ctx  -g $halg -I $file_input_data -o $file_hmac_output
 
-rm -f $file_hmac_output
+cleanup
 
 # Test large file, ie sequence hmac'ing.
 dd if=/dev/urandom of=$file_input_data bs=2093 count=1 2>/dev/null
 tpm2_hmac  -c $file_hmac_key_ctx  -g $halg -I $file_input_data -o $file_hmac_output
-if [ $? != 0 ];then
-	fail hmac
-fi
 
 ####handle test
 rm -f $file_hmac_output  
-tpm2_evictcontrol -A o -c $file_hmac_key_ctx -S $handle_hmac_key |tee evict.log
-c1="$?"
+
+tpm2_evictcontrol -A o -c $file_hmac_key_ctx -S $handle_hmac_key | tee evict.log
 grep "persistentHandle: "$handle_hmac_key"" evict.log
-c2="$?"
 
-if [ $c1 != 0 ] || [ $c2 != 0  ];then
-     fail evict
-fi
-
+echo "12345678" > $file_input_data
 tpm2_hmac  -k $handle_hmac_key  -g $halg -I $file_input_data -o $file_hmac_output 
-if [ $? != 0 ];then
-	fail hmac 
-fi
+
+exit 0
