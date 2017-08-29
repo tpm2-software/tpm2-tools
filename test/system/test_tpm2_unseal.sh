fe16282acc9182f9e40b743843b1d725be7d9d4d
William Roberts <william.c.roberts@intel.com>
2017-08-28 19:14:40 -0700

    test: fix -I- option to tpm2_create
    The fact that the option parsing worked with -I- was a sideaffect of
    always reallocing the new argv array, and no proper size check.
    
    During this test, the argv from the OS contained 17 entries.
    After handling the common options, it was 18, with a duplicate
    of the optarg from the option before -i-.
    
    There needs to be a space beteen a single option letter and its
    value.
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- test/system/test_tpm2_unseal.sh
+++ test/system/test_tpm2_unseal.sh
@@ -106,7 +106,7 @@ if [ $? != 0 ];then
     exit 1
 fi
 
-tpm2_create -g $alg_create_obj -G $alg_create_key -o $file_unseal_key_pub -O $file_unseal_key_priv -I- -c $file_primary_key_ctx -L $file_policy -A $obj_attr <<< $secret
+tpm2_create -g $alg_create_obj -G $alg_create_key -o $file_unseal_key_pub -O $file_unseal_key_priv -I - -c $file_primary_key_ctx -L $file_policy -A $obj_attr <<< $secret
 if [ $? != 0 ];then
     echo "create object with policy fail, please check the environment or parameters!"
     exit 1
