ebfae477c7f2484c2636018ec0fb7febddbd93bd
Javier Martinez Canillas <javierm@redhat.com>
2017-09-01 13:28:02 +0200

    test: Use relative path for yaml_get.py in test_tpm2_pcrevent.sh
    The test_all.sh script adds the tests directory in the $PATH variable but
    that's not the case when the tests are executed standalone. In this case,
    the test_tpm2_pcrevent.sh doesn't find the yaml_get.py and fails with:
    
    $ ./test_tpm2_pcrevent.sh
    ./test_tpm2_pcrevent.sh: line 74: yaml_get.py: command not found
    old_pcr_value=`yaml_get.py sha1 9 $yaml_out_file` on line 74 failed: 127
    
    Tests are expected to be executed inside the test/system sub-directory,
    so just use a relative path for the yaml_get.py script.
    
    While being there, add a printout to notify that the test was executed
    successfully to be consistent with the other tests that do the same.
    
    Signed-off-by: Javier Martinez Canillas <javierm@redhat.com>

--- test/system/test_tpm2_pcrevent.sh
+++ test/system/test_tpm2_pcrevent.sh
@@ -71,14 +71,14 @@ for l in `cat $hash_out_file`; do
 done;
 
 tpm2_pcrlist -L sha1:9 --format=yaml > $yaml_out_file
-old_pcr_value=`yaml_get.py sha1 9 $yaml_out_file`
+old_pcr_value=`./yaml_get.py sha1 9 $yaml_out_file`
 
 # Verify that extend works, and test large files
 dd if=/dev/urandom of=$hash_in_file count=1 bs=2093 2> /dev/null
 tpm2_pcrevent -Q -i 9 $hash_in_file
 
 tpm2_pcrlist -L sha1:9 --format=yaml > $yaml_out_file
-new_pcr_value=`yaml_get.py sha1 9 $yaml_out_file`
+new_pcr_value=`./yaml_get.py sha1 9 $yaml_out_file`
 
 if [ "$new_pcr_value" == "$old_pcr_value" ]; then
   echo "Expected PCR value to change after pcrevent with index 9."
@@ -110,5 +110,6 @@ if [ $? -eq 0 ]; then
   exit 1;
 fi
 
+echo "tpm2_pcrevent succeed"
 
 exit 0
