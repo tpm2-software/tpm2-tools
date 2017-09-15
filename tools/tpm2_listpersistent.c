c435c56eff27bb938178293f1a9829cee3a623f2
Javier Martinez Canillas <javierm@redhat.com>
2017-09-15 19:15:55 +0200

    tpm2_listpersistent: improve the tool output
    The tool output was far from ideal, it was neither easy for other tools
    to parse nor human readable. So convert to a better format that is also
    aligned with what other tpm2-tools print, for example tpm2_pcrlist.
    
    Old outout:
    
    2 persistent objects defined.
    
      0. Persistent handle: 0x81010000
      {
            Type: 0x1
            Hash algorithm(nameAlg): 0xb
            Attributes: 0x30072
      }
    
      1. Persistent handle: 0x81020000
      {
            Type: 0x23
            Hash algorithm(nameAlg): 0xb
            Attributes: 0x30072
      }
    
    New output:
    
    persistent-handle[0]:0x81010000 key-alg:rsa hash-alg:sha256 object-attr:0x30072
    persistent-handle[1]:0x81020000 key-alg:ecc hash-alg:sha256 object-attr:0x30072
    
    Suggested-by: William Roberts <william.c.roberts@intel.com>
    Signed-off-by: Javier Martinez Canillas <javierm@redhat.com>

--- tools/tpm2_listpersistent.c
+++ tools/tpm2_listpersistent.c
@@ -40,11 +40,14 @@
 
 #include "tpm2_options.h"
 #include "files.h"
+#include "log.h"
+#include "tpm2_alg_util.h"
 #include "tpm2_tool.h"
 #include "tpm2_util.h"
 
 int readPublic(TSS2_SYS_CONTEXT *sapi_context,
-               TPMI_DH_OBJECT    objectHandle)
+               TPMI_DH_OBJECT    objectHandle,
+               int               index)
 {
     UINT32 rval;
     TPMS_AUTH_RESPONSE sessionDataOut;
@@ -62,15 +65,17 @@ int readPublic(TSS2_SYS_CONTEXT *sapi_context,
     rval = Tss2_Sys_ReadPublic(sapi_context, objectHandle, 0, &outPublic, &name, &qualifiedName, &sessionsDataOut);
     if(rval != TPM_RC_SUCCESS)
     {
-        printf("\nTPM2_ReadPublic error: rval = 0x%0x\n\n",rval);
+        LOG_ERR("\nTPM2_ReadPublic error: rval = 0x%0x\n",rval);
         return -1;
     }
 
-    printf("  {\n");
-    printf("\tType: 0x%x\n ", outPublic.t.publicArea.type);
-    printf("\tHash algorithm(nameAlg): 0x%x\n ", outPublic.t.publicArea.nameAlg);
-    printf("\tAttributes: 0x%x\n", outPublic.t.publicArea.objectAttributes.val);
-    printf("  }\n");
+    TPMI_ALG_PUBLIC type = outPublic.t.publicArea.type;
+    TPMI_ALG_HASH nameAlg = outPublic.t.publicArea.nameAlg;
+    UINT32 attrs = outPublic.t.publicArea.objectAttributes.val;
+
+    tpm2_tool_output("persistent-handle[%d]:0x%x key-alg:%s hash-alg:%s object-attr:0x%x\n",
+                     index, objectHandle, tpm2_alg_util_algtostr(type),
+                     tpm2_alg_util_algtostr(nameAlg), attrs);
 
     return 0;
 }
@@ -89,20 +94,16 @@ int tpm2_tool_onrun(TSS2_SYS_CONTEXT *sapi_context, tpm2_option_flags flags) {
                                    &capabilityData, 0 );
     if(rval != TPM_RC_SUCCESS)
     {
-        printf("\n......GetCapability: Get persistent object list Error."
-               " TPM Error:0x%x......\n", rval);
+        LOG_ERR("\n......GetCapability: Get persistent object list Error."
+               " TPM Error:0x%x......", rval);
         return 1;
     }
 
-    printf( "%d persistent objects defined.\n", capabilityData.data.handles.count);
     UINT32 i;
-    for( i=0; i < capabilityData.data.handles.count; i++ )
-    {
-        printf("\n  %d. Persistent handle: 0x%x\n", i, capabilityData.data.handles.handle[i]);
-        if(readPublic(sapi_context, capabilityData.data.handles.handle[i]))
+    for(i = 0; i < capabilityData.data.handles.count; i++) {
+        if(readPublic(sapi_context, capabilityData.data.handles.handle[i], i))
             return 2;
     }
-    printf("\n");
 
     return 0;
 }
