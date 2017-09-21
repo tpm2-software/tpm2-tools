96af03778819a919ccf997aa201d806c5ae89c9c
William Roberts <william.c.roberts@intel.com>
2017-09-21 15:28:08 -0700

    test: fix test_tpm2_evictcontrol
    Update to use trap handler for error reporting and
    cleanup duties.
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- test/system/test_tpm2_evictcontrol.sh
+++ test/system/test_tpm2_evictcontrol.sh
@@ -30,49 +30,31 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
 # THE POSSIBILITY OF SUCH DAMAGE.
 #;**********************************************************************;
-file_primary_key_ctx=context.p_B1
-file_evict_key_pub=opuB1_B8
-file_evict_key_priv=oprB1_B8
-file_evict_key_ctx=context_load_out_B1_B8
-file_evict_key_name=name.load.B1_B8
-  
-persistentHandle=0x81010003
 
+onerror() {
+    echo "$BASH_COMMAND on line ${BASH_LINENO[0]} failed: $?"
+    exit 1
+}
+trap onerror ERR
 
-alg_hash=0x000B
-alg_primary_key=0x0001
-alg_evict_key=0x0008
+cleanup() {
+  rm -f primary.ctx decrypt.ctx key.pub key.priv key.name decrypt.out \
+        encrypt.out secret.dat &>/dev/null
+}
+trap cleanup EXIT
 
+cleanup
 
-rm $file_primary_key_ctx $file_evict_key_pub $file_evict_key_priv $file_evict_key_ctx $file_evict_key_name $file_output_data -rf
+tpm2_takeownership -Q -c
 
-tpm2_takeownership -c
+tpm2_createprimary -Q -A e -g sha256 -G rsa -C primary.ctx
 
-tpm2_createprimary -A e -g $alg_hash -G $alg_primary_key -C $file_primary_key_ctx
-if [ $? != 0 ];then
- echo "createprimary fail, please check the environment or parameters!"
- exit 1
-fi
-tpm2_create -g $alg_hash -G $alg_evict_key -u $file_evict_key_pub -r $file_evict_key_priv  -c $file_primary_key_ctx
-if [ $? != 0 ];then
- echo "create fail, please check the environment or parameters!"
- exit 1
-fi
-tpm2_load -c $file_primary_key_ctx  -u $file_evict_key_pub  -r $file_evict_key_priv -n $file_evict_key_name -C $file_evict_key_ctx
-if [ $? != 0 ];then
- echo "load fail, please check the environment or parameters!"
- exit 1
-fi
+tpm2_create -Q -g sha256 -G keyedhash -u key.pub -r key.priv  -c primary.ctx
 
-tpm2_evictcontrol -A o -c $file_evict_key_ctx  -S $persistentHandle
-if [ $? != 0 ];then
- echo "evictontronl persistentHandle fail, please check the environment or parameters!"
- exit 1
-fi
+tpm2_load -Q -c primary.ctx  -u key.pub  -r key.priv -n key.name -C key.ctx
 
-tpm2_evictcontrol -A o -H $persistentHandle   -S $persistentHandle
-if [ $? != 0 ];then
- echo "evictcontrol release Handle fail, please check the environment or parameters!"
- exit 1
-fi
+tpm2_evictcontrol -Q -A o -c key.ctx  -S 0x81010003
 
+tpm2_evictcontrol -Q -A o -H 0x81010003   -S 0x81010003
+
+exit 0
