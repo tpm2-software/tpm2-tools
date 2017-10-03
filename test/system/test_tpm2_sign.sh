617270c9cb2f12b1f8438afae4fee18a9ff0b106
William Roberts <william.c.roberts@intel.com>
2017-10-03 15:22:29 -0700

    test: tpm2_sign: clean ups
    * trim whitespace issues
    * add trap handlers for cleanup and error detection
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- test/system/test_tpm2_sign.sh
+++ test/system/test_tpm2_sign.sh
@@ -4,30 +4,30 @@
 # Copyright (c) 2016, Intel Corporation
 # All rights reserved.
 #
-# Redistribution and use in source and binary forms, with or without 
+# Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
-# 1. Redistributions of source code must retain the above copyright notice, 
+#
+# 1. Redistributions of source code must retain the above copyright notice,
 # this list of conditions and the following disclaimer.
-# 
-# 2. Redistributions in binary form must reproduce the above copyright notice, 
-# this list of conditions and the following disclaimer in the documentation 
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
 # and/or other materials provided with the distribution.
 #
 # 3. Neither the name of Intel Corporation nor the names of its contributors
 # may be used to endorse or promote products derived from this software without
 # specific prior written permission.
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
 # THE POSSIBILITY OF SUCH DAMAGE.
 #;**********************************************************************;
 
@@ -39,64 +39,46 @@ file_signing_key_name=name.load.B1_B8
 file_input_data=secret.data
 file_output_data=sig.4
 
-  
+
 handle_signing_key=0x81010005
 
 alg_hash=sha256
 alg_primary_key=0x0001
 alg_signing_key=0x0008
 
-fail()
-{
-	    echo "$1 test fail, please check the environment or parameters!"
- exit 1
+onerror() {
+    echo "$BASH_COMMAND on line ${BASH_LINENO[0]} failed: $?"
+    exit 1
 }
-Pass()
-{
-	    echo ""$1" pass" >>test_getpubak_pass.log
+trap onerror ERR
+
+cleanup() {
+    rm -f $file_input_data $file_primary_key_ctx $file_signing_key_pub \
+          $file_signing_key_priv $file_signing_key_ctx $file_signing_key_name \
+          $file_output_data
+
+    tpm2_evictcontrol -Q -Ao -H $handle_signing_key 2>/dev/null || true
 }
+trap cleanup EXIT
 
+cleanup
 
-if [ ! -e "$file_input_data" ]   
-  then    
 echo "12345678" > $file_input_data
-fi 
-
-rm $file_primary_key_ctx $file_signing_key_pub $file_signing_key_priv $file_signing_key_ctx $file_signing_key_name $file_output_data -rf
 
 tpm2_takeownership -c
-tpm2_createprimary -A e -g $alg_hash -G $alg_primary_key -C $file_primary_key_ctx
-if [ $? != 0 ];then
-	 fail createprimary 
-fi
-tpm2_create -g $alg_hash -G $alg_signing_key -u $file_signing_key_pub -r $file_signing_key_priv  -c $file_primary_key_ctx
-if [ $? != 0 ];then
-	fail create 
-fi
-tpm2_load -c $file_primary_key_ctx  -u $file_signing_key_pub  -r $file_signing_key_priv -n $file_signing_key_name -C $file_signing_key_ctx
-if [ $? != 0 ];then
-	fail load   
-fi
 
-tpm2_sign -c $file_signing_key_ctx -g $alg_hash -m $file_input_data -s $file_output_data
-if [ ! -e "$file_output_data" ];then    
- fail sign 
-else
- rm $file_output_data -rf
-fi
+tpm2_createprimary -Q -A e -g $alg_hash -G $alg_primary_key -C $file_primary_key_ctx
 
-tpm2_evictcontrol -A o -c $file_signing_key_ctx -S $handle_signing_key |tee evict.log
-c1="$?"
-grep "persistentHandle: "$handle_signing_key"" evict.log
-c2="$?"
+tpm2_create -Q -g $alg_hash -G $alg_signing_key -u $file_signing_key_pub -r $file_signing_key_priv  -c $file_primary_key_ctx
 
-if [ $c1 != 0 ] || [ $c2 != 0  ];then
-     fail evictcontrol 
-fi
+tpm2_load -Q -c $file_primary_key_ctx  -u $file_signing_key_pub  -r $file_signing_key_priv -n $file_signing_key_name -C $file_signing_key_ctx
 
-tpm2_sign -k $handle_signing_key -g $alg_hash -m $file_input_data -s $file_output_data
+tpm2_sign -Q -c $file_signing_key_ctx -g $alg_hash -m $file_input_data -s $file_output_data
 
-if [ $? != 0 ];then
-    fail sign
-fi
+rm -f $file_output_data
 
+tpm2_evictcontrol -Q -A o -c $file_signing_key_ctx -S $handle_signing_key
+
+tpm2_sign -Q -k $handle_signing_key -g $alg_hash -m $file_input_data -s $file_output_data
+
+exit 0
