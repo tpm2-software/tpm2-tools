752e4cecf1e72b7c714019c186539d3004761e2e
William Roberts <william.c.roberts@intel.com>
2017-10-03 15:14:15 -0700

    test: tpm2_quote: clean ups
    * trim whitespace issues
    * add trap handlers for cleanup and error detection
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- test/system/test_tpm2_quote.sh
+++ test/system/test_tpm2_quote.sh
@@ -4,30 +4,30 @@
 # Copyright (c) 2016, Intel Corporation
 # All rights reserved.
 #
-# Redistribution and use in source and binary forms, with or without 
+# Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
-# 1. Redistributions of source code must retain the above copyright notice, 
+#
+# 1. Redistributions of source code must retain the above copyright notice,
 # this list of conditions and the following disclaimer.
-# 
-# 2. Redistributions in binary form must reproduce the above copyright notice, 
-# this list of conditions and the following disclaimer in the documentation 
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
 # and/or other materials provided with the distribution.
 #
 # 3. Neither the name of Intel Corporation nor the names of its contributors
 # may be used to endorse or promote products derived from this software without
 # specific prior written permission.
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
 # THE POSSIBILITY OF SUCH DAMAGE.
 #;**********************************************************************;
 alg_primary_obj=sha256
@@ -47,89 +47,62 @@ file_quote_output=quote_"$file_quote_key_ctx"
 
 Handle_ak_quote=0x81010016
 Handle_ek_quote=0x81010017
+Handle_ak_quote2=0x81010018
 
 maxdigest=$(tpm2_dump_capability -c properties-fixed | grep TPM_PT_MAX_DIGEST | sed -r -e 's/.*(0x[0-9a-f]+)/\1/g')
 nonce=12345abcde12345abcde12345abcde12345abcde12345abcde12345abcde12345abcde12345abcde12345abcde12345abcde
 nonce=${nonce:0:2*$maxdigest}
 
-fail()
-{
-	    echo "$1 test fail, please check the environment or parameters!"
-#			    echo ""$1" fail" >>test_encryptdecrypt_error.log
- exit 1
+onerror() {
+    echo "$BASH_COMMAND on line ${BASH_LINENO[0]} failed: $?"
+    exit 1
 }
-Pass()
-{
-	    echo ""$1" pass" >>test_getpubak_pass.log
+trap onerror ERR
+
+cleanup() {
+    rm -f $file_primary_key_ctx $file_quote_key_pub $file_quote_key_priv \
+    $file_quote_key_name $file_quote_key_ctx $file_quote_output \
+    quote_handle_output_"$Handle_ak_quote" ek.pub2 ak.pub2 ak.name_2 \
+    quote_handle_output_"$Handle_ak_quote2"
+
+    tpm2_evictcontrol -Q -Ao -H $Handle_ek_quote 2>/dev/null || true
+    tpm2_evictcontrol -Q -Ao -H $Handle_ak_quote 2>/dev/null || true
+    tpm2_evictcontrol -Q -Ao -H $Handle_ak_quote2 2>/dev/null || true
 }
+trap cleanup EXIT
 
-rm $file_primary_key_ctx $file_quote_key_pub $file_quote_key_priv $file_quote_key_name $file_quote_key_ctx  $file_quote_output  -rf
-
+cleanup
 
 tpm2_takeownership -c
 
-tpm2_createprimary -A e -g $alg_primary_obj -G $alg_primary_key -C $file_primary_key_ctx
-if [ $? != 0 ];then
-	 fail createprimary 
-	 exit 1
-fi
-tpm2_create -g $alg_create_obj -G $alg_create_key -u $file_quote_key_pub -r $file_quote_key_priv  -c $file_primary_key_ctx
-if [ $? != 0 ];then
-	fail create 
-	exit 1
-fi
+tpm2_createprimary -Q -A e -g $alg_primary_obj -G $alg_primary_key -C $file_primary_key_ctx
 
-tpm2_load -c $file_primary_key_ctx  -u $file_quote_key_pub  -r $file_quote_key_priv -n $file_quote_key_name -C $file_quote_key_ctx
-if [ $? != 0 ];then
-	fail load   
-fi
+tpm2_create -Q -g $alg_create_obj -G $alg_create_key -u $file_quote_key_pub -r $file_quote_key_priv  -c $file_primary_key_ctx
 
-tpm2_quote -c $file_quote_key_ctx  -g $alg_quote -l 16,17,18 -o $file_quote_output -q $nonce
-if [ $? != 0 ];then
-	fail quote 
-fi
+tpm2_load -Q -c $file_primary_key_ctx  -u $file_quote_key_pub  -r $file_quote_key_priv -n $file_quote_key_name -C $file_quote_key_ctx
 
-rm $file_quote_output -rf
-tpm2_quote -c $file_quote_key_ctx  -L $alg_quote:16,17,18+$alg_quote1:16,17,18 -o $file_quote_output -q $nonce
-if [ $? != 0 ];then
-	fail quote 
-fi
+tpm2_quote -Q -c $file_quote_key_ctx  -g $alg_quote -l 16,17,18 -o $file_quote_output -q $nonce
+
+rm -f $file_quote_output
+
+tpm2_quote -Q -c $file_quote_key_ctx  -L $alg_quote:16,17,18+$alg_quote1:16,17,18 -o $file_quote_output -q $nonce
 
 #####handle testing
-tpm2_evictcontrol -A o -c $file_quote_key_ctx  -S $Handle_ak_quote
-if [ $? != 0 ];then
-	fail evict 
-fi
- 
-rm quote_handle_output_"$Handle_ak_quote" -rf
-tpm2_quote -k $Handle_ak_quote  -g $alg_quote -l 16,17,18 -o quote_handle_output_"$Handle_ak_quote" -q $nonce
-if [ $? != 0 ];then
-	fail quote_handle 
-fi
+tpm2_evictcontrol -Q -A o -c $file_quote_key_ctx -S $Handle_ak_quote
 
-rm quote_handle_output_"$Handle_ak_quote" -rf
-tpm2_quote -k $Handle_ak_quote  -L $alg_quote:16,17,18+$alg_quote1:16,17,18 -o quote_handle_output_"$Handle_ak_quote" -q $nonce
-if [ $? != 0 ];then
-	fail quote_handle 
-fi
+rm -f quote_handle_output_"$Handle_ak_quote"
+tpm2_quote -Q -k $Handle_ak_quote  -g $alg_quote -l 16,17,18 -o quote_handle_output_"$Handle_ak_quote" -q $nonce
+
+rm -f quote_handle_output_"$Handle_ak_quote"
+tpm2_quote -Q -k $Handle_ak_quote  -L $alg_quote:16,17,18+$alg_quote1:16,17,18 -o quote_handle_output_"$Handle_ak_quote" -q $nonce
 
 #####AK
-Handle_ak_quote=$(($Handle_ak_quote+0x2))
-val=`echo 'obase=16;'$Handle_ak_quote''|bc`
-Handle_ak_quote=0x"$val"
-tpm2_getpubek  -H  $Handle_ek_quote -g 0x01 -f ek.pub2  
-if [ $? != 0 ];then
-	fail getpubek 
-fi
-tpm2_getpubak  -E  $Handle_ek_quote -k  $Handle_ak_quote -f ak.pub2 -n ak.name_2  
-if [ $? != 0 ];then
-	fail getpubak 
-fi
+tpm2_getpubek -Q -H  $Handle_ek_quote -g 0x01 -f ek.pub2
 
-rm quote_handle_output_"$Handle_ak_quote" -rf
-tpm2_quote -k  $Handle_ak_quote -g $alg_quote -l 16,17,18 -o quote_handle_output_"$Handle_ak_quote" -q $nonce
-if [ $? != 0 ];then
-	fail quote_handle_ak 
-fi
-echo "quote test OK!"
+tpm2_getpubak -Q -E  $Handle_ek_quote -k  $Handle_ak_quote2 -f ak.pub2 -n ak.name_2
 
+rm -f quote_handle_output_"$Handle_ak_quote2"
+
+tpm2_quote -Q -k $Handle_ak_quote -g $alg_quote -l 16,17,18 -o quote_handle_output_"$Handle_ak_quote2" -q $nonce
+
+exit 0
