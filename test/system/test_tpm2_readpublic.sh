305fc5afd9b3db05dda5b5f6ecda19f13bab9cb1
William Roberts <william.c.roberts@intel.com>
2017-10-03 15:19:45 -0700

    test: tpm2_readpublic: clean ups
    * trim whitespace issues
    * add trap handlers for cleanup and error detection
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- test/system/test_tpm2_readpublic.sh
+++ test/system/test_tpm2_readpublic.sh
@@ -4,30 +4,30 @@
 # Copyright (c) 2016, Intel Corporation
 # All rights reserved.
 #
-# Redistribution and use in source and binary forms, with or without 
+# Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
-# 1. Redistributions of source code must retain the above copyright notice, 
+#
+# 1. Redistributions of source code must retain the above copyright notice,
 # this list of conditions and the following disclaimer.
-# 
-# 2. Redistributions in binary form must reproduce the above copyright notice, 
-# this list of conditions and the following disclaimer in the documentation 
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
 # and/or other materials provided with the distribution.
 #
 # 3. Neither the name of Intel Corporation nor the names of its contributors
 # may be used to endorse or promote products derived from this software without
 # specific prior written permission.
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
 # THE POSSIBILITY OF SUCH DAMAGE.
 #;**********************************************************************;
 alg_primary_obj=0x000B
@@ -44,50 +44,35 @@ file_readpub_output=readpub_"$file_readpub_key_ctx"
 
 Handle_readpub=0x81010014
 
-fail()
-{
-	    echo "$1 test fail, please check the environment or parameters!"
-        exit 1
+onerror() {
+    echo "$BASH_COMMAND on line ${BASH_LINENO[0]} failed: $?"
+    exit 1
 }
-Pass()
-{
-	    echo ""$1" pass" >>test_getpubak_pass.log
-}
+trap onerror ERR
 
-rm $file_primary_key_ctx $file_readpub_key_pub $file_readpub_key_priv $file_readpub_key_name $file_readpub_key_ctx  $file_readpub_output  -rf
+cleanup() {
+    rm -f $file_primary_key_ctx $file_readpub_key_pub $file_readpub_key_priv \
+    $file_readpub_key_name $file_readpub_key_ctx $file_readpub_output
+
+    tpm2_evictcontrol -Q -A o -H $Handle_readpub 2>/dev/null || true
+}
+trap cleanup EXIT
+
+cleanup
 
 tpm2_takeownership -c
 
-tpm2_createprimary -A e -g $alg_primary_obj -G $alg_primary_key -C $file_primary_key_ctx
-if [ $? != 0 ];then
-	 fail createprimary 
-fi
-tpm2_create -g $alg_create_obj -G $alg_create_key -u $file_readpub_key_pub -r $file_readpub_key_priv  -c $file_primary_key_ctx
-if [ $? != 0 ];then
-	fail create 
-fi
+tpm2_createprimary -Q -A e -g $alg_primary_obj -G $alg_primary_key -C $file_primary_key_ctx
 
-tpm2_load -c $file_primary_key_ctx  -u $file_readpub_key_pub  -r $file_readpub_key_priv -n $file_readpub_key_name -C $file_readpub_key_ctx
-if [ $? != 0 ];then
-	fail load   
-fi
+tpm2_create -Q -g $alg_create_obj -G $alg_create_key -u $file_readpub_key_pub -r $file_readpub_key_priv  -c $file_primary_key_ctx
 
-tpm2_readpublic -c $file_readpub_key_ctx -o $file_readpub_output
-if [ $? != 0 ];then
-	fail decrypt 
-fi
+tpm2_load -Q -c $file_primary_key_ctx  -u $file_readpub_key_pub  -r $file_readpub_key_priv -n $file_readpub_key_name -C $file_readpub_key_ctx
 
-#####handle testing
-tpm2_evictcontrol -A o -c $file_readpub_key_ctx  -S $Handle_readpub
-if [ $? != 0 ];then
-	fail evict 
-fi
- 
-rm $file_readpub_output -rf
-tpm2_readpublic -H $Handle_readpub -o $file_readpub_output
-if [ $? != 0 ];then
-	fail readpublic_handle 
-fi
+tpm2_readpublic -Q -c $file_readpub_key_ctx -o $file_readpub_output
 
-echo "readpublic test OK!"
+tpm2_evictcontrol -Q -A o -c $file_readpub_key_ctx -S $Handle_readpub
 
+rm -f $file_readpub_output
+tpm2_readpublic -Q -H $Handle_readpub -o $file_readpub_output
+
+exit 0
