59daf73622fd430b9ec846150c07bb3a43c10be8
William Roberts <william.c.roberts@intel.com>
2017-10-03 14:57:11 -0700

    test: tpm2_pcrlist: clean ups
    * trim whitespace issues
    * add trap handlers for cleanup and error detection
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- test/system/test_tpm2_pcrlist.sh
+++ test/system/test_tpm2_pcrlist.sh
@@ -4,62 +4,54 @@
 # Copyright (c) 2016, Intel Corporation
 # All rights reserved.
 #
-# Redistribution and use in source and binary forms, with or without 
+# Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
-# 1. Redistributions of source code must retain the above copyright notice, 
+#
+# 1. Redistributions of source code must retain the above copyright notice,
 # this list of conditions and the following disclaimer.
-# 
-# 2. Redistributions in binary form must reproduce the above copyright notice, 
-# this list of conditions and the following disclaimer in the documentation 
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
 # and/or other materials provided with the distribution.
 #
 # 3. Neither the name of Intel Corporation nor the names of its contributors
 # may be used to endorse or promote products derived from this software without
 # specific prior written permission.
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
 # THE POSSIBILITY OF SUCH DAMAGE.
 #;**********************************************************************;
 
+onerror() {
+    echo "$BASH_COMMAND on line ${BASH_LINENO[0]} failed: $?"
+    exit 1
+}
+trap onerror ERR
 
-tpm2_pcrlist 
+cleanup() {
+    rm -f pcrs.out
+}
+trap cleanup EXIT
 
-if [ $? != 0 ];then 
- echo "pcrlist  fail!"
- exit 1
-fi
+cleanup
 
-tpm2_pcrlist -g 0x04
+tpm2_pcrlist -Q
 
-if [ $? != 0 ];then 
- echo "pcrlist  fail!"
- exit 1
-fi
+tpm2_pcrlist -Q -g 0x04
 
-rm -rf pcrs
+tpm2_pcrlist -Q -L 0x04:17,18,19+sha256:0,17,18,19 -o pcrs.out
 
-tpm2_pcrlist -L 0x04:17,18,19+sha256:0,17,18,19 -o pcrs
+test -e pcrs.out
 
-if [ $? != 0 ];then 
- echo "pcrlist  fail!"
- exit 1
-fi
+tpm2_pcrlist -Q -s
 
-tpm2_pcrlist -s
-
-if [ $? != 0 ];then 
- echo "pcrlist  fail!"
- exit 1
-fi
-
-echo "pcrlist  OK!"
+exit 0
