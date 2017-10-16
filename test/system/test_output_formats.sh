ef878aecbdd41dedc78b87a4832e66c35e8a072d
William Roberts <william.c.roberts@intel.com>
2017-10-16 15:15:19 -0700

    test: cleanup outputs
    test_output_formats.sh was quite chatty when no errors were encountered,
    clean that up.
    
    Fixes: #561
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- test/system/test_output_formats.sh
+++ test/system/test_output_formats.sh
@@ -80,73 +80,46 @@ cleanup() {
 }
 trap cleanup EXIT
 
-info()
-{
-    echo
-    echo -n ">>> "
-    echo $*
-    echo
-}
-
 tpm2_takeownership -c
 
-tpm2_getpubek -g $alg_ek -f "$file_pubek_orig" -H $handle_ek
-info "created $alg_ek EK, TSS format was written to '$file_pubek_orig'"
-
-info "testing tpm2_readpublic output formats"
+tpm2_getpubek -Q -g $alg_ek -f "$file_pubek_orig" -H $handle_ek
 
 for fmt in tss pem der; do
 
     this_key="${file_pubek_base}.${fmt}"
 
-    tpm2_readpublic -H $handle_ek -f "$fmt" -o "$this_key"
-    info "wrote EK in $fmt format to '$this_key'"
+    tpm2_readpublic -Q -H $handle_ek -f "$fmt" -o "$this_key"
 
     if [ "$fmt" = tss ]; then
-        diff "$file_pubek_orig" "$this_key"
-        info "readpublic $fmt format output matches originally created key"
+        diff "$file_pubek_orig" "$this_key" > /dev/null
     else
-        openssl rsa -pubin -inform "$fmt" -text -in "$this_key"
-        info "OpenSSL successfully parsed $fmt key"
+        openssl rsa -pubin -inform "$fmt" -text -in "$this_key" &> /dev/null
     fi
 
 done
 
-tpm2_getpubak -g $alg_ak -E $handle_ek -k $handle_ak -f "$file_pubak_tss" -n "$file_pubak_name"
-info "created $alg_ak AK, TSS format written to '$file_pubak_tss'"
+tpm2_getpubak -Q -g $alg_ak -E $handle_ek -k $handle_ak -f "$file_pubak_tss" -n "$file_pubak_name"
 
-tpm2_readpublic -H $handle_ak -f "pem" -o "$file_pubak_pem"
-info "created PEM formatted AK in '$file_pubak_pem'"
+tpm2_readpublic -Q -H $handle_ak -f "pem" -o "$file_pubak_pem"
 
-info "testing tpm2_sign signature output formats"
-
-tpm2_hash -H e -g $alg_hash -t "$file_hash_ticket" -o "$file_hash_result" "$file_hash_input"
-info "created hash of $file_hash_input in '$file_hash_result'"
+tpm2_hash -Q -H e -g $alg_hash -t "$file_hash_ticket" -o "$file_hash_result" "$file_hash_input"
 
 for fmt in tss plain; do
     this_sig="${file_sig_base}.${fmt}"
-    tpm2_sign -k $handle_ak -g $alg_hash -m "${file_hash_input}" -f $fmt -s "${this_sig}" -t "${file_hash_ticket}"
-    info "created signature in $fmt format in '$this_sig'"
+    tpm2_sign -Q -k $handle_ak -g $alg_hash -m "${file_hash_input}" -f $fmt -s "${this_sig}" -t "${file_hash_ticket}"
 
     if [ "$fmt" = plain ]; then
-        openssl dgst -verify "$file_pubak_pem" -keyform pem -${alg_hash} -signature "$this_sig" "$file_hash_input"
-        info "OpenSSL successfully verified $fmt signature"
+        openssl dgst -verify "$file_pubak_pem" -keyform pem -${alg_hash} -signature "$this_sig" "$file_hash_input" > /dev/null
     fi
 done
-
-info "testing tpm2_quote signature output formats"
 
 for fmt in tss plain; do
     this_sig="${file_quote_sig_base}.${fmt}"
-    tpm2_quote -k $handle_ak -l 0 -g "$alg_hash" -f $fmt -m "$file_quote_msg" -s "$this_sig" -o "$file_quote_combined"
-    info "performed quote with signature format $fmt in '$this_sig'"
+    tpm2_quote -Q -k $handle_ak -l 0 -g "$alg_hash" -f $fmt -m "$file_quote_msg" -s "$this_sig" -o "$file_quote_combined"
 
     if [ "$fmt" = plain ]; then
-        openssl dgst -verify "$file_pubak_pem" -keyform pem -${alg_hash} -signature "$this_sig" "$file_quote_msg"
-        info "OpenSSL successfully verified $fmt quote signature"
+        openssl dgst -verify "$file_pubak_pem" -keyform pem -${alg_hash} -signature "$this_sig" "$file_quote_msg" > /dev/null
     fi
 done
 
-cleanup
-info "output format tests OK!"
-
+exit 0
