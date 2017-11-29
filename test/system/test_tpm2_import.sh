01269b0cc8b2ed86cf828f6224ff123ff34fd8a9
William Roberts <william.c.roberts@intel.com>
2017-11-28 17:57:05 -0800

    tpm2_encryptdecrypt: drop -D YES|NO argument
    tpm2_encryptdecrypt takes a -D argument to indicate to the
    tool to perform a decrypt operation. The option currently
    requires an argument of YES or NO. Remove this requirment
    and make the default operation encrypt and the -D option
    trigger a decryption.
    
    No update to the manpage is required, as this change places it
    in line with the man page.
    
    Fixes: #614
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- test/system/test_tpm2_import.sh
+++ test/system/test_tpm2_import.sh
@@ -61,7 +61,7 @@ tpm2_load -Q -H 0x81010005 -u import_key.pub -r import_key.priv -n import_key.na
 
 echo "plaintext" > "plain.txt"
 
-tpm2_encryptdecrypt -c import_key.ctx -D NO -I plain.txt -o plain.enc
+tpm2_encryptdecrypt -c import_key.ctx  -I plain.txt -o plain.enc
 
 openssl enc -in plain.enc -out plain.dec.ssl -d -K `xxd -p sym.key` -iv 0 \
 -aes-128-cfb
