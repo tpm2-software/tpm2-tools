e811be09d5a9f533780ae473bd9098ce7aa71c18
William Roberts <william.c.roberts@intel.com>
2018-01-08 14:44:00 -0800

    tools: add tpm2_policypcr
    Add a tool that can take an existing session and perform a policyPCR
    event.
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

---
+++ man/tpm2_policypcr.1.md
@@ -0,0 +1,93 @@
+% tpm2_policypcr(1) tpm2-tools | General Commands Manual
+%
+% JANUARY 2018
+
+# NAME
+
+**tpm2_policypcr**(1) - Perform a policyPCR event with the TPM.
+
+# SYNOPSIS
+
+**tpm2_policypcr** [*OPTIONS*]
+
+# DESCRIPTION
+
+**tpm2_policypcr**(1) generates a policy PCR event with the TPM. It is similair
+to **tpm2_createpolicy**(1), however, it expects a session to be already
+established via **tpm2_startauthsession**(1).
+
+# OPTIONS
+
+  * **-f**, **--policy-file**=_POLICY\_FILE_:
+    File to save the policy digest.
+
+  * **-F**, **--pcr-input-file**=_PCR\_FILE_:
+    Optional Path or Name of the file containing expected pcr values for the
+    specified index. Default is to read the current PCRs per the set list.
+
+  * **-L**, **--set-list**=_PCR\_LIST_:
+    The list of pcr banks and selected PCRs' ids (0~23) for each bank.
+
+  * **-S**, **--session**=_SESSION_FILE_:
+
+    The policy session file generated via the **-S** option to
+    **tpm2_startauthsession**(1).
+
+[common options](common/options.md)
+
+[common tcti options](common/tcti.md)
+
+[supported hash algorithms](common/hash.md)
+
+[algorithm specifiers](common/alg.md)
+
+# EXAMPLES
+
+Starts a *trial* session, builds a PCR policy and uses that policy in the creation of an object.
+Then, it uses a *policy* session to unseal some data stored in the object.
+```
+# Step 1: Create a trial session and build a PCR policy via a policyPCR event to generate
+#   a policy hash.
+#
+# Step 2: Create an object and use the policy hash as the policy to satisfy for usage.
+#
+# Step 3: Create an actual policy session and using a policyPCR event, update the session
+#  policy hash.
+#
+# Step 4: Using the actual policy session from step 3 in tpm2_unseal to unseal the object.
+#
+
+tpm2_createprimary -H e -g sha256 -G ecc -C primary.ctx
+
+tpm2_pcrlist -Q -L "sha1:0,1,2,3 -o pcr.dat
+
+handle=`tpm2_startauthsession -S session.dat | cut -d' ' -f 2-2`
+
+tpm2_policypcr -Q -S session.dat -L "sha1:0,1,2,3" -F pcr.dat -f policy.dat
+
+tpm2_flushcontext -H "$handle"
+
+tpm2_create -Q -g sha256 -G keyedhash -u key.pub -r key.priv -c primary.ctx -L policy.dat \
+  -A 'sign|fixedtpm|fixedparent|sensitivedataorigin' -I- <<< "12345678"
+
+tpm2_load -Q -c primary.ctx -u key.pub -r key.priv -n unseal.key.name -C unseal.key.ctx
+
+# Now that an object is created and a policy is required to access it, satisy the policy on
+# a session and use it to unseal the data stored in the object.
+
+handle=`tpm2_startauthsession -a -S session.dat | cut -d' ' -f 2-2`
+
+tpm2_policypcr -Q -S session.dat -L "sha1:0,1,2,3" -F pcr.dat -f policy.dat
+
+unsealed=`tpm2_unseal -S session.dat -c unseal.key.ctx`
+
+echo "$unsealed"
+
+tpm2_flushcontext -H "$handle"
+```
+
+# RETURNS
+
+0 on success or 1 on failure.
+
+[footer](common/footer.md)
---
+++ tools/tpm2_policypcr.c
@@ -0,0 +1,165 @@
+//**********************************************************************;
+// Copyright (c) 2018, Intel Corporation
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+//
+// 3. Neither the name of Intel Corporation nor the names of its contributors
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+// THE POSSIBILITY OF SUCH DAMAGE.
+//**********************************************************************;
+
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <ctype.h>
+
+#include <sapi/tpm20.h>
+
+#include "files.h"
+#include "log.h"
+#include "pcr.h"
+#include "tpm2_options.h"
+#include "tpm2_policy.h"
+#include "tpm2_session.h"
+#include "tpm2_tool.h"
+#include "tpm2_util.h"
+
+typedef struct tpm2_startauthsession_ctx tpm2_startauthsession_ctx;
+struct tpm2_startauthsession_ctx {
+   const char *session_path;
+   const char *raw_pcrs_file;
+   TPML_PCR_SELECTION pcr_selection;
+   const char *policy_out_path;
+};
+
+static tpm2_startauthsession_ctx ctx;
+
+static bool on_option(char key, char *value) {
+
+    switch (key) {
+    case 'f':
+        ctx.policy_out_path = value;
+        break;
+    case 'F':
+        ctx.raw_pcrs_file = value;
+        break;
+    case 'L': {
+        bool result = pcr_parse_selections(value, &ctx.pcr_selection);
+        if (!result) {
+            LOG_ERR("Could not parse PCR selections");
+            return false;
+        }
+    } break;
+    case 'S':
+        ctx.session_path = value;
+    break;
+    }
+    return true;
+}
+
+bool tpm2_tool_onstart(tpm2_options **opts) {
+
+    static struct option topts[] = {
+        { "policy-file",    required_argument,  NULL,   'f' },
+        { "pcr-input-file", required_argument,  NULL,   'F' },
+        { "set-list",       required_argument,  NULL,   'L' },
+        { "session",        required_argument,  NULL,   'S' },
+    };
+
+    *opts = tpm2_options_new("f:F:L:S:", ARRAY_LEN(topts), topts, on_option,
+                             NULL, false);
+
+    return *opts != NULL;
+}
+
+int tpm2_tool_onrun(TSS2_SYS_CONTEXT *sapi_context, tpm2_option_flags flags) {
+
+    UNUSED(flags);
+
+    int rc = 1;
+    tpm2_session *s = NULL;
+
+    bool fail = false;
+    if (!ctx.session_path) {
+        LOG_ERR("Must specify -S session file");
+        fail = true;
+    }
+
+    if (!ctx.pcr_selection.count) {
+        LOG_ERR("Must specify -L pcr selection list");
+        fail = true;
+    }
+
+    if (fail) {
+        return rc;
+    }
+
+    s = tpm2_session_restore(ctx.session_path);
+    if (!s) {
+        return rc;
+    }
+
+
+    bool result = tpm2_policy_build_pcr(sapi_context, s,
+            ctx.raw_pcrs_file,
+            &ctx.pcr_selection);
+    if (!result) {
+        LOG_ERR("Could not build pcr policy");
+        goto out;
+    }
+
+    TPM2B_DIGEST policy_digest = TPM2B_EMPTY_INIT;
+    result = tpm2_policy_get_digest(sapi_context, s,
+            &policy_digest);
+    if (!result) {
+        LOG_ERR("Could not build tpm policy");
+        goto out;
+    }
+
+    tpm2_tool_output("policy_digest: 0x");
+    UINT16 i;
+    for(i = 0; i < policy_digest.size; i++) {
+        tpm2_tool_output("%02X", policy_digest.buffer[i]);
+    }
+    tpm2_tool_output("\n");
+
+    if (ctx.policy_out_path) {
+        result = files_save_bytes_to_file(ctx.policy_out_path,
+                    (UINT8 *) &policy_digest.buffer,
+                    policy_digest.size);
+        if (!result) {
+            LOG_ERR("Failed to save policy digest into file \"%s\"",
+                    ctx.policy_out_path);
+            goto out;
+        }
+    }
+
+    rc = 0;
+
+out:
+    tpm2_session_free(&s);
+    return rc;
+}
