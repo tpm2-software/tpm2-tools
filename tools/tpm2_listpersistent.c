63c390f07481e18eaa02fbd998a5f795c12cb1d2
William Roberts <william.c.roberts@intel.com>
2018-02-02 14:49:12 -0800

    tpm2_listpersistent: clean up output
    listpersist now outputs a YAML compliant list of dictionaries.
    
    For exmaple:
    - { persistent-handle: 0x81000000, key-alg: ecc, hash-alg: sha1, object-attr: fixedtpm|fixedparent|sensitivedataorigin|userwithauth|restricted|decrypt }
    - { persistent-handle: 0x81000001, key-alg: keyedhash, hash-alg: sha256, object-attr: fixedtpm|fixedparent|sensitivedataorigin|userwithauth|restricted|decrypt }
    
    Fixes: #557
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- tools/tpm2_listpersistent.c
+++ tools/tpm2_listpersistent.c
@@ -51,7 +51,6 @@ typedef struct tpm_listpersistent_context tpm_listpersistent_context;
 struct tpm_listpersistent_context {
     TPMI_ALG_HASH nameAlg;
     TPMI_ALG_PUBLIC type;
-    UINT32 count;
 };
 
 static tpm_listpersistent_context ctx = {
@@ -82,49 +81,23 @@ static bool on_option(char key, char *value) {
     return true;
 }
 
-int readPublic(TSS2_SYS_CONTEXT *sapi_context, TPMI_DH_OBJECT objectHandle) {
+static int read_public(TSS2_SYS_CONTEXT *sapi_context,
+        TPMI_DH_OBJECT objectHandle, TPM2B_PUBLIC *outPublic) {
+
     UINT32 rval;
     TSS2L_SYS_AUTH_RESPONSE sessionsDataOut;
 
-    TPM2B_PUBLIC outPublic = TPM2B_EMPTY_INIT;
     TPM2B_NAME name = TPM2B_TYPE_INIT(TPM2B_NAME, name);
     TPM2B_NAME qualifiedName = TPM2B_TYPE_INIT(TPM2B_NAME, name);
 
-    rval = TSS2_RETRY_EXP(Tss2_Sys_ReadPublic(sapi_context, objectHandle, 0, &outPublic, &name, &qualifiedName, &sessionsDataOut));
-    if(rval != TPM2_RC_SUCCESS)
-    {
-        LOG_ERR("\nTPM2_ReadPublic error: rval = 0x%0x\n",rval);
+    rval = TSS2_RETRY_EXP(
+               Tss2_Sys_ReadPublic(sapi_context, objectHandle, NULL, outPublic,
+                       &name, &qualifiedName, &sessionsDataOut)
+           );
+    if (rval != TPM2_RC_SUCCESS) {
+        LOG_ERR("TPM2_ReadPublic error: rval = 0x%X", rval);
         return -1;
     }
-
-    TPMI_ALG_PUBLIC type = outPublic.publicArea.type;
-    TPMI_ALG_HASH nameAlg = outPublic.publicArea.nameAlg;
-    char *attrs = tpm2_attr_util_obj_attrtostr(
-            outPublic.publicArea.objectAttributes);
-    char *attrbuf = attrs;
-
-	/*
-	 * tmp must be declared at this scope for possible use in tpm2_tool_output when attrs
-	 * is null.
-	 */
-    char tmp[11]; /* UINT32 in hex (8) + "0x" + '\0' */
-    if (!attrs) {
-        LOG_WARN("Could not convert objectAttributes, converting to hex output");
-        snprintf(tmp, sizeof(tmp), "0x%x", outPublic.publicArea.objectAttributes);
-        attrbuf = tmp;
-    }
-
-    if ((ctx.type != TPM2_ALG_NULL && ctx.type != type) ||
-        (ctx.nameAlg != TPM2_ALG_NULL && ctx.nameAlg != nameAlg)) {
-        goto out;
-    }
-
-    tpm2_tool_output("persistent-handle[%d]:0x%x key-alg:%s hash-alg:%s object-attr:%s\n",
-                     ctx.count++, objectHandle, tpm2_alg_util_algtostr(type),
-                     tpm2_alg_util_algtostr(nameAlg), attrbuf);
-
-out:
-    free(attrs);
 
     return 0;
 }
@@ -151,20 +124,54 @@ int tpm2_tool_onrun(TSS2_SYS_CONTEXT *sapi_context, tpm2_option_flags flags) {
     UINT32 rval;
 
     UINT32 property = tpm2_util_endian_swap_32(TPM2_HT_PERSISTENT);
-    rval = TSS2_RETRY_EXP(Tss2_Sys_GetCapability(sapi_context, 0, TPM2_CAP_HANDLES,
-                                   property, TPM2_PT_TPM2_HR_PERSISTENT, &moreData,
-                                   &capabilityData, 0));
-    if(rval != TPM2_RC_SUCCESS)
-    {
-        LOG_ERR("\n......GetCapability: Get persistent object list Error."
-               " TPM Error:0x%x......", rval);
+    rval = TSS2_RETRY_EXP(
+                Tss2_Sys_GetCapability(sapi_context, NULL, TPM2_CAP_HANDLES,
+                        property, TPM2_PT_TPM2_HR_PERSISTENT, &moreData,
+                        &capabilityData, NULL));
+    if (rval != TPM2_RC_SUCCESS) {
+        LOG_ERR("GetCapability: Get persistent object list Error."
+                " TPM Error:0x%x......", rval);
         return 1;
     }
 
     UINT32 i;
-    for(i = 0; i < capabilityData.data.handles.count; i++) {
-        if(readPublic(sapi_context, capabilityData.data.handles.handle[i]))
+    for (i = 0; i < capabilityData.data.handles.count; i++) {
+        TPM2B_PUBLIC outPublic = TPM2B_EMPTY_INIT;
+        TPM2_HANDLE objectHandle = capabilityData.data.handles.handle[i];
+        if (read_public(sapi_context, objectHandle, &outPublic)) {
             return 2;
+        }
+
+        TPMI_ALG_PUBLIC type = outPublic.publicArea.type;
+        TPMI_ALG_HASH nameAlg = outPublic.publicArea.nameAlg;
+        if ((ctx.type != TPM2_ALG_NULL && ctx.type != type)
+                || (ctx.nameAlg != TPM2_ALG_NULL && ctx.nameAlg != nameAlg)) {
+            /* Skip, filter me out */
+            continue;
+        }
+
+        char *attrs = tpm2_attr_util_obj_attrtostr(
+                outPublic.publicArea.objectAttributes);
+        char *attrbuf = attrs;
+
+        /*
+         * tmp must be declared at this scope for possible use in tpm2_tool_output when attrs
+         * is null.
+         */
+        char tmp[11]; /* UINT32 in hex (8) + "0x" + '\0' */
+        if (!attrs) {
+            LOG_WARN(
+                    "Could not convert objectAttributes, converting to hex output");
+            snprintf(tmp, sizeof(tmp), "0x%X",
+                    outPublic.publicArea.objectAttributes);
+            attrbuf = tmp;
+        }
+
+        tpm2_tool_output(
+                "- { persistent-handle: 0x%X, key-alg: %s, hash-alg: %s, object-attr: %s }\n",
+                objectHandle, tpm2_alg_util_algtostr(type),
+                tpm2_alg_util_algtostr(nameAlg), attrbuf);
+        free(attrs);
     }
 
     return 0;
