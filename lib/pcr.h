794bfc0c03e26fa88a1666f6265353e0cb1f2745
William Roberts <william.c.roberts@intel.com>
2018-02-09 11:27:30 -0800

    lib/pcr: remove dead code
    get_max_supported_pcrs() was unused and untested, remove.
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- lib/pcr.h
+++ lib/pcr.h
@@ -37,6 +37,5 @@
 
 bool pcr_parse_selections(const char *arg, TPML_PCR_SELECTION *pcrSels);
 bool pcr_parse_list(const char *str, size_t len, TPMS_PCR_SELECTION *pcrSel);
-TSS2_RC get_max_supported_pcrs(TSS2_SYS_CONTEXT *sapi_context, UINT32 *max_pcrs);
 
 #endif /* SRC_PCR_H_ */
