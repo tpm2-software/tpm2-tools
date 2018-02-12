b08dd6738d2d98706e68e31da14be7d4df195bd6
William Roberts <william.c.roberts@intel.com>
2018-02-12 10:32:27 -0800

    tpm2_rc_decode: request no sapi-init
    tpm2_rc_decode doesn't need a sapi context, thus request
    that it is not initialized.
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- tools/aux/tpm2_rc_decode.c
+++ tools/aux/tpm2_rc_decode.c
@@ -74,7 +74,7 @@ static bool on_arg(int argc, char **argv) {
 bool tpm2_tool_onstart(tpm2_options **opts) {
 
     *opts = tpm2_options_new(NULL, 0, NULL, NULL, on_arg,
-            TPM2_OPTIONS_SHOW_USAGE);
+            TPM2_OPTIONS_SHOW_USAGE|TPM2_OPTIONS_NO_SAPI);
 
     return *opts != NULL;
 }
