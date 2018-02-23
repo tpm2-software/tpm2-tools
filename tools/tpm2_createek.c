89a6e69963d81d0957336402474fb783e1789149
William Roberts <william.c.roberts@intel.com>
2018-02-22 16:37:36 -0800

    tpm2_createek: update to use create_primary func
    Now that tpm2_hierarchy has a routine for creating a primary key,
    use this when creating an ek.
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- tools/tpm2_createek.c
+++ tools/tpm2_createek.c
@@ -40,6 +40,8 @@
 #include "log.h"
 #include "tpm2_alg_util.h"
 #include "tpm2_convert.h"
+#include "tpm2_ctx_mgmt.h"
+#include "tpm2_hierarchy.h"
 #include "tpm2_password_util.h"
 #include "tpm2_session.h"
 #include "tpm2_tool.h"
@@ -48,15 +50,14 @@
 typedef struct createek_context createek_context;
 struct createek_context {
     struct {
-        TPM2B_AUTH owner;
-        TPM2B_AUTH endorse;
-        TPM2B_AUTH ek;
+        TPMS_AUTH_COMMAND owner;
+        TPMS_AUTH_COMMAND endorse;
+        TPMS_AUTH_COMMAND ek;
     } passwords;
+    tpm2_hierearchy_pdata objdata;
     char *out_file_path;
     char *context_file;
-    TPM2_HANDLE persistent_handle;
-    TPM2_ALG_ID algorithm;
-    TPMS_AUTH_COMMAND session_data;
+    TPMI_DH_PERSISTENT persistent_handle;
     tpm2_convert_pubkey_fmt format;
     struct {
         bool f;
@@ -65,42 +66,18 @@ struct createek_context {
 
 static createek_context ctx = {
     .passwords = {
-        .owner = TPM2B_EMPTY_INIT,
-        .endorse = TPM2B_EMPTY_INIT,
-        .ek = TPM2B_EMPTY_INIT,
+        .owner = TPMS_AUTH_COMMAND_INIT(TPM2_RS_PW),
+        .endorse = TPMS_AUTH_COMMAND_INIT(TPM2_RS_PW),
+        .ek = TPMS_AUTH_COMMAND_INIT(TPM2_RS_PW),
     },
-    .algorithm = TPM2_ALG_RSA,
     .format = pubkey_format_tss,
-    .session_data = TPMS_AUTH_COMMAND_INIT(TPM2_RS_PW),
+    .objdata = TPM2_HIERARCHY_DATA_INIT
 };
 
 static bool set_key_algorithm(TPM2B_PUBLIC *inPublic)
 {
 
-    static BYTE auth_policy[] = {
-            0x83, 0x71, 0x97, 0x67, 0x44, 0x84, 0xB3, 0xF8, 0x1A, 0x90, 0xCC,
-            0x8D, 0x46, 0xA5, 0xD7, 0x24, 0xFD, 0x52, 0xD7, 0x6E, 0x06, 0x52,
-            0x0B, 0x64, 0xF2, 0xA1, 0xDA, 0x1B, 0x33, 0x14, 0x69, 0xAA
-    };
-
-    inPublic->publicArea.nameAlg = TPM2_ALG_SHA256;
-
-    // First clear attributes bit field.
-    inPublic->publicArea.objectAttributes = 0;
-    inPublic->publicArea.objectAttributes |= TPMA_OBJECT_RESTRICTED;
-    inPublic->publicArea.objectAttributes &= ~TPMA_OBJECT_USERWITHAUTH;
-    inPublic->publicArea.objectAttributes |= TPMA_OBJECT_ADMINWITHPOLICY;
-    inPublic->publicArea.objectAttributes &= ~TPMA_OBJECT_SIGN;
-    inPublic->publicArea.objectAttributes |= TPMA_OBJECT_DECRYPT;
-    inPublic->publicArea.objectAttributes |= TPMA_OBJECT_FIXEDTPM;
-    inPublic->publicArea.objectAttributes |= TPMA_OBJECT_FIXEDPARENT;
-    inPublic->publicArea.objectAttributes |= TPMA_OBJECT_SENSITIVEDATAORIGIN;
-    inPublic->publicArea.authPolicy.size = 32;
-    memcpy(inPublic->publicArea.authPolicy.buffer, auth_policy, 32);
-
-    inPublic->publicArea.type = ctx.algorithm;
-
-    switch (ctx.algorithm) {
+    switch (inPublic->publicArea.type) {
     case TPM2_ALG_RSA :
         inPublic->publicArea.parameters.rsaDetail.symmetric.algorithm =
                 TPM2_ALG_AES;
@@ -142,7 +119,7 @@ static bool set_key_algorithm(TPM2B_PUBLIC *inPublic)
         inPublic->publicArea.unique.sym.size = 0;
         break;
     default:
-        LOG_ERR("The algorithm type input(%4.4x) is not supported!", ctx.algorithm);
+        LOG_ERR("The algorithm type input(%4.4x) is not supported!", inPublic->publicArea.type);
         return false;
     }
 
@@ -151,72 +128,34 @@ static bool set_key_algorithm(TPM2B_PUBLIC *inPublic)
 
 static bool create_ek_handle(TSS2_SYS_CONTEXT *sapi_context) {
 
-    TSS2L_SYS_AUTH_RESPONSE sessionsDataOut;
-
-    TSS2L_SYS_AUTH_COMMAND sessionsData = { 1, { ctx.session_data }};
-
-    TPML_PCR_SELECTION creationPCR = { .count = 0 };
-
-    TPM2B_SENSITIVE_CREATE inSensitive =
-            TPM2B_TYPE_INIT(TPM2B_SENSITIVE_CREATE, sensitive);
-
-    TPM2B_PUBLIC inPublic = TPM2B_TYPE_INIT(TPM2B_PUBLIC, publicArea);
-
-    TPM2B_DATA outsideInfo = TPM2B_EMPTY_INIT;
-
-    TPM2B_NAME name = TPM2B_TYPE_INIT(TPM2B_NAME, name);
-
-    TPM2B_PUBLIC outPublic = TPM2B_EMPTY_INIT;
-
-    TPM2B_CREATION_DATA creationData = TPM2B_EMPTY_INIT;
-
-    TPM2B_DIGEST creationHash = TPM2B_TYPE_INIT(TPM2B_DIGEST, buffer);
-
-    TPMT_TK_CREATION creationTicket = TPMT_TK_CREATION_EMPTY_INIT;
-
-    memcpy(&sessionsData.auths[0].hmac, &ctx.passwords.endorse, sizeof(ctx.passwords.endorse));
-
-    bool result = set_key_algorithm(&inPublic);
+    bool result = set_key_algorithm(&ctx.objdata.in.public);
     if (!result) {
         return false;
     }
 
-    /* Create EK and get a handle to the key */
-    TPM2_HANDLE handle2048ek;
-    UINT32 rval = TSS2_RETRY_EXP(Tss2_Sys_CreatePrimary(sapi_context, TPM2_RH_ENDORSEMENT,
-            &sessionsData, &inSensitive, &inPublic, &outsideInfo, &creationPCR,
-            &handle2048ek, &outPublic, &creationData, &creationHash,
-            &creationTicket, &name, &sessionsDataOut));
-    if (rval != TPM2_RC_SUCCESS) {
-        LOG_PERR(Tss2_Sys_CreatePrimary, rval);
+    result = tpm2_hierarrchy_create_primary(sapi_context, &ctx.passwords.endorse,
+            &ctx.objdata);
+    if (!result) {
         return false;
     }
 
-    LOG_INFO("EK create success. Got handle: 0x%8.8x", handle2048ek);
-
     if (ctx.persistent_handle) {
-        memcpy(&sessionsData.auths[0].hmac, &ctx.passwords.owner, sizeof(ctx.passwords.owner));
 
-        rval = TSS2_RETRY_EXP(Tss2_Sys_EvictControl(sapi_context, TPM2_RH_OWNER, handle2048ek,
-                &sessionsData, ctx.persistent_handle, &sessionsDataOut));
-        if (rval != TPM2_RC_SUCCESS) {
-            LOG_PERR(Tss2_Sys_EvictControl, rval);
+        result = tpm2_ctx_mgmt_evictcontrol(sapi_context, TPM2_RH_OWNER,
+                &ctx.passwords.owner, ctx.objdata.out.handle,
+                ctx.persistent_handle);
+        if (!result) {
             return false;
         }
 
-        LOG_INFO("EvictControl EK persistent success.");
-
-        rval = TSS2_RETRY_EXP(Tss2_Sys_FlushContext(sapi_context, handle2048ek));
-        if (rval != TPM2_RC_SUCCESS) {
+        TSS2_RC rval = TSS2_RETRY_EXP(Tss2_Sys_FlushContext(sapi_context, ctx.objdata.out.handle));
+        if (rval != TSS2_RC_SUCCESS) {
             LOG_PERR(Tss2_Sys_FlushContext, rval);
             return false;
         }
-
-        LOG_INFO("Flush transient EK success.");
-
     } else if (ctx.context_file) {
-        bool result = files_save_tpm_context_to_path(sapi_context, handle2048ek,
-                ctx.context_file);
+        bool result = files_save_tpm_context_to_path(sapi_context,
+                ctx.objdata.out.handle, ctx.context_file);
         if (!result) {
             LOG_ERR("Error saving tpm context for handle");
             return false;
@@ -224,7 +163,8 @@ static bool create_ek_handle(TSS2_SYS_CONTEXT *sapi_context) {
     }
 
     if (ctx.out_file_path) {
-        return tpm2_convert_pubkey_save(&outPublic, ctx.format, ctx.out_file_path);
+        return tpm2_convert_pubkey_save(&ctx.objdata.out.public,
+                ctx.format, ctx.out_file_path);
     }
 
     return true;
@@ -243,34 +183,34 @@ static bool on_option(char key, char *value) {
         }
         break;
     case 'e':
-        result = tpm2_password_util_from_optarg(value, &ctx.passwords.endorse);
+        result = tpm2_password_util_from_optarg(value, &ctx.passwords.endorse.hmac);
         if (!result) {
             LOG_ERR("Invalid endorse password, got\"%s\"", value);
             return false;
         }
         break;
     case 'o':
-        result = tpm2_password_util_from_optarg(value, &ctx.passwords.owner);
+        result = tpm2_password_util_from_optarg(value, &ctx.passwords.owner.hmac);
         if (!result) {
             LOG_ERR("Invalid owner password, got\"%s\"", value);
             return false;
         }
         break;
     case 'P':
-        result = tpm2_password_util_from_optarg(value, &ctx.passwords.ek);
+        result = tpm2_password_util_from_optarg(value, &ctx.passwords.ek.hmac);
         if (!result) {
             LOG_ERR("Invalid EK password, got\"%s\"", value);
             return false;
         }
         break;
-    case 'g':
-        ctx.algorithm = tpm2_alg_util_from_optarg(value);
-        if (ctx.algorithm == TPM2_ALG_ERROR) {
-            LOG_ERR("Could not convert algorithm to value, got: %s",
-                    value);
+    case 'g': {
+        TPMI_ALG_PUBLIC type = tpm2_alg_util_from_optarg(value);
+        if (type == TPM2_ALG_ERROR) {
+            LOG_ERR("Invalid key algorithm, got\"%s\"", value);
             return false;
         }
-        break;
+        ctx.objdata.in.public.publicArea.type = type;
+    }   break;
     case 'p':
         if (!value) {
             LOG_ERR("Please specify an output file to save the pub ek to.");
@@ -279,12 +219,13 @@ static bool on_option(char key, char *value) {
         ctx.out_file_path = value;
         break;
     case 'S': {
-        tpm2_session *s = tpm2_session_restore(value);
-        if (!s) {
-            return false;
-        }
-        ctx.session_data.sessionHandle = tpm2_session_get_handle(s);
-        tpm2_session_free(&s);
+// TODO: restore and fix broken session handling
+//        tpm2_session *s = tpm2_session_restore(value);
+//        if (!s) {
+//            return false;
+//        }
+//        ctx.session_data.sessionHandle = tpm2_session_get_handle(s);
+//        tpm2_session_free(&s);
     } break;
     case 'f':
         ctx.format = tpm2_convert_pubkey_fmt_from_optarg(value);
@@ -311,15 +252,41 @@ bool tpm2_tool_onstart(tpm2_options **opts) {
         { "algorithm",            required_argument, NULL, 'g' },
         { "file",                 required_argument, NULL, 'p' },
         { "session",              required_argument, NULL, 'S' },
-        { "dbg",                  required_argument, NULL, 'd' },
         { "format",               required_argument, NULL, 'f' },
         { "context",              required_argument, NULL, 'c' },
     };
 
-    *opts = tpm2_options_new("e:o:H:P:g:p:S:d:f:c:", ARRAY_LEN(topts), topts,
-                             on_option, NULL, TPM2_OPTIONS_SHOW_USAGE);
+    *opts = tpm2_options_new("e:o:H:P:g:p:S:f:c:", ARRAY_LEN(topts), topts,
+                             on_option, NULL, 0);
 
     return *opts != NULL;
+}
+
+static void set_default_obj_attrs(void) {
+
+    ctx.objdata.in.public.publicArea.objectAttributes =
+      TPMA_OBJECT_RESTRICTED  | TPMA_OBJECT_ADMINWITHPOLICY
+    | TPMA_OBJECT_DECRYPT     | TPMA_OBJECT_FIXEDTPM
+    | TPMA_OBJECT_FIXEDPARENT | TPMA_OBJECT_SENSITIVEDATAORIGIN;
+}
+
+static void set_default_auth_policy(void) {
+
+    static const TPM2B_DIGEST auth_policy = {
+        .size = 32,
+        .buffer = {
+            0x83, 0x71, 0x97, 0x67, 0x44, 0x84, 0xB3, 0xF8, 0x1A, 0x90, 0xCC,
+            0x8D, 0x46, 0xA5, 0xD7, 0x24, 0xFD, 0x52, 0xD7, 0x6E, 0x06, 0x52,
+            0x0B, 0x64, 0xF2, 0xA1, 0xDA, 0x1B, 0x33, 0x14, 0x69, 0xAA
+        }
+    };
+
+    TPM2B_DIGEST *authp = &ctx.objdata.in.public.publicArea.authPolicy;
+    *authp = auth_policy;
+}
+
+static void set_default_hierarchy(void) {
+    ctx.objdata.in.hierarchy = TPM2_RH_ENDORSEMENT;
 }
 
 int tpm2_tool_onrun(TSS2_SYS_CONTEXT *sapi_context, tpm2_option_flags flags) {
@@ -336,6 +303,15 @@ int tpm2_tool_onrun(TSS2_SYS_CONTEXT *sapi_context, tpm2_option_flags flags) {
                 " save the context to, not both!");
         return 1;
     }
+
+    /* override the default attrs */
+    set_default_obj_attrs();
+
+    /* set the auth policy */
+    set_default_auth_policy();
+
+    /* set the default hierarchy */
+    set_default_hierarchy();
 
     /* normalize 0 success 1 failure */
     return create_ek_handle(sapi_context) != true;
