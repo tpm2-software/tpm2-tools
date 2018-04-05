98572d051e9649a17731f0d94ae32087dabf5e57
Joshua Lock <joshua.g.lock@intel.com>
2018-04-05 17:04:32 +0100

    tpm2_rsaencrypt: single option to specify key
    Use a single option, -c/--key-context, to handle both file and handle context
    values for the key object.
    
    Signed-off-by: Joshua Lock <joshua.g.lock@intel.com>

--- test/integration/tests/rsadecrypt.sh
+++ test/integration/tests/rsadecrypt.sh
@@ -74,7 +74,7 @@ tpm2_create -Q -g $alg_hash -G $alg_rsaencrypt_key -u $file_rsaencrypt_key_pub -
 
 tpm2_loadexternal -Q -a n   -u $file_rsaencrypt_key_pub  -C $file_rsaencrypt_key_ctx
 
-tpm2_rsaencrypt -Q -c $file_rsaencrypt_key_ctx -o $file_rsa_en_output_data < $file_input_data
+tpm2_rsaencrypt -Q -c file:$file_rsaencrypt_key_ctx -o $file_rsa_en_output_data < $file_input_data
 
 tpm2_load -Q -C file:$file_primary_key_ctx -u $file_rsaencrypt_key_pub -r $file_rsaencrypt_key_priv  -n $file_rsaencrypt_key_name  -o $file_rsadecrypt_key_ctx
 
