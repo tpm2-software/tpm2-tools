dce520347649fbd106685a994c590bd0b445b7a6
Joshua Lock <joshua.g.lock@intel.com>
2018-04-05 20:41:30 +0100

    tpm2_verifysignature: single option to specify key
    Use a single option, -c/--key-context, to handle both file and handle context
    values for the key object.
    
    Signed-off-by: Joshua Lock <joshua.g.lock@intel.com>

--- test/integration/tests/verifysignature.sh
+++ test/integration/tests/verifysignature.sh
@@ -79,16 +79,16 @@ tpm2_load -Q -C file:$file_primary_key_ctx  -u $file_signing_key_pub  -r $file_s
 
 tpm2_sign -Q -c file:$file_signing_key_ctx -g $alg_hash -m $file_input_data -s $file_output_data
 
-tpm2_verifysignature -Q -c $file_signing_key_ctx  -g $alg_hash -m $file_input_data  -s $file_output_data -t $file_verify_tk_data
+tpm2_verifysignature -Q -c file:$file_signing_key_ctx  -g $alg_hash -m $file_input_data  -s $file_output_data -t $file_verify_tk_data
 
 tpm2_hash -Q -a n -g $alg_hash -o $file_input_data_hash -t $file_input_data_hash_tk $file_input_data
 
 rm -f $file_verify_tk_data
-tpm2_verifysignature -Q -c  $file_signing_key_ctx  -D  $file_input_data_hash -s $file_output_data  -t $file_verify_tk_data
+tpm2_verifysignature -Q -c file:$file_signing_key_ctx  -D  $file_input_data_hash -s $file_output_data  -t $file_verify_tk_data
 
 rm -f $file_verify_tk_data $file_signing_key_ctx  -rf
 tpm2_loadexternal -Q -a n -u $file_signing_key_pub -C  $file_signing_key_ctx
 
-tpm2_verifysignature -Q -c  $file_signing_key_ctx  -g $alg_hash -m $file_input_data  -s $file_output_data -t $file_verify_tk_data
+tpm2_verifysignature -Q -c file:$file_signing_key_ctx  -g $alg_hash -m $file_input_data  -s $file_output_data -t $file_verify_tk_data
 
 exit 0
