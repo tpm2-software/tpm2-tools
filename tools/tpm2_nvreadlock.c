40fe4866c6d2f8c09537ec11f336145ffdefc724
William Roberts <william.c.roberts@intel.com>
2018-04-24 12:40:35 -0700

    tpm2_nvreadlock: remove dead option passwdInHex
    Signed-off-by: William Roberts <william.c.roberts@intel.com>
    Signed-off-by: Remus Wu <remuswu1019@gmail.com>

--- tools/tpm2_nvreadlock.c
+++ tools/tpm2_nvreadlock.c
@@ -124,11 +124,10 @@ bool tpm2_tool_onstart(tpm2_options **opts) {
         { "index"       , required_argument, NULL, 'x' },
         { "auth-handle"  , required_argument, NULL, 'a' },
         { "handle-passwd", required_argument, NULL, 'P' },
-        { "passwdInHex" , no_argument,       NULL, 'X' },
         { "input-session-handle",1,          NULL, 'S' },
     };
 
-    *opts = tpm2_options_new("x:a:P:Xp:d:S:hv", ARRAY_LEN(topts), topts,
+    *opts = tpm2_options_new("x:a:P:p:d:S:hv", ARRAY_LEN(topts), topts,
             on_option, NULL, TPM2_OPTIONS_SHOW_USAGE);
 
     return *opts != NULL;
