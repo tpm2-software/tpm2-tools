f2d0bae1a65e7efcbd28bf99e8edd1156b7b6dba
William Roberts <william.c.roberts@intel.com>
2018-04-30 12:53:28 -0700

    travis: skip cloneing source if already downloaded
    Allows the docker.run script to be run in an already initialized
    docker container.
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- .ci/download-deps.sh
+++ .ci/download-deps.sh
@@ -36,26 +36,35 @@ function get_deps() {
 	echo "pwd starting: `pwd`"
 	pushd "$1"
 	echo "pwd clone tss: `pwd`"
-	git clone --depth=1 https://github.com/tpm2-software/tpm2-tss.git
-	pushd tpm2-tss
-	echo "pwd build tss: `pwd`"
-	./bootstrap
-	./configure
-	make -j4
-	make install
-	popd
-	echo "pwd done tss: `pwd`"
+	if [ ! -d tpm2-tss ]; then
+		git clone --depth=1 https://github.com/tpm2-software/tpm2-tss.git
+		pushd tpm2-tss
+		echo "pwd build tss: `pwd`"
+		./bootstrap
+		./configure CFLAGS=-g
+		make -j4
+		make install
+		popd
+		echo "pwd done tss: `pwd`"
+	else
+		echo "tss already downloaded/built/installed, skipping"
+	fi
 
-	echo "pwd clone abrmd: `pwd`"
-	git clone --depth=1 https://github.com/tpm2-software/tpm2-abrmd.git
-	pushd tpm2-abrmd
-	echo "pwd build abrmd: `pwd`"
-	./bootstrap
-	./configure
-	make -j4
-	make install
-	popd
-	echo "pwd done abrmd: `pwd`"
-	popd
-	echo "pwd done: `pwd`"
+	if [ ! -d tpm2-abrmd ]; then
+		echo "pwd clone abrmd: `pwd`"
+		git clone --depth=1 https://github.com/tpm2-software/tpm2-abrmd.git
+		pushd tpm2-abrmd
+		echo "pwd build abrmd: `pwd`"
+		./bootstrap
+		./configure CFLAGS=-g
+		make -j4
+		make install
+		popd
+		echo "pwd done abrmd: `pwd`"
+		popd
+		echo "pwd done: `pwd`"
+	else
+		echo "abrmd already downloaded/built/installed, skipping"
+	fi
+
 }
