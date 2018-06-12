ebc8c092ae96e3ce4f6e663f18f390b1939fbc19
Joshua Lock <joshua.g.lock@intel.com>
2018-06-12 14:06:31 +0100

    tpm2_nvread: no need to specify hierarchy when using index auth
    Reduce the need to specify the same argument to two different options.
    When the user specifies an --auth-hierarchy/-P of "index" and doesn't
    specify an --auth-handle/-a default to the NV_INDEX specified by the
    --index/-x option.
    
    Signed-off-by: Joshua Lock <joshua.g.lock@intel.com>

--- tools/tpm2_nvread.c
+++ tools/tpm2_nvread.c
@@ -64,6 +64,7 @@ struct tpm_nvread_ctx {
     struct {
         UINT8 L : 1;
         UINT8 P : 1;
+        UINT8 a : 1;
     } flags;
     char *hierarchy_auth_str;
 };
@@ -199,6 +200,7 @@ static bool on_option(char key, char *value) {
         if (!result) {
             return false;
         }
+        ctx.flags.a = 1;
         break;
     case 'f':
         ctx.output_file = value;
@@ -305,6 +307,7 @@ int tpm2_tool_onrun(TSS2_SYS_CONTEXT *sapi_context, tpm2_option_flags flags) {
             goto out;
         }
     }
+
     if (ctx.flags.P) {
         result = tpm2_auth_util_from_optarg(sapi_context, ctx.hierarchy_auth_str,
                 &ctx.auth.session_data, &ctx.auth.session);
@@ -312,6 +315,20 @@ int tpm2_tool_onrun(TSS2_SYS_CONTEXT *sapi_context, tpm2_option_flags flags) {
             LOG_ERR("Invalid handle authorization, got\"%s\"",
                 ctx.hierarchy_auth_str);
             return false;
+        }
+    }
+
+    /* If the users specifies "index" for auth-hierarchy and doesn't explicitly
+    * pass a value to -a use the index passed to -x/--index for the
+    * authorisation index.
+    */
+    if (!ctx.flags.a && ctx.flags.P) {
+        // we don't have an auth hierarchy set, use the index
+        if (!strcmp(ctx.hierarchy_auth_str, "index")) {
+            ctx.auth.hierarchy = ctx.nv_index;
+        } else {
+            LOG_ERR("Invalid auth options, use either an explicit -a or a -P"
+                    " value of \"index\".");
         }
     }
 
