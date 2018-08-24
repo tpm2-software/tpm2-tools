914d0cb3a2d4c0bc9093fd78d46bc1c886636303
Joshua Lock <joshua.g.lock@intel.com>
2018-08-24 11:42:27 +0100

    ESAPI: finish port of lib/tpm2_capability
    Signed-off-by: Joshua Lock <joshua.g.lock@intel.com>

--- lib/tpm2_capability.h
+++ lib/tpm2_capability.h
@@ -58,38 +58,15 @@ bool tpm2_capability_get (ESYS_CONTEXT *context,
         TPMS_CAPABILITY_DATA **capability_data);
 
 /**
- * Invokes GetCapability to retrieve the current value of a capability from the
- * TPM.
- * @param sapi_ctx
- *  system api context
- * @param capability
- *  the capability being requested from the TPM
- * @param property
- *  property
- * @param count
- *  maximum number of values to return
- * @param capability_data
- *  capability data structure to populate
- * @return
- *  True if the capability_data structure is successfully filled, False if the
- *  call to the TPM fails.
- */
-bool tpm2_capability_get_sapi (TSS2_SYS_CONTEXT *sapi_ctx,
-        TPM2_CAP capability,
-        UINT32 property,
-        UINT32 count,
-        TPMS_CAPABILITY_DATA *capability_data);
-
-/**
  * Attempts to find a vacant handle in the persistent handle namespace.
- * @param sapi_ctx
- *  system api context
+ * @param ctx
+ *  Enhanced System API (ESAPI) context
  * @param vacant
  *  the vacant handle found by the function if True returned
  * @return
  *  True if a vacant handle was found successfully, False otherwise.
  */
-bool tpm2_capability_find_vacant_persistent_handle (TSS2_SYS_CONTEXT *sapi_ctx,
+bool tpm2_capability_find_vacant_persistent_handle (ESYS_CONTEXT *ctx,
         UINT32 *vacant);
 
 #endif /* LIB_TPM2_CAPABILITY_H_ */
