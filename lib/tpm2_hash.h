92fc1a768a555363a183251741783c2689db74fe
Joshua Lock <joshua.g.lock@intel.com>
2018-08-24 12:09:31 +0100

    ESAPI: purge SAPI from tpm2_hash
    Signed-off-by: Joshua Lock <joshua.g.lock@intel.com>

--- lib/tpm2_hash.h
+++ lib/tpm2_hash.h
@@ -33,7 +33,6 @@
 
 #include <stdbool.h>
 
-#include <tss2/tss2_sys.h>
 #include <tss2/tss2_esys.h>
 
 /**
@@ -60,10 +59,6 @@ bool tpm2_hash_compute_data(ESYS_CONTEXT *context, TPMI_ALG_HASH halg,
         TPMI_RH_HIERARCHY hierarchy, BYTE *buffer, UINT16 length,
         TPM2B_DIGEST **result, TPMT_TK_HASHCHECK **validation);
 
-bool tpm2_hash_compute_data_sapi(TSS2_SYS_CONTEXT *context, TPMI_ALG_HASH halg,
-        TPMI_RH_HIERARCHY hierarchy, BYTE *buffer, UINT16 length,
-        TPM2B_DIGEST *result, TPMT_TK_HASHCHECK *validation);
-
 /**
  * Hashes a FILE * object via the tpm.
  * @param context
@@ -85,9 +80,5 @@ bool tpm2_hash_compute_data_sapi(TSS2_SYS_CONTEXT *context, TPMI_ALG_HASH halg,
 bool tpm2_hash_file(ESYS_CONTEXT *ectx, TPMI_ALG_HASH halg,
         TPMI_RH_HIERARCHY hierarchy, FILE *input, TPM2B_DIGEST **result,
         TPMT_TK_HASHCHECK **validation);
-
-bool tpm2_hash_file_sapi(TSS2_SYS_CONTEXT *context, TPMI_ALG_HASH halg,
-        TPMI_RH_HIERARCHY hierarchy, FILE *input, TPM2B_DIGEST *result,
-        TPMT_TK_HASHCHECK *validation);
 
 #endif /* SRC_TPM_HASH_H_ */
