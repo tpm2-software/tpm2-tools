c5cd3d762f52d67e94b7d8148493b795c2bd9460
Joshua Lock <joshua.g.lock@intel.com>
2018-08-24 14:37:32 +0100

    ESAPI: port lib/tpm2_ctx_mgmt
    Signed-off-by: Joshua Lock <joshua.g.lock@intel.com>

--- lib/tpm2_ctx_mgmt.h
+++ lib/tpm2_ctx_mgmt.h
@@ -1,25 +1,30 @@
 #ifndef LIB_TPM2_CTX_MGMT_H_
 #define LIB_TPM2_CTX_MGMT_H_
 
+#include "tpm2_session.h"
+
 /**
  * Invokes evictcontrol for manipulating the persistence of loaded
  * objects in TPM memory.
- * @param sapi_context
- *  The system api context
+ * @param context
+ *  The Enhanced System API (ESAPI) context
+ * @param auth
+ *  The authorisation hierarchy, either TPM2_RH_OWNER or TPM2_RH_PLATFORM
  * @param sdata
- *  The authorization data.
+ *  The authorization data for auth.
  * @param objhandle
- *  The object handle to manipulate.
+ *  The object handle of a loaded object to manipulate.
  * @param phandle
- *  The persistent handle to use.
+ *  The handle to persist objhandle at, if objhandle is transient.
  * @return
  *  True on success, False on error.
  *  Use LOG_PERR() to output error information.
  */
-bool tpm2_ctx_mgmt_evictcontrol(TSS2_SYS_CONTEXT *sapi_context,
-        TPMI_RH_PROVISION provision,
+bool tpm2_ctx_mgmt_evictcontrol(ESYS_CONTEXT *context,
+        ESYS_TR auth,
         TPMS_AUTH_COMMAND *sdata,
-        TPMI_DH_OBJECT objhandle,
+        tpm2_session *sess,
+        ESYS_TR objhandle,
         TPMI_DH_PERSISTENT phandle);
 
 #endif /* LIB_TPM2_CTX_MGMT_H_ */
