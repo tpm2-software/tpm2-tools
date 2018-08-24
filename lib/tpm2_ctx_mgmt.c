aa42df8faf74d5419d2dc7b27dbfd53f3c366268
Joshua Lock <joshua.g.lock@intel.com>
2018-08-24 14:37:32 +0100

    ESAPI: port lib/tpm2_ctx_mgmt
    Signed-off-by: Joshua Lock <joshua.g.lock@intel.com>

--- lib/tpm2_ctx_mgmt.c
+++ lib/tpm2_ctx_mgmt.c
@@ -1,30 +1,46 @@
 #include <stdbool.h>
 
-#include <tss2/tss2_sys.h>
+#include <tss2/tss2_esys.h>
 
 #include "log.h"
+#include "tpm2_auth_util.h"
 #include "tpm2_ctx_mgmt.h"
 
-bool tpm2_ctx_mgmt_evictcontrol(TSS2_SYS_CONTEXT *sapi_context,
-        TPMI_RH_PROVISION provision,
+bool tpm2_ctx_mgmt_evictcontrol(ESYS_CONTEXT *ectx,
+        ESYS_TR auth,
         TPMS_AUTH_COMMAND *sdata,
-        TPMI_DH_OBJECT objhandle,
+        tpm2_session *sess,
+        ESYS_TR objhandle,
         TPMI_DH_PERSISTENT phandle) {
 
-    TSS2L_SYS_AUTH_COMMAND sessionsData =
-        TSS2L_SYS_AUTH_COMMAND_INIT(1, { *sdata });
+    TSS2_RC rval;
 
-    TSS2L_SYS_AUTH_RESPONSE sessionsDataOut;
-
-    TSS2_RC rval = TSS2_RETRY_EXP(Tss2_Sys_EvictControl(sapi_context,
-        provision,
-        objhandle,
-        &sessionsData,
-        phandle,
-        &sessionsDataOut));
-    if (rval != TSS2_RC_SUCCESS) {
-        LOG_PERR(Tss2_Sys_EvictControl, rval);
+    ESYS_TR shandle1 = tpm2_auth_util_get_shandle(ectx, auth, sdata, sess);
+    if (shandle1 == ESYS_TR_NONE) {
+        LOG_ERR("Couldn't get shandle for eviction target");
         return false;
+    }
+
+    ESYS_TR outHandle;
+    rval = Esys_EvictControl(ectx, auth, objhandle,
+            shandle1, ESYS_TR_NONE, ESYS_TR_NONE,
+            phandle, &outHandle);
+
+    if (rval != TSS2_RC_SUCCESS) {
+        LOG_PERR(Esys_EvictControl, rval);
+        switch(rval) {
+            case TSS2_ESYS_RC_BAD_TR:
+                LOG_ERR("Bad TR");
+                break;
+            default:
+            break;
+        }
+        return false;
+    }
+
+    rval = Esys_TR_Close(ectx, &outHandle);
+    if (rval != TPM2_RC_SUCCESS) {
+        LOG_PERR(Esys_TR_Close, rval);
     }
 
     return true;
