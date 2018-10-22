66396bbbb6a8f37405b2805acf8b765bfd82355c
Imran Desai <imran.desai@intel.com>
2018-10-22 16:14:21 -0700

    tpm2_policyauthorize.c: Aptly renaming structure per the tool name

--- tools/tpm2_policyauthorize.c
+++ tools/tpm2_policyauthorize.c
@@ -39,8 +39,8 @@
 #include "tpm2_tool.h"
 #include "tpm2_util.h"
 
-typedef struct tpm2_startauthsession_ctx tpm2_startauthsession_ctx;
-struct tpm2_startauthsession_ctx {
+typedef struct tpm2_policyauthorize_ctx tpm2_policyauthorize_ctx;
+struct tpm2_policyauthorize_ctx {
    //File path for the session context data
    const char *session_path;
    //File path for the policy digest that will be authorized
@@ -55,7 +55,7 @@ struct tpm2_startauthsession_ctx {
    const char *out_policy_dgst_path;
 };
 
-static tpm2_startauthsession_ctx ctx;
+static tpm2_policyauthorize_ctx ctx;
 
 static bool on_option(char key, char *value) {
 
