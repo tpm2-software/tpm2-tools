d21e32b395124d227bab5aeefeadfe25fcc7dd25
Imran Desai <imran.desai@intel.com>
2018-11-15 16:32:17 -0700

    tpm2_policypassword: Adding new tool to bind policies with PolicyPassword

---
+++ man/tpm2_policypassword.1.md
@@ -0,0 +1,79 @@
+% tpm2_policypassword(1) tpm2-tools | General Commands Manual
+%
+% AUGUST 2018
+
+# NAME
+
+**tpm2_policypassword**(1) - Enables binding a policy to the authorization value
+ of the authorized TPM object.
+
+# SYNOPSIS
+
+**tpm2_policypassword** [*OPTIONS*]
+
+# DESCRIPTION
+
+**tpm2_policypassword**(1) Enables a policy that requires the object's
+authentication passphrase be provided. This is equivalent to authenticating
+using the object passphrase in plaintext, only this enforces it as a policy.
+It provides a mechanism to allow for password authentication when an object only
+allows policy based authorization, ie object attribute "userwithauth" is 0.
+If using a resource manager (RM), then one supporting extended sessions, like
+[tpm2-abrmd](https://github.com/tpm2-software/tpm2-abrmd) is required.
+
+# OPTIONS
+
+  * **-o**, **--policy-file**=_POLICY\_FILE_:
+
+    File to save the compounded policy digest.
+
+  * **-S**, **--session**=_SESSION_FILE_:
+
+    The policy session file generated via the **-S** option to
+    **tpm2_startauthsession**(1).
+
+[common options](common/options.md)
+
+[common tcti options](common/tcti.md)
+
+[supported hash algorithms](common/hash.md)
+
+[algorithm specifiers](common/alg.md)
+
+# EXAMPLES
+
+We want to authenticate using the TPM objects plaintext authentication value.
+While we could authenticate with an ephermal password session, in this example
+we will authenticate with the plaintext passphrase in  a policy session instead
+using the **tpm2_policypassword** tool.
+## Create the password policy:
+```
+tpm2_startauthsession -S session.dat
+tpm2_policypassword -S session.dat -o policy.dat
+tpm2_flushcontext -S session.dat
+```
+## Create the object with a passphrase and the password policy:
+```
+tpm2_createprimary -a o -o prim.ctx
+tpm2_create -g sha256 -G aes -u key.pub -r key.priv -C prim.ctx -L policy.pass \
+  -p testpswd
+```
+## Authenticate with plaintext passphrase input:
+```
+tpm2_load -C prim.ctx -u key.pub -r key.priv -n key.name -o key.ctx
+tpm2_encryptdecrypt -c key.ctx -o encrypt.out -I plain.txt -p text
+```
+## Authenticate with password and the policy:
+```
+tpm2_startauthsession -a -S session.dat
+tpm2_policypassword -S session.dat -o policy.dat
+tpm2_encryptdecrypt -c key.ctx -o encrypt.out -I plain.txt \
+  -p session:session.dat+testpswd
+tpm2_flushcontext -S session.dat
+```
+
+# RETURNS
+
+0 on success or 1 on failure.
+
+[footer](common/footer.md)
---
+++ tools/tpm2_policypassword.c
@@ -0,0 +1,140 @@
+//**********************************************************************;
+// Copyright (c) 2018, Intel Corporation
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+//
+// 3. Neither the name of Intel Corporation nor the names of its contributors
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+// THE POSSIBILITY OF SUCH DAMAGE.
+//**********************************************************************;
+
+#include <tss2/tss2_sys.h>
+
+#include "files.h"
+#include "log.h"
+#include "tpm2_alg_util.h"
+#include "tpm2_options.h"
+#include "tpm2_policy.h"
+#include "tpm2_session.h"
+#include "tpm2_tool.h"
+#include "tpm2_util.h"
+
+typedef struct tpm2_policypassword_ctx tpm2_policypassword_ctx;
+struct tpm2_policypassword_ctx {
+   //File path for the session context data
+   const char *session_path;
+   //File path for storing the policy digest output
+   const char *out_policy_dgst_path;
+};
+
+static tpm2_policypassword_ctx ctx;
+
+static bool on_option(char key, char *value) {
+
+    switch (key) {
+    case 'o':
+        ctx.out_policy_dgst_path = value;
+        break;
+    case 'S':
+        ctx.session_path = value;
+        break;
+    }
+
+    return true;
+}
+
+bool tpm2_tool_onstart(tpm2_options **opts) {
+
+    static struct option topts[] = {
+        { "policy-file",       required_argument, NULL, 'o' },
+        { "session",           required_argument, NULL, 'S' },
+    };
+
+    *opts = tpm2_options_new("o:S:", ARRAY_LEN(topts), topts, on_option,
+                             NULL, 0);
+
+    return *opts != NULL;
+}
+
+bool is_input_option_args_valid(void) {
+
+    if (!ctx.session_path) {
+        LOG_ERR("Must specify -S session file.");
+        return false;
+    }
+    return true;
+}
+
+int tpm2_tool_onrun(TSS2_SYS_CONTEXT *sapi_context, tpm2_option_flags flags) {
+
+    UNUSED(flags);
+
+    bool retval = is_input_option_args_valid();
+    if (!retval) {
+        return -1;
+    }
+
+    int rc = 1;
+    tpm2_session *s = tpm2_session_restore(sapi_context, ctx.session_path);
+    if (!s) {
+        return rc;
+    }
+
+    bool result = tpm2_policy_build_policypassword(sapi_context, s);
+    if (!result) {
+        LOG_ERR("Could not build policypassword TPM");
+        goto out;
+    }
+
+    TPM2B_DIGEST policy_digest = TPM2B_EMPTY_INIT;
+    result = tpm2_policy_get_digest(sapi_context, s, &policy_digest);
+    if (!result) {
+        LOG_ERR("Could not build tpm policy");
+        goto out;
+    }
+
+    tpm2_util_hexdump(policy_digest.buffer, policy_digest.size);
+
+    if (ctx.out_policy_dgst_path) {
+        result = files_save_bytes_to_file(ctx.out_policy_dgst_path, policy_digest.buffer,
+                    policy_digest.size);
+        if (!result) {
+            LOG_ERR("Failed to save policy digest into file \"%s\"",
+                    ctx.out_policy_dgst_path);
+            goto out;
+        }
+    }
+
+    result = tpm2_session_save(sapi_context, s, ctx.session_path);
+    if (!result) {
+        LOG_ERR("Failed to save policy to file \"%s\"", ctx.session_path);
+        goto out;
+    }
+
+    rc = 0;
+
+out:
+    tpm2_session_free(&s);
+    return rc;
+}
