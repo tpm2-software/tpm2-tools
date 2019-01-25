3ea6e2a77d602c10b32863cf21e771cb18404c1c
William Roberts <william.c.roberts@intel.com>
2019-01-25 11:04:05 -0800

    tpm2_changeauth: fix transient object changeauth
    Populate the ESYS_TR handle when given a TPM2_HANDLE. When specifying an object
    via a TPM2_HANDLE provided by -a, convert that handle into an ESYS_TR handle
    identifier.
    
    Additionally, the switch statement verifying the type of handle, PCR, transient, etc
    cannot check the TPM2_HANDLE on the transient case, as the handle is 0 and is not
    exposed via the ESYS_TR paradigm.
    
    This fixes transient object change auth.
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- tools/tpm2_changeauth.c
+++ tools/tpm2_changeauth.c
@@ -312,6 +312,16 @@ static bool process_change_tpm_handle_auth(ESYS_CONTEXT *ectx) {
         return false;
     }
 
+    if (!ctx.tpm_handle_parent_context_object.tr_handle) {
+        result = tpm2_util_sys_handle_to_esys_handle(
+                ectx,
+                ctx.tpm_handle_parent_context_object.handle,
+                &ctx.tpm_handle_parent_context_object.tr_handle);
+        if (!result) {
+            return result;
+        }
+    }
+
     TPM2B_PRIVATE *outPrivate = NULL;
     TSS2_RC rval = Esys_ObjectChangeAuth(ectx,
                         ctx.tpm_handle_context_object.tr_handle,
@@ -429,8 +439,9 @@ static bool is_input_option_args_valid(void) {
                 ctx.is_nv = true;
                 break;
             default:
-                LOG_ERR("Unsupported handle type for auth change");
-                return false;
+                // transient objects are only specified via file context and thus
+                // have no handle just an ESYS_TR
+                ctx.is_transient = true;
         }
     }
 
