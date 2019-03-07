b0923db1fea55853ceb6d9dba8693b259997bb2f
GlovePuppet <touched.by.his.noodley.appendage@gmail.com>
2019-03-07 11:42:09 -0500

    Added tpm2_nvincrement to nv group
    Signed-off-by: GlovePuppet <touched.by.his.noodley.appendage@gmail.com>

--- scripts/utils/analyze.py
+++ scripts/utils/analyze.py
@@ -157,7 +157,7 @@ class ToolConflictor(object):
                 "gname": "nv",
                 "tools-in-group": [
                     "tpm2_nvreadlock", "tpm2_nvrelease", "tpm2_nvdefine",
-                    "tpm2_nvread", "tpm2_nvwrite", "tpm2_nvlist"
+                    "tpm2_nvread", "tpm2_nvwrite", "tpm2_nvlist", "tpm2_nvincrement"
                 ],
                 "tools": [],
                 "conflict": None,
