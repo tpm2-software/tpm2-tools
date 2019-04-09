b2b7c3c1c4a1056a72f9a63fff08317be02d216b
Jonas Witschel <diabonas@gmx.de>
2019-04-09 17:41:59 +0200

    test/integration: don't hardcode header file location
    Attempt to locate the file using pkg-config, falling back to
    /usr/local/include and /usr/include if not available.
    
    Fixes d4a34cf818e8c93eb5cf186edb57d99f2454d18b
    
    Signed-off-by: Jonas Witschel <diabonas@gmx.de>

--- test/integration/tests/rc_decode.sh
+++ test/integration/tests/rc_decode.sh
@@ -49,20 +49,28 @@ trap - EXIT
 #
 declare -A codes
 
-if [ ! -f /usr/local/include/tss2/tss2_tpm2_types.h ]; then
-    echo "Expected TSS2 headers to be installed in /usr/local/include"
+tss2_tpm2_types=''
+for dir in "$(pkg-config --variable includedir tss2-esys)" /usr/local/include /usr/include; do
+    if [ -f "$dir/tss2/tss2_tpm2_types.h" ]; then
+        tss2_tpm2_types="$dir/tss2/tss2_tpm2_types.h"
+        break
+    fi
+done
+
+if [ -z "$tss2_tpm2_types" ]; then
+    echo "Could not find TSS2 headers"
     exit 1
 fi
 
 # Populate the main TPM2_RC values
-eval $(grep -Po "^#define \K(TPM2_RC.*)" /usr/local/include/tss2/tss2_tpm2_types.h \
+eval $(grep -Po "^#define \K(TPM2_RC.*)" "$tss2_tpm2_types" \
           | grep -v '+' \
           | sed "s%/*[^/]*/$%%g" \
           | sed "s%[[:space:]]*((TPM2_RC)[[:space:]]*%=%g" \
           | sed "s%)%%g")
 
 # Generate the TPM2_RC array based on TSS2 header
-varlist="$(sed -rn "s%^#define (TPM2_RC_[^[:space:]]*)[[:space:]]*\(\(TPM2_RC\) \((TPM2_RC[^\)]*)[^/]*/\* ([^\*]*) \*/%\1=\$\(\(\2\)\):\3%p" /usr/local/include/tss2/tss2_tpm2_types.h)"
+varlist="$(sed -rn "s%^#define (TPM2_RC_[^[:space:]]*)[[:space:]]*\(\(TPM2_RC\) \((TPM2_RC[^\)]*)[^/]*/\* ([^\*]*) \*/%\1=\$\(\(\2\)\):\3%p" "$tss2_tpm2_types")"
 while IFS='=' read key value; do
     codes[${key}]="${value}"
 done <<< "${varlist}"
