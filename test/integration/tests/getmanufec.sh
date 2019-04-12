6df7ac23255a532db88e1c2b531ef8802b49f2a6
sebastien le stum <lestums@gmail.com>
2019-04-11 22:57:39 -0400

    tools/tpm2_getmanufec: Change "-o" to "-w" for owner and "-f" to "-o"
    "-o" is used for "out-file". Thus use the "-w' for oWner and changed
    "-f" back to appropriate "-o"
    
    Signed-off-by: sebastien le stum <lestums@gmail.com>

--- test/integration/tests/getmanufec.sh
+++ test/integration/tests/getmanufec.sh
@@ -68,9 +68,9 @@ cmp ECcert.bin ECcert2.bin
 
 # Test providing endorsement password to create EK and owner password to persist.
 tpm2_clear
-tpm2_changeauth -o $opass -e $epass
+tpm2_changeauth -w $opass -e $epass
 
-tpm2_getmanufec -H $handle -U -E ECcert2.bin -f test_ek.pub -o $opass -e $epass \
+tpm2_getmanufec -H $handle -U -E ECcert2.bin -o test_ek.pub -w $opass -e $epass \
                 https://ekop.intel.com/ekcertservice/
 
 tpm2_listpersistent | grep -q $handle
@@ -83,7 +83,7 @@ if [ $(md5sum ECcert.bin| awk '{ print $1 }') != "56af9eb8a271bbf7ac41b780acd91f
 fi
 
 # Test with automatic persistent handle
-tpm2_getmanufec -H - -U -E ECcert2.bin -f test_ek.pub -o $opass -e $epass \
+tpm2_getmanufec -H - -U -E ECcert2.bin -o test_ek.pub -w $opass -e $epass \
                 https://ekop.intel.com/ekcertservice/ > man.log
 phandle=`yaml_get_kv man.log \"persistent\-handle\"`
 
