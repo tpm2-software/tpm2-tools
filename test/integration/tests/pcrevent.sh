ff50de1084823893ad809859a34dcb84c220a922
sebastien le stum <lestums@gmail.com>
2019-04-11 23:06:57 -0400

    tools/tpm2_pcrevent: Migrate from "-i" to "-x"
    Signed-off-by: sebastien le stum <lestums@gmail.com>

--- test/integration/tests/pcrevent.sh
+++ test/integration/tests/pcrevent.sh
@@ -81,7 +81,7 @@ old_pcr_value=`yaml_get_kv $yaml_out_file \"sha1\" 9`
 
 # Verify that extend works, and test large files
 dd if=/dev/urandom of=$hash_in_file count=1 bs=2093 2> /dev/null
-tpm2_pcrevent -Q -i 9 $hash_in_file
+tpm2_pcrevent -Q -x 9 $hash_in_file
 
 tpm2_pcrlist -L sha1:9 > $yaml_out_file
 new_pcr_value=`yaml_get_kv $yaml_out_file \"sha1\" 9`
