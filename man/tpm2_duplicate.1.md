f67f2a8912188443c4be5a9a51751d01823dc115
GlovePuppet <touched.by.his.noodley.appendage@gmail.com>
2019-04-18 19:14:35 -0400

    tpm2_duplicate: introduce new tool
    Introduce a new tool for duplicating tpm objects, man page and tests.
    
    Signed-off-by: GlovePuppet <touched.by.his.noodley.appendage@gmail.com>
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

---
+++ man/tpm2_duplicate.1.md
@@ -0,0 +1,84 @@
+% tpm2_duplicate(1) tpm2-tools | General Commands Manual
+%
+% APRIL 2019
+
+# NAME
+
+tpm2_duplicate(1) -  Duplicates a loaded object so that it may be used in a different hierarchy.
+
+# SYNOPSIS
+
+**tpm2_duplicate** [*OPTIONS*]
+
+# DESCRIPTION
+
+**tpm2_duplicate**(1) - This tool duplicates a loaded object so that it may be used in a different hierarchy. The new parent key for the duplicate may be on the same or different TPM or TPM_RH_NULL.
+
+# OPTIONS
+
+These options control the key importation process:
+
+  * **-g**, **--inner-wrapper-alg**=_ALGORITHM_:
+
+    The symmetric algorithm to be used for the inner wrapper. Supports:
+    * aes - AES 128 in CFB mode.
+    * null - none
+
+  * **-i**, **--input-key-file**=_FILE_:
+
+    Specifies the filename of the symmetric key (128 bit data) to be used for the inner wrapper. Valid only when specified symmetric algorithm is not null
+
+  * **-o**, **--output-key-file**=_FILE_:
+
+    Specifies the filename to store the symmetric key (128 bit data) that was used for the inner wrapper. Valid only when specified symmetric algorithm is not null and --input-key-file is not specified
+
+  * **-C**, **--parent-key**=_PARENT\_CONTEXT_:
+
+    Specifies the context object for the parent key. Either a file, a handle number or null to select TPM2_RH_NULL.
+
+  * **-r**, **--duplicate-key-private**=_FILE_:
+
+    Specifies the file path to save the private portion of the duplicated object.
+
+  * **-s**, **--output-enc-seed-file**=_FILE_:
+
+    Specifies the file path required to save the encrypted seed of the duplicated
+    object.
+
+  * **-p**, **--auth-key**=_KEY\_AUTH_:
+    The authorization value for the key, optional.
+    Follows the authorization formatting of the
+    "password for parent key" option: **-P**.
+
+[common options](common/options.md)
+
+[common tcti options](common/tcti.md)
+
+[context object format](common/ctxobj.md)
+
+[algorithm specifiers](common/alg.md)
+
+# EXAMPLES
+
+To duplicate a key, one needs the key to duplicate, created with a policy that allows duplication and a new parent:
+```
+tpm2_startauthsession -S session.dat
+tpm2_policycommandcode -S session.dat -o policy.dat 0x14B
+tpm2_flushcontext -S session.dat
+
+tpm2_createprimary -a o -g sha256 -G rsa -o primary.ctxt
+tpm2_create -C primary.ctxt -g sha256 -G rsa -r key.prv -u key.pub -L policy.dat -b "sensitivedataorigin"
+
+tpm2_loadexternal -a o -u new_parent.pub -o new_parent.ctxt
+
+tpm2_startauthsession --policy-session -S session.dat
+tpm2_policycommandcode -S session.dat -o policy.dat 0x14B
+tpm2_duplicate -C new_parent.ctxt -c key.ctxt -G null -p "session:session.dat" -r duprv.bin -s seed.dat
+tpm2_flushcontext -S session.dat
+```
+
+# RETURNS
+
+0 on success or 1 on failure.
+
+[footer](common/footer.md)
---
+++ test/integration/tests/duplicate.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+#;**********************************************************************;
+#
+# Copyright (c) 2019, GlovePuppet
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of Intel Corporation nor the names of its contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+# THE POSSIBILITY OF SUCH DAMAGE.
+#;**********************************************************************;
+
+source helpers.sh
+
+TPM_CC_DUPLICATE=0x14B
+
+cleanup() {
+    rm -f primary.ctx \
+          new_parent.prv new_parent.pub new_parent.ctx \
+          policy.dat session.dat \
+          key.prv key.pub key.ctx \
+          duppriv.bin dupseed.dat \
+          key2.prv key2.pub key2.ctx \
+          sym_key_in.bin
+
+    if [ "$1" != "no-shut-down" ]; then
+          shut_down
+    fi
+}
+trap cleanup EXIT
+
+start_up
+
+create_duplication_policy() {
+    tpm2_startauthsession -Q -S session.dat
+    tpm2_policycommandcode -Q -S session.dat -o policy.dat $TPM_CC_DUPLICATE
+    tpm2_flushcontext -Q -S session.dat
+    rm session.dat
+}
+
+start_duplication_session() {
+    tpm2_startauthsession -Q --policy-session -S session.dat
+    tpm2_policycommandcode -Q -S session.dat -o policy.dat $TPM_CC_DUPLICATE
+}
+
+end_duplication_session() {
+    tpm2_flushcontext -Q -S session.dat
+    rm session.dat
+}
+
+dump_duplication_session() {
+    rm session.dat
+}
+
+dd if=/dev/urandom of=sym_key_in.bin bs=1 count=16 status=none
+
+tpm2_createprimary -Q -a o -g sha256 -G rsa -o primary.ctx
+
+# Create a new parent, we will only use the public portion
+tpm2_create -Q -C primary.ctx -g sha256 -G rsa -r new_parent.prv -u new_parent.pub -b "decrypt|fixedparent|fixedtpm|restricted|sensitivedataorigin"
+
+# Create the key we want to duplicate
+create_duplication_policy
+tpm2_create -Q -C primary.ctx -g sha256 -G rsa -r key.prv -u key.pub -L policy.dat -b "sensitivedataorigin|sign|decrypt"
+tpm2_load -Q -C primary.ctx -r key.prv -u key.pub -o key.ctx
+
+tpm2_loadexternal -Q -a o -u new_parent.pub -o new_parent.ctx
+
+## Null parent, Null Sym Alg
+start_duplication_session
+tpm2_duplicate -Q -C null -c key.ctx -g null -p "session:session.dat" -r dupprv.bin -s dupseed.dat
+end_duplication_session
+
+## Null Sym Alg
+start_duplication_session
+tpm2_duplicate -Q -C new_parent.ctx -c key.ctx -g null -p "session:session.dat" -r dupprv.bin -s dupseed.dat
+end_duplication_session
+
+## Null parent
+start_duplication_session
+tpm2_duplicate -Q -C null -c key.ctx -g aes -i sym_key_in.bin -p "session:session.dat" -r dupprv.bin -s dupseed.dat
+end_duplication_session
+
+## AES Sym Alg, user supplied key
+start_duplication_session
+tpm2_duplicate -Q -C new_parent.ctx -c key.ctx -g aes -i sym_key_in.bin -p "session:session.dat" -r dupprv.bin -s dupseed.dat
+end_duplication_session
+
+## AES Sym Alg, no user supplied key
+start_duplication_session
+tpm2_duplicate -Q -C new_parent.ctx -c key.ctx -g aes -o sym_key_out.bin -p "session:session.dat" -r dupprv.bin -s dupseed.dat
+end_duplication_session
+
+## Repeat the tests with a key that requires encrypted duplication
+tpm2_create -Q -C primary.ctx -g sha256 -G rsa -r key2.prv -u key2.pub -L policy.dat -b "sensitivedataorigin|sign|decrypt|encryptedduplication"
+tpm2_load -Q -C primary.ctx -r key2.prv -u key2.pub -o key2.ctx
+
+## AES Sym Alg, user supplied key
+start_duplication_session
+tpm2_duplicate -Q -C new_parent.ctx -c key2.ctx -g aes -i sym_key_in.bin -p "session:session.dat" -r dupprv.bin -s dupseed.dat
+end_duplication_session
+
+## AES Sym Alg, no user supplied key
+start_duplication_session
+tpm2_duplicate -Q -C new_parent.ctx -c key2.ctx -g aes -o sym_key_out.bin -p "session:session.dat" -r dupprv.bin -s dupseed.dat
+end_duplication_session
+
+trap - ERR
+
+## Null parent - should fail (TPM_RC_HIERARCHY)
+start_duplication_session
+tpm2_duplicate -Q -C null -c key2.ctx -g aes -i sym_key_in.bin -p "session:session.dat" -r dupprv.bin -s dupseed.dat
+if [ $? -eq 0 ]; then
+  echo "Expected \"tpm2_duplicate -C null \" to fail."
+  exit 1
+fi
+dump_duplication_session
+
+## Null Sym Alg - should fail (TPM_RC_SYMMETRIC)
+start_duplication_session
+tpm2_duplicate -Q -C new_parent.ctx -c key2.ctx -g null -p "session:session.dat" -r dupprv.bin -s dupseed.dat
+if [ $? -eq 0 ]; then
+  echo "Expected \"tpm2_duplicate -g null \" to fail."
+  exit 1
+fi
+dump_duplication_session
+
+exit 0
