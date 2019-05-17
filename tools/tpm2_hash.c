6b75596be005b4904cb9f7cfd80fb5ce0a61471e
Remus Wu <remuswu1019@gmail.com>
2019-05-17 09:42:51 +0000

    tpm2_hash: Fix option typo
    Signed-off-by: Remus Wu <remuswu1019@gmail.com>

--- tools/tpm2_hash.c
+++ tools/tpm2_hash.c
@@ -175,7 +175,7 @@ static bool on_option(char key, char *value) {
 bool tpm2_tool_onstart(tpm2_options **opts) {
 
     static struct option topts[] = {
-        {"hierachy", required_argument, NULL, 'H'},
+        {"hierarchy", required_argument, NULL, 'H'},
         {"halg",     required_argument, NULL, 'g'},
         {"outfile",  required_argument, NULL, 'o'},
         {"ticket",   required_argument, NULL, 't'},
