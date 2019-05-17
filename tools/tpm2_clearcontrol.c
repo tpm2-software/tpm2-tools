5dc2dd2fe002946af10f55af79525af3daf9cf44
Imran Desai <imran.desai@intel.com>
2019-05-17 10:18:08 -0700

    tpm2_clearcontrol: Change tool options to accurately reflect spec language
    Purpose of the tool is to set/clear the TPMA_PERMANENT.disableClear attribute.
    Only platform-hierarchy or lockout permanent handles can operate with the tool.
    While both platform and lockout can set the attribute, only platform can clear.
    
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- tools/tpm2_clearcontrol.c
+++ tools/tpm2_clearcontrol.c
@@ -2,6 +2,7 @@
 
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "log.h"
 #include "tpm2_auth_util.h"
@@ -11,9 +12,11 @@
 
 typedef struct clearcontrol_ctx clearcontrol_ctx;
 struct clearcontrol_ctx {
+    TPMI_YES_NO disable_clear;
+    bool set;
     bool clear;
-    bool platform;
-
+    bool specifyhandle;
+    ESYS_TR rh;
     struct {
         char *auth_str;
         tpm2_session *session;
@@ -24,17 +27,14 @@ static clearcontrol_ctx ctx;
 
 static bool clearcontrol(ESYS_CONTEXT *ectx) {
 
-    ESYS_TR rh = ctx.platform ? ESYS_TR_RH_PLATFORM : ESYS_TR_RH_LOCKOUT;
-    TPMI_YES_NO disable = ctx.clear ? 0 : 1;
+    LOG_WARN ("Sending TPM2_ClearControl(%s) disableClear command with auth handle %s",
+            ctx.disable_clear ? "SET" : "CLEAR",
+            ctx.rh == ESYS_TR_RH_PLATFORM ? "TPM2_RH_PLATFORM" : "TPM2_RH_LOCKOUT");
 
-    LOG_INFO ("Sending TPM2_ClearControl(%s) command on %s",
-            ctx.clear ? "CLEAR" : "SET",
-            ctx.platform ? "TPM2_RH_PLATFORM" : "TPM2_RH_LOCKOUT");
+    ESYS_TR shandle = tpm2_auth_util_get_shandle(ectx, ctx.rh, ctx.auth.session);
 
-    ESYS_TR shandle = tpm2_auth_util_get_shandle(ectx, rh, ctx.auth.session);
-
-    TSS2_RC rval = Esys_ClearControl(ectx, rh,
-                shandle, ESYS_TR_NONE, ESYS_TR_NONE, disable);
+    TSS2_RC rval = Esys_ClearControl(ectx, ctx.rh,
+                shandle, ESYS_TR_NONE, ESYS_TR_NONE, ctx.disable_clear);
     if (rval != TPM2_RC_SUCCESS && rval != TPM2_RC_INITIALIZE) {
         LOG_PERR(Esys_ClearControl, rval);
         return false;
@@ -44,16 +44,63 @@ static bool clearcontrol(ESYS_CONTEXT *ectx) {
     return true;
 }
 
+static bool set_clearcontrol_auth_handle(const char *value) {
+
+    if (!strcmp(value, "p")) {
+        ctx.rh = ESYS_TR_RH_PLATFORM;
+        return true;
+    }
+
+    if (!strcmp(value, "l")) {
+        ctx.rh = ESYS_TR_RH_LOCKOUT;
+        return true;
+    }
+
+    LOG_ERR("Unknown or unsupported auth handle string."
+        " Specify p(latform)|l(ockout)");
+    return false;
+}
+
+static bool sanitize_input_arguments(void) {
+
+    if (!ctx.specifyhandle) {
+        ctx.rh = ESYS_TR_RH_LOCKOUT;
+    }
+
+    if (!ctx.set && !ctx.clear) {
+        LOG_ERR("Specify operation (set|clear)");
+        return false;
+    }
+
+    if (ctx.clear && ctx.rh == ESYS_TR_RH_LOCKOUT) {
+        LOG_ERR("Only platform hierarchy handle can be specified"
+            " for this operation");
+        return false;
+    }
+
+    return true;
+}
+
 static bool on_option(char key, char *value) {
 
+    bool result;
     switch (key) {
+    case 's':
+        ctx.disable_clear = 1;
+        ctx.set = true;
+        break;
     case 'c':
+        ctx.disable_clear = 0;
         ctx.clear = true;
         break;
-    case 'p':
-        ctx.platform = true;
+    case 'a':
+        ctx.specifyhandle = true;
+        result = set_clearcontrol_auth_handle(value);
+        if (!result) {
+            return false;
+        }
         break;
-    case 'L':
+    case 'p':
         ctx.auth.auth_str = value;
         break;
     }
@@ -64,12 +111,13 @@ static bool on_option(char key, char *value) {
 bool tpm2_tool_onstart(tpm2_options **opts) {
 
     const struct option topts[] = {
+        { "set",            no_argument,       NULL, 's' },
         { "clear",          no_argument,       NULL, 'c' },
-        { "auth-lockout",   required_argument, NULL, 'L' },
-        { "platform",       no_argument,       NULL, 'p' },
+        { "auth-handle",    required_argument, NULL, 'a' },
+        { "auth",           required_argument, NULL, 'p' },
     };
 
-    *opts = tpm2_options_new("cL:p", ARRAY_LEN(topts), topts,
+    *opts = tpm2_options_new("sca:p:", ARRAY_LEN(topts), topts,
                              on_option, NULL, 0);
 
     return *opts != NULL;
@@ -82,8 +130,13 @@ int tpm2_tool_onrun(ESYS_CONTEXT *ectx, tpm2_option_flags flags) {
     bool res = tpm2_auth_util_from_optarg(ectx, ctx.auth.auth_str,
             &ctx.auth.session, true);
     if (!res) {
-        LOG_ERR("Invalid lockout authorization, got\"%s\"",
+        LOG_ERR("Invalid authorization, got\"%s\"",
             ctx.auth.auth_str);
+        return 1;
+    }
+
+    res = sanitize_input_arguments();
+    if (!res) {
         return 1;
     }
 
