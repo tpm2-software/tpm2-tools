6288af0f2266e22b2f6cfc9ef88abe15acc219f3
joholl <johannes.holland@infineon.com>
2019-06-14 17:15:58 +0200

    tests: rework toggle_options.sh
    Summary of changes:
     * added verbosity to the test toggle_options.sh
     * options in the code must now be referenced on the corresponding man page
     * options on the man pages have to exist in the code
     * only consider .c files for code
    
    Signed-off-by: joholl <johannes.holland@infineon.com>

--- test/integration/tests/toggle_options.sh
+++ test/integration/tests/toggle_options.sh
@@ -8,18 +8,14 @@ trap - EXIT
 
 srcdir="$(readlink -e "$(dirname "$0")")"
 toolsdir="$(readlink -e "${srcdir}"/../../../tools)"
+mandir="$(readlink -e "${srcdir}"/../../../man)"
+
 
 # Provide a sanitizing test on whether a toggle
 # is effectively taken into account on option parsing.
+# Also, checks if the options are documented in the
+# respective man file consistently.
 # Functionnal check is left for dedicated test cases.
-#
-# On failure, the function will print the information
-# linked : toggle, filename, getopts string and case
-#
-# Line header help in guessing what is wrong :
-#   - <Y,!> : Getopt string is OK but no "case" was found
-#   - <!,Y> : Getopt string do not contain toggle but "case" exist
-#   - <!,!> : Option is declared but not used neither by "case" or "getopt"
 #
 # It assumes that the layout for describing toggles and
 # option is coherent among the tools
@@ -31,47 +27,89 @@ function check_toggle() {
         exit 254
     fi
 
-    for i in $(grep -R "'${toggle}'[[:space:]]*}" | sed "s%[[:space:]]*%%g"); do
-        optionnotfound=0
-        casenotfound=0
+    for i in $(grep -R "'${toggle}'[[:space:]]*}" --include=*.c | sed "s%[[:space:]]*%%g"); do
+        # An example:
+        # i:           tools/tpm2_nvdefine.c:{"hierarchy",required_argument,NULL,'a'},
+        # filename:    tools/tpm2_nvdefine.c
+        # match:       {"hierarchy",required_argument,NULL,'a'},
+        # option:      a
+        # option_long: hierarchy
+        # optionlist:  "x:a:s:b:P:p:L:"
+        # getcase:     case'a':
 
         filename=${i%%:*};
         match=${i##*:};
         option="$(sed -r "s%.*'([^'])'.*%\1%g" <<< "${match}")"
+        option_long="$(grep -oP '(?<={").*(?=")' <<< "${match}")"
         optionlist="$(grep -R "tpm2_options_new" "${filename}" | sed -r 's%.*("[^"]+").*%\1%g')"
         getcase="$(grep "case '${option}'" "${filename}" | sed "s%[[:space:]]*%%g")"
+
+        echo "filename: $filename"
+        echo "    match:        $match"
+        echo "    option:       $option"
+        echo "    option_long:  $option_long"
+        echo "    optionlist:   $optionlist"
+        echo "    getcase:      $getcase"
 
         if [[ "${filename}" =~ tpm2_options.c$ ]]; then
             continue
         fi
 
         if ! grep -q "${option}" <<< "${optionlist}"; then
-            optionnotfound=1
-            echo -n "<!,"
+            echo "$filename: option -$option (--$option_long) not found in option list $optionlist"
+            exit 1
         fi
 
         if ! test -n "${getcase}"; then
-            casenotfound=1
-            echo -n "!> : "
+            echo "$filename: switch case '$option' not found for option -$option (--$option_long)"
+            exit 1
         fi
 
-        if [ ${casenotfound} -eq 0 ] && [ ${optionnotfound} -ne 0 ]; then
-            echo -n "Y> : "
-        fi
+        ####################### check man page #######################
+        man_filename="$(basename $filename)"                 # tpm2_nvdefine.c
+        man_filename="$mandir/${man_filename%.*}.1.md"       # man/tpm2_nvdefine.1.md
+        man=$(cat "$man_filename")
 
-        if [ ${casenotfound} -ne 0 ] || [ ${optionnotfound} -ne 0 ]; then
-            fail=1
-            echo "${option} : ${filename} : ${match} : ${optionlist} : ${getcase%%:*}"
-            echo "------"
+        # resolve markdown includes
+        man_resolved="$man"
+        for md_include in $(grep -Po '(?<=\]\()common/.*(?=\))' <<< "$man"); do
+            man_resolved="$man_resolved $(cat $mandir/$md_include)"
+        done
+
+        # search markdown for option (short and long)
+        man_opt=$(grep -oe "\*\*-$option\*\*, \*\*\\\--$option_long\*\*" <<< "$man_resolved") || true
+
+        if [ -n "$man_opt" ]; then
+            echo "    man_opt:      $man_opt"
+        else
+            echo "$filename: missing option -$option/--$option_long in $man_filename"
+            exit 1
         fi
     done
 }
 
 fail=0
 
-# For each detected option toggle, check if it is actually declared to be used
+# For each detected option toggle, check if it is actually declared to be used and documented
 for i in $(grep -rn "case '.'" "${toolsdir}"/*.c | cut -d"'" -f2-2 | sort | uniq); do
     check_toggle "${i}"
+done
+
+# For each documented option toggle in the man pages, look if it is present in the code
+for j in $(grep -roe "\*\*-.\*\*, \*\*\\\--.*\*\*" --include=*.1.md | sed -r 's/\s//g' ); do
+    filename=${j%%:*};
+    option="$(grep -oP '(?<=\*\*-).(?=\*\*)' <<< "$j")"
+    option_long="$(grep -oP '(?<=\*\*\\\--).*(?=\*\*)' <<< "$j")"
+
+    c_filename=$(basename ${filename%.1.md}).c
+
+    echo "$filename: looking for -$option (--$option_long) in $c_filename"
+    found=$(grep -r "case '${option}'" "$toolsdir" --include="$c_filename") || true
+
+    if [ -z "$found" ]; then
+        echo "$filename: missing option -$option (--$option_long) in $c_filename"
+        exit 1
+    fi
 done
 
 exit $fail
