aafb78d3b13a82c609b9977ac4ff2c30fec19cb1
Imran Desai <imranodesai@gmail.com>
2019-06-17 11:27:27 -0700

    tpm2_nvread: refactor object and session interface
    Signed-off-by: Imran Desai <imranodesai@gmail.com>

--- tools/tpm2_nvread.c
+++ tools/tpm2_nvread.c
@@ -22,32 +22,29 @@
 
 typedef struct tpm_nvread_ctx tpm_nvread_ctx;
 struct tpm_nvread_ctx {
+    struct {
+        const char *ctx_path;
+        const char *auth_str;
+        tpm2_loaded_object object;
+    } auth_hierarchy;
+
     TPM2_HANDLE nv_index;
-    TPMI_RH_PROVISION hierarchy;
 
     UINT32 size_to_read;
     UINT32 offset;
-    struct {
-        char *auth_str;
-        tpm2_session *session;
-    } auth;
     char *output_file;
-    struct {
-        UINT8 a : 1;
-    } flags;
 };
 
-static tpm_nvread_ctx ctx = {
-    .hierarchy = TPM2_RH_OWNER
-};
+static tpm_nvread_ctx ctx;
 
 static tool_rc nv_read(ESYS_CONTEXT *ectx, tpm2_option_flags flags) {
 
     UINT8* data_buffer = NULL;
     UINT16 bytes_written;
     tool_rc rc = tpm2_util_nv_read(ectx, ctx.nv_index, ctx.size_to_read,
-                    ctx.offset, ctx.hierarchy, ctx.auth.session,
-                    &data_buffer, &bytes_written);
+                    ctx.offset, ctx.auth_hierarchy.object.handle,
+                    ctx.auth_hierarchy.object.session, &data_buffer,
+                    &bytes_written);
     if (rc != tool_rc_success) {
         goto out;
     }
@@ -91,20 +88,20 @@ static bool on_option(char key, char *value) {
             LOG_ERR("NV Index cannot be 0");
             return false;
         }
+        /*
+         * If the users doesn't specify an authorization hierarchy use the index
+         * passed to -x/--index for the authorization index.
+         */
+        ctx.auth_hierarchy.ctx_path = value;
         break;
     case 'a':
-        result = tpm2_hierarchy_from_optarg(value, &ctx.hierarchy,
-                TPM2_HIERARCHY_FLAGS_O|TPM2_HIERARCHY_FLAGS_P);
-        if (!result) {
-            return false;
-        }
-        ctx.flags.a = 1;
+        ctx.auth_hierarchy.ctx_path = value;
         break;
     case 'o':
         ctx.output_file = value;
         break;
     case 'P':
-        ctx.auth.auth_str = value;
+        ctx.auth_hierarchy.auth_str = value;
         break;
     case 's':
         result = tpm2_util_string_to_uint32(value, &ctx.size_to_read);
@@ -148,18 +145,11 @@ tool_rc tpm2_tool_onrun(ESYS_CONTEXT *ectx, tpm2_option_flags flags) {
 
     UNUSED(flags);
 
-    /* If the users doesn't specify an authorisation hierarchy use the index
-     * passed to -x/--index for the authorisation index.
-     */
-    if (!ctx.flags.a) {
-        ctx.hierarchy = ctx.nv_index;
-    }
-
-    tool_rc rc = tpm2_auth_util_from_optarg(ectx, ctx.auth.auth_str,
-            &ctx.auth.session, false);
+    tool_rc rc = tpm2_util_object_load_auth(ectx, ctx.auth_hierarchy.ctx_path,
+        ctx.auth_hierarchy.auth_str, &ctx.auth_hierarchy.object, false);
     if (rc != tool_rc_success) {
         LOG_ERR("Invalid handle authorization, got \"%s\"",
-            ctx.auth.auth_str);
+            ctx.auth_hierarchy.auth_str);
         return rc;
     }
 
@@ -168,5 +158,5 @@ tool_rc tpm2_tool_onrun(ESYS_CONTEXT *ectx, tpm2_option_flags flags) {
 
 tool_rc tpm2_tool_onstop(ESYS_CONTEXT *ectx) {
     UNUSED(ectx);
-    return tpm2_session_close(&ctx.auth.session);
+    return tpm2_session_close(&ctx.auth_hierarchy.object.session);
 }
