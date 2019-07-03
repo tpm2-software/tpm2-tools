cfe1a3ab702f9220daf96cf2f25a265e5caa8bd3
Imran Desai <imranodesai@gmail.com>
2019-07-03 10:10:15 -0700

    tpm2_clear: Add a test for evaluating tpm2_clear implementation
    progresses #1042
    
    Signed-off-by: Imran Desai <imranodesai@gmail.com>

---
+++ test/integration/tests/clear.sh
@@ -0,0 +1,31 @@
+# SPDX-License-Identifier: BSD-3-Clause
+
+source helpers.sh
+
+cleanup() {
+
+    shut_down
+}
+trap cleanup EXIT
+
+start_up
+
+lockPasswd=lockoutpass
+platPasswd=platformpass
+
+#Test tpm2_clear works with blank lockout auth as default
+tpm2_clear
+
+#Test tpm2_clear works with non-empy lockout auth as default
+tpm2_changeauth -c l $lockPasswd
+tpm2_clear $lockPasswd
+
+#Test tpm2_clear works with non-empy lockout auth and specified auth hierarchy
+tpm2_changeauth -c l $lockPasswd
+tpm2_clear -c l $lockPasswd
+
+#Test tpm2_clear works with non-empy platform auth and specified auth hierarchy
+tpm2_changeauth -c p $platPasswd
+tpm2_clear -c p $platPasswd
+
+exit 0
