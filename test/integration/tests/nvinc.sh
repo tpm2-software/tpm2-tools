c1dfc62bcd3d44a7f8ea25c855ccdd335df7bfd7
Imran Desai <imran.desai@intel.com>
2019-07-25 16:42:02 -0700

    tpm2_nvread: Change method of specifying NV index
    Make the cmdline option "-x" an argument.
    
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- test/integration/tests/nvinc.sh
+++ test/integration/tests/nvinc.sh
@@ -34,7 +34,7 @@ tpm2_nvdefine -Q   $nv_test_index -C o -s 8 -a "ownerread|policywrite|ownerwrite
 
 tpm2_nvincrement -Q   $nv_test_index -C o
 
-tpm2_nvread -Q -x $nv_test_index -C o -s 8
+tpm2_nvread -Q   $nv_test_index -C o -s 8
 
 tpm2_nvlist > nv.out
 yaml_get_kv nv.out "$nv_test_index" > /dev/null
@@ -48,7 +48,7 @@ echo -n -e '\x00\x00\x00\x00\x00\x00\x00\x02' > nv.test_inc
 
 tpm2_nvincrement -Q   $nv_test_index -C o
 
-tpm2_nvread -x $nv_test_index -C o -s 8 > cmp.dat
+tpm2_nvread   $nv_test_index -C o -s 8 > cmp.dat
 
 cmp nv.test_inc cmp.dat
 
@@ -67,13 +67,13 @@ echo -n -e '\x00\x00\x00\x00\x00\x00\x00\x03' > nv.test_inc
 # Counter is initialised to highest value previously seen (in this case 2) then incremented
 tpm2_nvincrement -Q   0x1500016 -C 0x1500016 -P pcr:${alg_pcr_policy}:${pcr_ids}+$file_pcr_value
 
-tpm2_nvread -x 0x1500016 -C 0x1500016 -P pcr:${alg_pcr_policy}:${pcr_ids}+$file_pcr_value -s 8 > cmp.dat
+tpm2_nvread   0x1500016 -C 0x1500016 -P pcr:${alg_pcr_policy}:${pcr_ids}+$file_pcr_value -s 8 > cmp.dat
 
 cmp nv.test_inc cmp.dat
 
 # this should fail because authread is not allowed
 trap - ERR
-tpm2_nvread -x 0x1500016 -C 0x1500016 -P "index" 2>/dev/null
+tpm2_nvread   0x1500016 -C 0x1500016 -P "index" 2>/dev/null
 trap onerror ERR
 
 tpm2_nvrelease -Q -x 0x1500016 -C 0x40000001
@@ -86,14 +86,14 @@ tpm2_nvdefine -Q   $nv_test_index -C o -s 8 -a "ownerread|policywrite|ownerwrite
 
 tpm2_nvincrement -Q   $nv_test_index -C o
 
-tpm2_nvread -Q -x $nv_test_index -C o -s 8
+tpm2_nvread -Q   $nv_test_index -C o -s 8
 
 tpm2_nvreadlock -Q -x $nv_test_index -C o
 
 # Reset ERR signal handler to test for expected nvread error
 trap - ERR
 
-tpm2_nvread -Q -x $nv_test_index -C o -s 8 2> /dev/null
+tpm2_nvread -Q   $nv_test_index -C o -s 8 2> /dev/null
 if [ $? != 1 ];then
  echo "nvread didn't fail!"
  exit 1
@@ -118,15 +118,15 @@ tpm2_nvdefine   0x1500015 -C 0x40000001 -s 8 \
 
 # Use index password write/read, implicit -C
 tpm2_nvincrement -Q   0x1500015 -P "index"
-tpm2_nvread -Q -x 0x1500015 -P "index"
+tpm2_nvread -Q   0x1500015 -P "index"
 
 # Use index password write/read, explicit -C
 tpm2_nvincrement -Q   0x1500015 -C 0x1500015 -P "index"
-tpm2_nvread -Q -x 0x1500015 -C 0x1500015 -P "index"
+tpm2_nvread -Q   0x1500015 -C 0x1500015 -P "index"
 
 # use owner password
 tpm2_nvincrement -Q   0x1500015 -C 0x40000001 -P "owner"
-tpm2_nvread -Q -x 0x1500015 -C 0x40000001 -P "owner"
+tpm2_nvread -Q   0x1500015 -C 0x40000001 -P "owner"
 
 # Check a bad password fails
 trap - ERR
