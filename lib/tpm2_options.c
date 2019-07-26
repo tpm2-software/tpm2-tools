tpm2-software/tpm2-tools

commit 56710b6e1c4d3e02ff77ad367c216a59b51ca765

lib/tpm2_options: remove -X option
The -X option was introduced as a global option, which presented some
issues, notably:
1. Possible NULL pointer dereferences on tools unaware of a possible
   NULL ESAPI.
2. Globaly reserved option for many tools that don't need it.

To correct this, make a new ESAPI flag for optional SAPI initialization
and couple it with a new TCTI called "none". Tools that have this
optional SAPI flag set, will also support --tcti=none. Tools that don't
support the "none" tcti, will throw an error. Update the makecredential
test which was using -X and and a negative test to getrandom to make
sure the none tcti fails properly when a tool requires a SAPI.

Fixes: #1442

Signed-off-by: William Roberts <william.c.roberts@intel.com>

lib/tpm2_options.c
@@ -170,20 +170,31 @@ static const char *find_default_tcti(void) {
     return NULL;
 }
 
-static tcti_conf tcti_get_config(const char *optstr) {
+static tcti_conf tcti_get_default_conf(void) {
 
-    /* set up the default configuration */
     tcti_conf conf = {
         .name = find_default_tcti()
     };
+
+    return conf;
+}
+
+static tcti_conf tcti_get_config(const char *optstr) {
+
+    /* set up the default configuration */
+    tcti_conf conf = { 0 };
 
     /* no tcti config supplied, get it from env */
     if (!optstr) {
         optstr = getenv (TPM2TOOLS_ENV_TCTI);
         if (!optstr) {
             /* nothing user supplied, use default */
-            return conf;
+            return tcti_get_default_conf();
         }
+    }
+
+    if (!strcmp(optstr, "none")) {
+        return conf;
     }
 
     char *split = strchr(optstr, ':');
@@ -205,11 +216,12 @@ static tcti_conf tcti_get_config(const char *optstr) {
 
     /* Case A */
     if (!optstr[0] && !split[1]) {
-        return conf;
+        return tcti_get_default_conf();
     }
 
     /* Case B */
     if (!optstr[0]) {
+        conf = tcti_get_default_conf();
         conf.opts = &split[1];
         return conf;
     }
@@ -332,7 +344,7 @@ tpm2_option_code tpm2_handle_options (int argc, char **argv,
      * grep -rn case\ \'[a-zA-Z]\' | awk '{print $3}' | sed s/\'//g | sed s/\://g | sort | uniq | less
      */
     struct option long_options [] = {
-        { "tcti",          required_argument, NULL, 'T' },
+        { "tcti",          optional_argument, NULL, 'T' },
         { "help",          optional_argument, NULL, 'h' },
         { "verbose",       no_argument,       NULL, 'V' },
         { "quiet",         no_argument,       NULL, 'Q' },
@@ -405,12 +417,6 @@ tpm2_option_code tpm2_handle_options (int argc, char **argv,
         case 'Z':
             flags->enable_errata = 1;
             break;
-        case 'X':
-            if (tool_opts) {
-                flags->no_tpm = 1;
-                tool_opts->flags |= TPM2_OPTIONS_NO_SAPI;
-            }
-            break;
         case '?':
             goto out;
         default:
@@ -444,20 +450,33 @@ tpm2_option_code tpm2_handle_options (int argc, char **argv,
     }
 
     /* Only init a TCTI if the tool needs it and if the -h/--help option isn't present */
-    if (!show_help){
-      if (!tool_opts || !(tool_opts->flags & TPM2_OPTIONS_NO_SAPI)) {
-          tcti_conf conf = tcti_get_config(tcti_conf_option);
+    if (!show_help) {
 
-          *tcti = tpm2_tcti_ldr_load(conf.name, conf.opts);
-          if (!*tcti) {
-              LOG_ERR("Could not load tcti, got: \"%s\"", conf.name);
-              goto out;
-          }
+        /* tool doesn't request a sapi, don't initialize one */
+        if (!tool_opts || !(tool_opts->flags & TPM2_OPTIONS_NO_SAPI)) {
 
-          if (!flags->enable_errata) {
-              flags->enable_errata = !!getenv (TPM2TOOLS_ENV_ENABLE_ERRATA);
-          }
-      }
+            tcti_conf conf = tcti_get_config(tcti_conf_option);
+
+            /* name can be NULL for optional SAPI tools */
+            if (conf.name) {
+                *tcti = tpm2_tcti_ldr_load(conf.name, conf.opts);
+                if (!*tcti) {
+                  LOG_ERR("Could not load tcti, got: \"%s\"", conf.name);
+                  goto out;
+                }
+
+                if (!flags->enable_errata) {
+                  flags->enable_errata = !!getenv (TPM2TOOLS_ENV_ENABLE_ERRATA);
+                }
+                /*
+                 * no loader requested ie --tcti=none is an error if tool
+                 * doesn't indicate an optional SAPI
+                 */
+            } else if (!tool_opts || !(tool_opts->flags & TPM2_OPTIONS_OPTIONAL_SAPI)) {
+                LOG_ERR("Requested no tcti, but tool requires TCTI.");
+                goto out;
+            }
+        }
     }
 
     rc = tpm2_option_code_continue;

lib/tpm2_options.h
@@ -102,6 +102,7 @@ typedef bool (*tpm2_arg_handler)(int argc, char **argv);
  *  Skip SAPI initialization. Removes the "-T" common option.
  */
 #define TPM2_OPTIONS_NO_SAPI 0x1
+#define TPM2_OPTIONS_OPTIONAL_SAPI 0x2
 
 struct tpm2_options {
     struct {

test/integration/tests/getrandom.sh
@@ -64,4 +64,12 @@ if [ $? -eq 0 ]; then
     exit 1
 fi
 
+# verify that tpm2_getrandom requires a TCTI
+./tools/tpm2_listpersistent -T none &> /dev/null
+if [ $? -eq 0 ]; then
+    echo "tpm2_getrandom should fail with tcti: \"none\""
+    exit 1
+fi
+
+
 exit 0

test/integration/tests/makecredential.sh
@@ -75,6 +75,7 @@ Loadkeyname=`cat $output_ak_pub_name | xxd -p -c $file_size`
 
 tpm2_makecredential -Q -e $output_ek_pub  -s $file_input_data  -n $Loadkeyname -o $output_mkcredential
 
-tpm2_makecredential -Q -e $output_ek_pub  -s $file_input_data  -n $Loadkeyname -o $output_mkcredential --openssl-backend
+# use no tpm backend
+tpm2_makecredential -T none -Q -e $output_ek_pub  -s $file_input_data  -n $Loadkeyname -o $output_mkcredential
 
 exit 0

tools/tpm2_makecredential.c
@@ -287,7 +287,7 @@ bool tpm2_tool_onstart(tpm2_options **opts) {
     };
 
     *opts = tpm2_options_new("e:s:n:o:", ARRAY_LEN(topts), topts, on_option,
-                             NULL, 0);
+                             NULL, TPM2_OPTIONS_OPTIONAL_SAPI);
 
     return *opts != NULL;
 }
@@ -301,10 +301,11 @@ int tpm2_tool_onrun(ESYS_CONTEXT *ectx, tpm2_option_flags flags) {
         return -11;
     }
 
-    // Run it outside of a TPM
-    if (flags.no_tpm) {
-        return make_external_credential_and_save() != true;
-    }
+    printf("make credential has ESAPI CTX: %p", ectx);
 
-    return make_credential_and_save(ectx) != true;
+    // Run it outside of a TPM
+    bool result = ectx ? make_credential_and_save(ectx) :
+            make_external_credential_and_save();
+
+    return result != true;
 }

commit 02db8f4679577724397ac6ea2a5eac2ed18ac981

tcti: allow specifying long option --tcti without equals sign
If 'optional_argument' is used instead of 'required_argument', only the
syntax '--tcti=<value>' is accepted, but not '--tcti <value>'. Since
this does not match the definition of the short option as required in
'common_short_opts' and using --tcti without an argument does not make
sense anyway, we use required_argument and add a unit test for the case
without equals sign.

Fixes: 56710b6e1c4d3e02ff77ad367c216a59b51ca765
Signed-off-by: Jonas Witschel <diabonas@gmx.de>

lib/tpm2_options.c
@@ -218,7 +218,7 @@ tpm2_option_code tpm2_handle_options (int argc, char **argv,
      * grep -rn case\ \'[a-zA-Z]\' | awk '{print $3}' | sed s/\'//g | sed s/\://g | sort | uniq | less
      */
     struct option long_options [] = {
-        { "tcti",          optional_argument, NULL, 'T' },
+        { "tcti",          required_argument, NULL, 'T' },
         { "help",          optional_argument, NULL, 'h' },
         { "verbose",       no_argument,       NULL, 'V' },
         { "quiet",         no_argument,       NULL, 'Q' },

test/unit/test_options.c
@@ -152,12 +152,40 @@ static void test_tcti_short_option_no_errata(void **state) {
     assert_int_equal(oc, tpm2_option_code_continue);
 }
 
-static void test_tcti_long_option_no_errata(void **state) {
+static void test_tcti_long_option_with_equals_no_errata(void **state) {
     UNUSED(state);
 
     char *argv[] = {
         "program",
         "--tcti=tctifake",    // Set TCTI to something specific
+        "-Z"                  // Disable errata getenv call
+    };
+
+    int argc = ARRAY_LEN(argv);
+
+    tpm2_options *tool_opts = NULL;
+    tpm2_option_flags flags = { .all = 0 };
+    TSS2_TCTI_CONTEXT *tcti = NULL;
+
+    /* we never call getenv() because we use -T */
+    /* we never probe for a tcti */
+    /* we just use what is given, in this case, return a mocked instance */
+    will_return(__wrap_Tss2_TctiLdr_Initialize, TSS2_RC_SUCCESS);
+    will_return(__wrap_Tss2_TctiLdr_Initialize, &tcti_instance);
+
+    tpm2_option_code oc = tpm2_handle_options(argc, argv, tool_opts, &flags,
+            &tcti);
+    assert_non_null(tcti);
+    assert_int_equal(oc, tpm2_option_code_continue);
+}
+
+static void test_tcti_long_option_no_equals_no_errata(void **state) {
+    UNUSED(state);
+
+    char *argv[] = {
+        "program",
+        "--tcti",             // Set TCTI to something specific
+        "tctifake",
         "-Z"                  // Disable errata getenv call
     };
 
@@ -217,7 +245,8 @@ int main(int argc, char *argv[]) {
             cmocka_unit_test(test_null_tcti_getenv_no_errata),
             cmocka_unit_test(test_null_tcti_getenv_with_errata),
             cmocka_unit_test(test_tcti_short_option_no_errata),
-            cmocka_unit_test(test_tcti_long_option_no_errata),
+            cmocka_unit_test(test_tcti_long_option_no_equals_no_errata),
+            cmocka_unit_test(test_tcti_long_option_with_equals_no_errata),
             cmocka_unit_test(test_invalid_tcti_no_errata),
     };
 