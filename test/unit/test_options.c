tpm2-software/tpm2-tools

commit 0505f2b3969e8ca2006dcfbb083d45a3b62bfb30

tcti: Replace tpm2_tcti_ldr with libtss2-tctildr.
This commit removes the tpm2_tcti_ldr module replacing it with the new
tctildr library from the tpm2-tss repo. This is a "drop in" replacement,
leaving the existing `--tcti` command line interface unchanged.

Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

lib/tpm2_options.c
@@ -10,6 +10,8 @@
 #include <libgen.h>
 #include <unistd.h>
 
+#include <tss2/tss2_tctildr.h>
+
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
@@ -17,7 +19,6 @@
 #include "config.h"
 #include "log.h"
 #include "tpm2_options.h"
-#include "tpm2_tcti_ldr.h"
 
 #ifndef VERSION
   #warning "VERSION Not known at compile time, not embedding..."
@@ -107,103 +108,10 @@ void tpm2_options_free(tpm2_options *opts) {
     free(opts->short_opts);
     free(opts);
 }
-typedef struct tcti_conf tcti_conf;
-struct tcti_conf {
-    const char *name;
-    const char *opts;
-};
 
 static inline const char *fixup_name(const char *name) {
 
     return !strcmp(name, "abrmd") ? "tabrmd" : name;
-}
-
-static const char *find_default_tcti(void) {
-
-    const char *defaults[] = {
-        "tabrmd",
-        "device",
-        "mssim"
-    };
-
-    size_t i;
-    for(i=0; i < ARRAY_LEN(defaults); i++) {
-        const char *name = defaults[i];
-        bool is_present = tpm2_tcti_ldr_is_tcti_present(name);
-        if (is_present) {
-            return name;
-        }
-    }
-
-    return NULL;
-}
-
-static tcti_conf tcti_get_default_conf(void) {
-
-    tcti_conf conf = {
-        .name = find_default_tcti()
-    };
-
-    return conf;
-}
-
-static tcti_conf tcti_get_config(const char *optstr) {
-
-    /* set up the default configuration */
-    tcti_conf conf = { 0 };
-
-    /* no tcti config supplied, get it from env */
-    if (!optstr) {
-        optstr = tpm2_util_getenv (TPM2TOOLS_ENV_TCTI);
-        if (!optstr) {
-            /* nothing user supplied, use default */
-            return tcti_get_default_conf();
-        }
-    }
-
-    if (!strcmp(optstr, "none")) {
-        return conf;
-    }
-
-    char *split = strchr(optstr, ':');
-    if (!split) {
-        /* --tcti=device */
-        conf.name = fixup_name(optstr);
-        return conf;
-    }
-
-    /*
-     * If it has a ":", it could be either one of the following:
-     * case A: --tcti=:               --> default name and default (null) config
-     * case B: --tcti=:/dev/foo       --> default name, custom config
-     * case C: --tcti=device:         --> custom name, default (null) config
-     * case D: --tcti=device:/dev/foo --> custom name, custom config
-     */
-
-    split[0] = '\0';
-
-    /* Case A */
-    if (!optstr[0] && !split[1]) {
-        return tcti_get_default_conf();
-    }
-
-    /* Case B */
-    if (!optstr[0]) {
-        conf = tcti_get_default_conf();
-        conf.opts = &split[1];
-        return conf;
-    }
-
-    /* Case C */
-    if (!split[1]) {
-        conf.name = fixup_name(optstr);
-        return conf;
-    }
-
-    /* Case D */
-    conf.name = fixup_name(optstr);
-    conf.opts = &split[1];
-    return conf;
 }
 
 static bool execute_man(char *prog_name, bool show_errors) {
@@ -242,20 +150,17 @@ static bool execute_man(char *prog_name, bool show_errors) {
 }
 
 static void show_version (const char *name) {
+    const char *tcti_default = NULL;
+    TSS2_TCTI_INFO *info = NULL;
 
-#ifdef DISABLE_DLCLOSE
-    char *dlconfig="disabled";
-#else
-    char *dlconfig="enabled";
-#endif
-
-    const char *tcti_default = find_default_tcti();
-    if (!tcti_default) {
-        tcti_default = "none";
+    TSS2_RC rc = Tss2_TctiLdr_GetInfo(NULL, &info);
+    if (rc == TSS2_RC_SUCCESS && info != NULL) {
+        tcti_default = info->name;
     }
 
-    printf("tool=\"%s\" version=\"%s\" tctis=\"dynamic\" tcti-default=%s dlclose=%s\n",
-            name, VERSION, tcti_default, dlconfig);
+    printf("tool=\"%s\" version=\"%s\" tctis=\"libtss2-tctildr\" tcti-default=%s\n",
+            name, VERSION, tcti_default);
+    Tss2_TctiLdr_FreeInfo(&info);
 }
 
 void tpm2_print_usage(const char *command, struct tpm2_options *tool_opts) {
@@ -425,30 +330,30 @@ tpm2_option_code tpm2_handle_options (int argc, char **argv,
         /* tool doesn't request a sapi, don't initialize one */
         if (!tool_opts || !(tool_opts->flags & TPM2_OPTIONS_NO_SAPI)) {
 
-            tcti_conf conf = tcti_get_config(tcti_conf_option);
-
-            /* name can be NULL for optional SAPI tools */
-            if (conf.name) {
-                *tcti = tpm2_tcti_ldr_load(conf.name, conf.opts);
-                if (!*tcti) {
-                  LOG_ERR("Could not load tcti, got: \"%s\"", conf.name);
-                  goto out;
+            if (tcti_conf_option == NULL)
+                tcti_conf_option = tpm2_util_getenv(TPM2TOOLS_ENV_TCTI);
+            else if (!strcmp(tcti_conf_option, "none")) {
+                if (!tool_opts || !(tool_opts->flags & TPM2_OPTIONS_OPTIONAL_SAPI)) {
+                    LOG_ERR("Requested no tcti, but tool requires TCTI.");
+                    goto out;
                 }
-
-                if (!flags->enable_errata) {
-                  flags->enable_errata = !!tpm2_util_getenv (TPM2TOOLS_ENV_ENABLE_ERRATA);
-                }
+                goto none;
+            }
+            rc = Tss2_TctiLdr_Initialize(tcti_conf_option, tcti);
+            if (rc != TSS2_RC_SUCCESS || !*tcti) {
+              LOG_ERR("Could not load tcti, got: \"%s\"", tcti_conf_option);
+              goto out;
+            }
                 /*
                  * no loader requested ie --tcti=none is an error if tool
                  * doesn't indicate an optional SAPI
                  */
-            } else if (!tool_opts || !(tool_opts->flags & TPM2_OPTIONS_OPTIONAL_SAPI)) {
-                LOG_ERR("Requested no tcti, but tool requires TCTI.");
-                goto out;
+            if (!flags->enable_errata) {
+                flags->enable_errata = !!tpm2_util_getenv (TPM2TOOLS_ENV_ENABLE_ERRATA);
             }
         }
     }
-
+none:
     rc = tpm2_option_code_continue;
 out:
 
@@ -471,11 +376,15 @@ out:
         if (!did_manpager) {
             tpm2_print_usage(argv[0], tool_opts);
         }
-
-        const TSS2_TCTI_INFO *info = tpm2_tcti_ldr_getinfo();
-        if (info) {
-            printf("\ntcti-help(%s): %s\n", info->name, info->config_help);
+        if (tcti_conf_option && strcmp(tcti_conf_option, "none")) {
+            TSS2_TCTI_INFO *info = NULL;
+            rc = Tss2_TctiLdr_GetInfo (tcti_conf_option, &info);
+            if (rc == TSS2_RC_SUCCESS && info) {
+                printf("\ntcti-help(%s): %s\n", info->name, info->config_help);
+            }
+            Tss2_TctiLdr_FreeInfo (&info);
         }
+
         rc = tpm2_option_code_stop;
     }
 

test/unit/test_options.c
@@ -8,7 +8,6 @@
 #include <cmocka.h>
 
 #include "tpm2_options.h"
-#include "tpm2_tcti_ldr.h"
 #include "tpm2_util.h"
 
 typedef struct test_pair test_pair;
@@ -25,39 +24,6 @@ struct test_pair {
     } output;
 };
 
-static void set_dlsym(void *handle, const char *symbol, void *ret) {
-
-    test_pair *e = calloc(1, sizeof(test_pair));
-    assert_non_null(e);
-
-    e->input[0].v = handle;
-    e->input[1].cn = symbol;
-    e->output.v = ret;
-
-    will_return(__wrap_tpm2_util_dlsym, e);
-}
-
-static void set_dlopen(const char *name, void *ret) {
-
-    test_pair *e = calloc(1, sizeof(test_pair));
-    assert_non_null(e);
-
-    e->input[0].cn = name;
-    e->output.v = ret;
-
-    will_return(__wrap_tpm2_util_dlopen, e);
-}
-
-static void set_dlclose(void *expected, int ret) {
-
-    test_pair *e = calloc(1, sizeof(test_pair));
-    assert_non_null(e);
-
-    e->input[0].v = expected;
-    e->output.i = ret;
-
-    will_return(__wrap_tpm2_util_dlclose, e);
-}
 
 static void set_getenv(const char *name, char *ret) {
 
@@ -69,37 +35,7 @@ static void set_getenv(const char *name, char *ret) {
 
     will_return(__wrap_tpm2_util_getenv, e);
 }
-
-void *__wrap_tpm2_util_dlopen(const char *filename, int flags) {
-    UNUSED(flags);
-
-    test_pair *x = mock_ptr_type(test_pair *);
-
-    const char *expected_filename = x->input[0].cn;
-    void *ret = x->output.v;
-    free(x);
-
-    assert_string_equal(filename, expected_filename);
-
-    return ret;
-}
-
 #define HANDLE_SKIP_CHECK ((void *) -1)
-
-int __wrap_tpm2_util_dlclose(void *handle) {
-
-    test_pair *x = mock_ptr_type(test_pair *);
-
-    void *expected_handle = x->input[0].v;
-    int ret = x->output.i;
-    free(x);
-    if (expected_handle != HANDLE_SKIP_CHECK) {
-        assert_ptr_equal(expected_handle, handle);
-    }
-
-    return ret;
-}
-
 char *__wrap_tpm2_util_getenv(const char *name) {
 
     test_pair *x = mock_ptr_type(test_pair *);
@@ -113,95 +49,20 @@ char *__wrap_tpm2_util_getenv(const char *name) {
     return ret;
 }
 
-void *__wrap_tpm2_util_dlsym(void *handle, const char *symbol) {
-
-    test_pair *x = mock_ptr_type(test_pair *);
-
-    void *expected_handle = x->input[0].v;
-    const char *expected_symbol = x->input[1].cn;
-    void *ret = x->output.v;
-    free(x);
-
-    assert_ptr_equal(handle, expected_handle);
-    assert_string_equal(symbol, expected_symbol);
-
-    return ret;
-}
-/*
- * implement a dummy TCTI
- */
-static TSS2_RC tcti_receive (TSS2_TCTI_CONTEXT *context,
-                   size_t *size,
-                   uint8_t *response,
-                   int32_t timeout) {
-    UNUSED(context);
-    UNUSED(size);
-    UNUSED(response);
-    UNUSED(timeout);
-
-    return TSS2_RC_SUCCESS;
-}
-
-TSS2_RC tcti_transmit (TSS2_TCTI_CONTEXT *context,
-                    size_t size,
-                    const uint8_t *command) {
-    UNUSED(context);
-    UNUSED(size);
-    UNUSED(command);
-
-    return TSS2_RC_SUCCESS;
-}
-
-void tcti_finalize (TSS2_TCTI_CONTEXT *context) {
-    UNUSED(context);
-}
-
-static TSS2_RC tcti_init(
-    TSS2_TCTI_CONTEXT *context,
-    size_t *size,
-    const char *config) {
-
-    UNUSED(config);
-
-    if (size == NULL) {
-        return TSS2_TCTI_RC_BAD_VALUE;
+TSS2_RC
+__wrap_Tss2_TctiLdr_Initialize (const char *nameConf,
+                                TSS2_TCTI_CONTEXT **context)
+{
+    UNUSED(nameConf);
+    printf ("fml\n");
+    TSS2_RC rc = mock_type(TSS2_RC);
+    if (rc == TSS2_RC_SUCCESS) {
+        *context = mock_type(TSS2_TCTI_CONTEXT*);
     }
-
-    if (context == NULL) {
-        *size = sizeof(TSS2_TCTI_CONTEXT_COMMON_V2);
-        return TSS2_RC_SUCCESS;
-    }
-
-    TSS2_TCTI_MAGIC (context) = 0x9886dc39e78df261;
-    TSS2_TCTI_VERSION (context) = 1;
-
-    TSS2_TCTI_TRANSMIT (context) = tcti_transmit;
-    TSS2_TCTI_RECEIVE (context) = tcti_receive;
-    TSS2_TCTI_FINALIZE (context) = tcti_finalize;
-    TSS2_TCTI_CANCEL (context) = NULL;
-    TSS2_TCTI_GET_POLL_HANDLES (context) = NULL;
-    TSS2_TCTI_SET_LOCALITY (context) = NULL;
-    TSS2_TCTI_MAKE_STICKY (context) = NULL;
-
-    return TSS2_RC_SUCCESS;
+    return rc;
 }
 
-static const TSS2_TCTI_INFO *tcti_info_func(void) {
-    static const TSS2_TCTI_INFO info = {
-       .config_help = "dummy config help",
-       .description = "dummy description",
-       .init = tcti_init,
-       .name = "dummy tcti",
-       .version = 0x42
-    };
-
-    return &info;
-}
-
-#define DLOPEN_HANDLE ((void *)0xDEADBEEF)
-#define DLSYM_HANDLE  ((void *)0xBADCC0DE)
-#define EXPECTED_DEFAULT_TCTI_SONAME "libtss2-tcti-tabrmd.so.0"
-#define EXPECTED_DEFAULT_TCTI_NAME "tabrmd"
+static TSS2_TCTI_CONTEXT_COMMON_V2 tcti_instance = { 0, };
 
 static void common_prelude(void) {
     /*
@@ -212,33 +73,9 @@ static void common_prelude(void) {
      * then results in us following the normal tcti loading logic.
      */
     set_getenv(TPM2TOOLS_ENV_TCTI, NULL);
-    set_dlopen(EXPECTED_DEFAULT_TCTI_SONAME, DLOPEN_HANDLE);
-    set_dlclose(DLOPEN_HANDLE, 0);
-
-    /*
-     * After this we try the raw string of tabrmd, which should fail
-     */
-    set_dlopen(EXPECTED_DEFAULT_TCTI_NAME, NULL);
-
-    /*
-     * After this we try the full name, which should work
-     */
-    set_dlopen(EXPECTED_DEFAULT_TCTI_SONAME, DLOPEN_HANDLE);
-
-    /*
-     * Now that we have an open library, we call dlsym to get the tcti info func
-     * which will return the dummy tcti
-     */
-    set_dlsym(DLOPEN_HANDLE, TSS2_TCTI_INFO_SYMBOL, tcti_info_func);
-}
-
-static int test_teardown(void **state) {
-    UNUSED(state);
-#ifndef DISABLE_DLCLOSE
-    set_dlclose(HANDLE_SKIP_CHECK, 0);
-#endif
-    tpm2_tcti_ldr_unload();
-    return 0;
+    /* mock Tss2_TctiLdr_Initialize */
+    will_return (__wrap_Tss2_TctiLdr_Initialize, TSS2_RC_SUCCESS);
+    will_return (__wrap_Tss2_TctiLdr_Initialize, &tcti_instance);
 }
 
 static void test_null_tcti_getenv_no_errata(void **state) {
@@ -261,7 +98,6 @@ static void test_null_tcti_getenv_no_errata(void **state) {
             &tcti);
     assert_non_null(tcti);
     assert_int_equal(oc, tpm2_option_code_continue);
-    free(tcti);
 }
 
 static void test_null_tcti_getenv_with_errata(void **state) {
@@ -286,7 +122,6 @@ static void test_null_tcti_getenv_with_errata(void **state) {
             &tcti);
     assert_non_null(tcti);
     assert_int_equal(oc, tpm2_option_code_continue);
-    free(tcti);
 }
 
 static void test_tcti_short_option_no_errata(void **state) {
@@ -307,20 +142,14 @@ static void test_tcti_short_option_no_errata(void **state) {
 
     /* we never call getenv() because we use -T */
     /* we never probe for a tcti */
-    /* we just use what is given, in this case, we allow it to dlopen */
-    set_dlopen("tctifake", DLOPEN_HANDLE);
-
-    /*
-     * Now that we have an open library, we call dlsym to get the tcti info func
-     * which will return the dummy tcti
-     */
-    set_dlsym(DLOPEN_HANDLE, TSS2_TCTI_INFO_SYMBOL, tcti_info_func);
+    /* we just use what is given, in this case, return a mocked instance */
+    will_return(__wrap_Tss2_TctiLdr_Initialize, TSS2_RC_SUCCESS);
+    will_return(__wrap_Tss2_TctiLdr_Initialize, &tcti_instance);
 
     tpm2_option_code oc = tpm2_handle_options(argc, argv, tool_opts, &flags,
             &tcti);
     assert_non_null(tcti);
     assert_int_equal(oc, tpm2_option_code_continue);
-    free(tcti);
 }
 
 static void test_tcti_long_option_no_errata(void **state) {
@@ -340,20 +169,14 @@ static void test_tcti_long_option_no_errata(void **state) {
 
     /* we never call getenv() because we use -T */
     /* we never probe for a tcti */
-    /* we just use what is given, in this case, we allow it to dlopen */
-    set_dlopen("tctifake", DLOPEN_HANDLE);
-
-    /*
-     * Now that we have an open library, we call dlsym to get the tcti info func
-     * which will return the dummy tcti
-     */
-    set_dlsym(DLOPEN_HANDLE, TSS2_TCTI_INFO_SYMBOL, tcti_info_func);
+    /* we just use what is given, in this case, return a mocked instance */
+    will_return(__wrap_Tss2_TctiLdr_Initialize, TSS2_RC_SUCCESS);
+    will_return(__wrap_Tss2_TctiLdr_Initialize, &tcti_instance);
 
     tpm2_option_code oc = tpm2_handle_options(argc, argv, tool_opts, &flags,
             &tcti);
     assert_non_null(tcti);
     assert_int_equal(oc, tpm2_option_code_continue);
-    free(tcti);
 }
 
 /*
@@ -367,14 +190,10 @@ int main(int argc, char *argv[]) {
     UNUSED(argv);
 
     const struct CMUnitTest tests[] = {
-            cmocka_unit_test_teardown(test_null_tcti_getenv_no_errata,
-                    test_teardown),
-            cmocka_unit_test_teardown(test_null_tcti_getenv_with_errata,
-                    test_teardown),
-            cmocka_unit_test_teardown(test_tcti_short_option_no_errata,
-                    test_teardown),
-            cmocka_unit_test_teardown(test_tcti_long_option_no_errata,
-                    test_teardown),
+            cmocka_unit_test(test_null_tcti_getenv_no_errata),
+            cmocka_unit_test(test_null_tcti_getenv_with_errata),
+            cmocka_unit_test(test_tcti_short_option_no_errata),
+            cmocka_unit_test(test_tcti_long_option_no_errata),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);

Makefile.am
@@ -18,12 +18,12 @@ LIB_COMMON := lib/libcommon.a
 
 AM_CFLAGS := \
     $(INCLUDE_DIRS) $(EXTRA_CFLAGS) $(TSS2_ESYS_CFLAGS) $(TSS2_MU_CFLAGS) \
-    $(CRYPTO_CFLAGS) $(CODE_COVERAGE_CFLAGS)
+    $(CRYPTO_CFLAGS) $(CODE_COVERAGE_CFLAGS) $(TSS2_TCTILDR_CFLAGS)
 
 AM_LDFLAGS   := $(EXTRA_LDFLAGS) $(CODE_COVERAGE_LIBS)
 
 LDADD = \
-    $(LIB_COMMON) $(TSS2_ESYS_LIBS) $(TSS2_MU_LIBS) $(CRYPTO_LIBS) $(LIBDL_LDFLAGS)
+    $(LIB_COMMON) $(TSS2_ESYS_LIBS) $(TSS2_MU_LIBS) $(CRYPTO_LIBS) $(TSS2_TCTILDR_LIBS)
 
 # keep me sorted
 bin_PROGRAMS = \
@@ -244,9 +244,7 @@ test_unit_test_tpm2_error_CFLAGS   = $(AM_CFLAGS) $(CMOCKA_CFLAGS)
 test_unit_test_tpm2_error_LDADD    = $(CMOCKA_LIBS) $(LDADD)
 
 test_unit_test_options_CFLAGS   = $(AM_CFLAGS) $(CMOCKA_CFLAGS)
-test_unit_test_options_LDFLAGS  = -Wl,--wrap=tpm2_util_dlopen \
-                               -Wl,--wrap=tpm2_util_dlclose \
-                               -Wl,--wrap=tpm2_util_dlsym \
+test_unit_test_options_LDFLAGS  = -Wl,--wrap=Tss2_TctiLdr_Initialize \
                                -Wl,--wrap=tpm2_util_getenv
 test_unit_test_options_LDADD    = $(CMOCKA_LIBS) $(LDADD)
 

configure.ac
@@ -32,6 +32,7 @@ AM_CONDITIONAL(
     [HAVE_MAN_PAGES],
     [test -d "${srcdir}/man/man1" -o "x${PANDOC}" = "xyes"])
 PKG_CHECK_MODULES([TSS2_ESYS], [tss2-esys >= 2.3.0])
+PKG_CHECK_MODULES([TSS2_TCTILDR], [tss2-tctildr])
 PKG_CHECK_MODULES([TSS2_MU], [tss2-mu])
 PKG_CHECK_MODULES([CRYPTO], [libcrypto >= 1.0.2g])
 PKG_CHECK_MODULES([CURL], [libcurl])
@@ -63,13 +64,10 @@ AC_CANONICAL_HOST
 case "${host_os}" in
     *nto-qnx*)
         EXTRA_CFLAGS="$EXTRA_CFLAGS -D_QNX_SOURCE"
-        LIBDL_LDFLAGS=""
         ;;
     *)
-        LIBDL_LDFLAGS="-ldl"
         ;;
 esac
-AC_SUBST([LIBDL_LDFLAGS])
 
 AC_ARG_ENABLE([unit],
             [AS_HELP_STRING([--enable-unit],
@@ -114,14 +112,6 @@ AS_IF([test "x$enable_unit" != xno], [
           [AC_MSG_ERROR([Required executable xxd not found, some system tests will fail!])])
     enable_unit="$enable_unit (tpm2_abrmd: $tpm2_abrmd, tpm_server: $tpm_server, bash: $BASH_SHELL, python: $PYTHON, xxd: $XXD)"
 ])
-
-AC_ARG_ENABLE([dlclose],
-  [AS_HELP_STRING([--disable-dlclose],
-                            [Some versions of libc cause a sigsegv on exit, this disables the dlclose and works around that bug])],
-  [AC_DEFINE([DISABLE_DLCLOSE], [1],
-  [Some versions of libc cause a sigsegv on exit with dlclose(), this disables the dlclose()
-  and works around that bug])]
- )
 
 AC_ARG_ENABLE([hardening],
   [AS_HELP_STRING([--disable-hardening],

lib/tpm2_tcti_ldr.c
@@ -1,114 +1,0 @@
-/* SPDX-License-Identifier: BSD-3-Clause */
-
-#include <dlfcn.h>
-#include <limits.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "log.h"
-#include "tpm2_tcti_ldr.h"
-
-#define TSS2_TCTI_SO_FORMAT "libtss2-tcti-%s.so.0"
-
-static void *handle;
-static const TSS2_TCTI_INFO *info;
-
-void tpm2_tcti_ldr_unload(void) {
-    if (handle) {
-#ifndef DISABLE_DLCLOSE
-        tpm2_util_dlclose(handle);
-#endif
-        handle = NULL;
-        info = NULL;
-    }
-}
-
-const TSS2_TCTI_INFO *tpm2_tcti_ldr_getinfo(void) {
-    return info;
-}
-
-static void* tpm2_tcti_ldr_dlopen(const char *name) {
-
-    char path[PATH_MAX];
-    size_t size = snprintf(path, sizeof(path), TSS2_TCTI_SO_FORMAT, name);
-    if (size >= sizeof(path)) {
-        LOG_ERR("Truncated TCTI friendly name conversion, got: \"%s\", made: \"%s\"",
-                name, path);
-        return NULL;
-    }
-
-    return tpm2_util_dlopen(path, RTLD_LAZY);
-}
-
-bool tpm2_tcti_ldr_is_tcti_present(const char *name) {
-
-    void *handle = tpm2_tcti_ldr_dlopen(name);
-    if (handle) {
-        tpm2_util_dlclose(handle);
-    }
-
-    return handle != NULL;
-}
-
-TSS2_TCTI_CONTEXT *tpm2_tcti_ldr_load(const char *path, const char *opts) {
-
-    TSS2_TCTI_CONTEXT *tcti_ctx = NULL;
-
-    if (handle) {
-        LOG_ERR("Attempting to load multiple tcti's simultaneously is not supported!");
-        return NULL;
-    }
-
-    /*
-     * Try what they gave us, if it doesn't load up, try
-     * libtss2-tcti-xxx.so replacing xxx with what they gave us.
-     */
-    handle = tpm2_util_dlopen (path, RTLD_LAZY);
-    if (!handle) {
-
-        handle = tpm2_tcti_ldr_dlopen(path);
-        if (!handle) {
-            LOG_ERR("Could not dlopen library: \"%s\"", PSTR(path));
-            return NULL;
-        }
-    }
-
-    TSS2_TCTI_INFO_FUNC infofn = (TSS2_TCTI_INFO_FUNC)tpm2_util_dlsym(handle, TSS2_TCTI_INFO_SYMBOL);
-    if (!infofn) {
-        LOG_ERR("Symbol \"%s\"not found in library: \"%s\"",
-                TSS2_TCTI_INFO_SYMBOL, path);
-        goto err;
-    }
-
-    info = infofn();
-
-    TSS2_TCTI_INIT_FUNC init = info->init;
-
-    size_t size;
-    TSS2_RC rc = init(NULL, &size, opts);
-    if (rc != TPM2_RC_SUCCESS) {
-        LOG_ERR("tcti init setup routine failed for library: \"%s\""
-                " options: \"%s\"", path, PSTR(opts));
-        goto err;
-    }
-
-    tcti_ctx = (TSS2_TCTI_CONTEXT*) calloc(1, size);
-    if (tcti_ctx == NULL) {
-        LOG_ERR("oom");
-        goto err;
-    }
-
-    rc = init(tcti_ctx, &size, opts);
-    if (rc != TPM2_RC_SUCCESS) {
-        LOG_ERR("tcti init allocation routine failed for library: \"%s\""
-                " options: \"%s\"", path, PSTR(opts));
-        goto err;
-    }
-
-    return tcti_ctx;
-
-err:
-    free(tcti_ctx);
-    tpm2_util_dlclose(handle);
-    return NULL;
-}

lib/tpm2_tcti_ldr.h
@@ -1,47 +1,0 @@
-/* SPDX-License-Identifier: BSD-3-Clause */
-
-#include <tss2/tss2_sys.h>
-
-#ifndef LIB_TPM2_TCTI_LDR_H_
-#define LIB_TPM2_TCTI_LDR_H_
-
-/**
- * Loads a TCTI from a friendly name, library name, or path.
- * For example
- *  friendly:     path = tabrmd
- *  library name: path = libtss2-tcti-mssim.so
- *  full path:    path = /home/user/lib/libtss2-tcti-custom.so
- * @param path
- *  The path/library to load.
- * @param opts
- *  The tcti option configs.
- * @return
- *  A tcti context on success or NULL on failure.
- */
-TSS2_TCTI_CONTEXT *tpm2_tcti_ldr_load(const char *path, const char *opts);
-
-/**
- * Returns the loaded TCTIs information structure,
- * which contains the initialization routine, description
- * and help string amongst other things.
- * @return
- *  NULL if no TCTI is loaded, else the info structure pointer.
- */
-const TSS2_TCTI_INFO *tpm2_tcti_ldr_getinfo(void);
-
-/**
- * Given a tcti name, like mssim, tells you if the
- * library is present using dlopen(3).
- * @param name
- *   The friendly name of the tcti.
- * @return
- *  True if present, false otherwise.
- */
-bool tpm2_tcti_ldr_is_tcti_present(const char *name);
-
-/**
- * Unloads the tcti loaded via tpm2_tcti_ldr_load();
- */
-void tpm2_tcti_ldr_unload(void);
-
-#endif /* LIB_TPM2_TCTI_LDR_H_ */

lib/tpm2_util.c
@@ -685,22 +685,7 @@ outname:
     return rc;
 }
 
-void *tpm2_util_dlopen(const char *filename, int flags) {
-
-    return dlopen(filename, flags);
-}
-
-int tpm2_util_dlclose(void *handle) {
-
-    return dlclose(handle);
-}
-
 char *tpm2_util_getenv(const char *name) {
 
     return getenv(name);
-}
-
-void *tpm2_util_dlsym(void *handle, const char *symbol) {
-
-    return dlsym(handle, symbol);
 }

lib/tpm2_util.h
@@ -395,12 +395,6 @@ tool_rc tpm2_util_esys_handle_to_sys_handle(ESYS_CONTEXT *context,
  */
 ESYS_TR tpm2_tpmi_hierarchy_to_esys_tr(TPMI_RH_PROVISION inh);
 
-void *tpm2_util_dlopen(const char *filename, int flags);
-
-int tpm2_util_dlclose(void *handle);
-
 char *tpm2_util_getenv(const char *name);
-
-void *tpm2_util_dlsym(void *handle, const char *symbol);
 
 #endif /* STRING_BYTES_H */

commit 218c6ae0f7da2d2f1915cc03c35d6ba74e1dc5e6

tcti: use separate rc variable for Tss2_TctiLdr_*
The return code of Tss2_TctiLdr_* is of type TSS2_RC, while tpm2-tools
internally uses tpm2_option_code. This results in a zero exit status
even if the TCTI could not be initialised because the return code is
not interpreted correctly. Also add a unit test for this scenario.

Fixes: GH-1617
Fixes: 0505f2b3969e8ca2006dcfbb083d45a3b62bfb30
Signed-off-by: Jonas Witschel <diabonas@gmx.de>

lib/tpm2_options.c
@@ -207,6 +207,7 @@ tpm2_option_code tpm2_handle_options (int argc, char **argv,
         TSS2_TCTI_CONTEXT **tcti) {
 
     tpm2_option_code rc = tpm2_option_code_err;
+    TSS2_RC rc_tcti;
     bool result = false;
     bool show_help = false;
     bool manpager = true;
@@ -339,8 +340,8 @@ tpm2_option_code tpm2_handle_options (int argc, char **argv,
                 }
                 goto none;
             }
-            rc = Tss2_TctiLdr_Initialize(tcti_conf_option, tcti);
-            if (rc != TSS2_RC_SUCCESS || !*tcti) {
+            rc_tcti = Tss2_TctiLdr_Initialize(tcti_conf_option, tcti);
+            if (rc_tcti != TSS2_RC_SUCCESS || !*tcti) {
               LOG_ERR("Could not load tcti, got: \"%s\"", tcti_conf_option);
               goto out;
             }
@@ -378,8 +379,8 @@ out:
         }
         if (tcti_conf_option && strcmp(tcti_conf_option, "none")) {
             TSS2_TCTI_INFO *info = NULL;
-            rc = Tss2_TctiLdr_GetInfo (tcti_conf_option, &info);
-            if (rc == TSS2_RC_SUCCESS && info) {
+            rc_tcti = Tss2_TctiLdr_GetInfo (tcti_conf_option, &info);
+            if (rc_tcti == TSS2_RC_SUCCESS && info) {
                 printf("\ntcti-help(%s): %s\n", info->name, info->config_help);
             }
             Tss2_TctiLdr_FreeInfo (&info);

test/unit/test_options.c
@@ -179,6 +179,30 @@ static void test_tcti_long_option_no_errata(void **state) {
     assert_int_equal(oc, tpm2_option_code_continue);
 }
 
+static void test_invalid_tcti_no_errata(void **state) {
+    UNUSED(state);
+
+    char *argv[] = {
+        "program",
+        "-T",      // Set TCTI to something specific
+        "tctiinvalid",
+        "-Z"       // Disable errata getenv call
+    };
+
+    int argc = ARRAY_LEN(argv);
+
+    tpm2_options *tool_opts = NULL;
+    tpm2_option_flags flags = { .all = 0 };
+    TSS2_TCTI_CONTEXT *tcti = NULL;
+
+    will_return(__wrap_Tss2_TctiLdr_Initialize, TSS2_TCTI_RC_NOT_SUPPORTED);
+
+    tpm2_option_code oc = tpm2_handle_options(argc, argv, tool_opts, &flags,
+            &tcti);
+    assert_int_equal(oc, tpm2_option_code_err);
+}
+
+
 /*
  * link required symbol, but tpm2_tool.c declares it AND main, which
  * we have a main below for cmocka tests.
@@ -194,6 +218,7 @@ int main(int argc, char *argv[]) {
             cmocka_unit_test(test_null_tcti_getenv_with_errata),
             cmocka_unit_test(test_tcti_short_option_no_errata),
             cmocka_unit_test(test_tcti_long_option_no_errata),
+            cmocka_unit_test(test_invalid_tcti_no_errata),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);