2e2706e27ffdd2c31c2fc2937bff7e10d7da9527
Imran Desai <imran.desai@intel.com>
2019-08-09 05:31:19 -0700

    tpm2_policyduplicationselect: Add integration test
    Signed-off-by: Imran Desai <imran.desai@intel.com>

---
+++ test/integration/tests/abrmd_policyduplicationselect.sh
@@ -0,0 +1,78 @@
+# SPDX-License-Identifier: BSD-3-Clause
+#;**********************************************************************;
+
+source helpers.sh
+
+source_parent_object=src_o.ctx
+new_parent_object=dst_n.ctx
+new_parent_name=dst_n.name
+duplicable_object_private=dupkey.priv
+duplicable_object_public=dupkey.pub
+duplicable_object=dupkey.ctx
+duplicable_object_name=dupkey.name
+policy_duplication_select=policydupselect.dat
+duplicated_object_private=dupkey_new.priv
+duplicated_object_seed=dupseed.dat
+policy_session=session.dat
+
+
+cleanup() {
+  rm -f  $source_parent_object $new_parent_object $new_parent_name \
+  $duplicable_object_private $duplicable_object_public $duplicable_object \
+  $policy_duplication_select $duplicated_object_private $policy_session \
+  $duplicated_object_seed $duplicable_object_name
+
+  if [ "$1" != "no-shut-down" ]; then
+     shut_down
+  fi
+}
+
+trap cleanup EXIT
+
+start_up
+
+cleanup "no-shut-down"
+
+# Error trapped and reported
+tpm2_clear
+
+# Create source parent and destination(or new) parent
+tpm2_createprimary -C n -g sha256 -G rsa -c $new_parent_object -Q
+
+tpm2_createprimary -C o -g sha256 -G rsa -c $source_parent_object -Q
+
+# Create the restricted parent policy
+tpm2_readpublic -c $new_parent_object -n $new_parent_name -Q
+
+tpm2_startauthsession -S $policy_session
+
+tpm2_policyduplicationselect -S $policy_session  -N $new_parent_name \
+  -L $policy_duplication_select -Q
+
+tpm2_flushcontext $policy_session
+
+rm $policy_session
+
+# Create the object to be duplicated using the policy
+tpm2_create -C $source_parent_object -g sha256 -G rsa \
+  -r $duplicable_object_private -u $duplicable_object_public \
+  -L $policy_duplication_select -a "sensitivedataorigin|sign|decrypt" \
+  -c $duplicable_object -Q
+
+# Satisfy the policy and duplicate the object
+tpm2_readpublic -c $duplicable_object -n $duplicable_object_name -Q
+
+tpm2_startauthsession -S $policy_session --policy-session
+
+tpm2_policyduplicationselect -S $policy_session  -N $new_parent_name \
+  -n $duplicable_object_name -Q
+
+tpm2_duplicate -C $new_parent_object -c $duplicable_object -G null \
+  -p session:$policy_session -r $duplicated_object_private \
+  -s $duplicated_object_seed
+
+tpm2_flushcontext  $policy_session
+
+rm $policy_session
+
+exit 0
