b33835f1ecac8c5e8d6251e5a8b68d9f7401f268
Imran Desai <imran.desai@intel.com>
2019-08-15 09:46:50 -0700

    tpm2_makecredential.c: Apply code formatter for appropriate white spacing
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- tools/tpm2_makecredential.c
+++ tools/tpm2_makecredential.c
@@ -20,10 +20,10 @@ struct tpm_makecred_ctx {
     TPM2B_PUBLIC public;
     TPM2B_DIGEST credential;
     struct {
-        UINT8 e : 1;
-        UINT8 s : 1;
-        UINT8 n : 1;
-        UINT8 o : 1;
+        UINT8 e :1;
+        UINT8 s :1;
+        UINT8 n :1;
+        UINT8 o :1;
     } flags;
 };
 
@@ -89,7 +89,6 @@ static tool_rc make_external_credential_and_save(void) {
      */
     TPMI_ALG_HASH name_alg = ctx.public.publicArea.nameAlg;
 
-
     /*
      * Generate and encrypt seed
      */
@@ -100,8 +99,7 @@ static tool_rc make_external_credential_and_save(void) {
     TPM2B_ENCRYPTED_SECRET encrypted_seed = TPM2B_EMPTY_INIT;
     unsigned char label[10] = { 'I', 'D', 'E', 'N', 'T', 'I', 'T', 'Y', 0 };
     bool res = tpm2_identity_util_encrypt_seed_with_public_key(&seed,
-            &ctx.public, label, 9,
-            &encrypted_seed);
+            &ctx.public, label, 9, &encrypted_seed);
     if (!res) {
         LOG_ERR("Failed Seed Encryption\n");
         return tool_rc_general_error;
@@ -113,39 +111,32 @@ static tool_rc make_external_credential_and_save(void) {
     TPM2B_MAX_BUFFER hmac_key;
     TPM2B_MAX_BUFFER enc_key;
     tpm2_identity_util_calc_outer_integrity_hmac_key_and_dupsensitive_enc_key(
-            &ctx.public,
-            &ctx.object_name,
-            &seed,
-            &hmac_key,
-            &enc_key);
+            &ctx.public, &ctx.object_name, &seed, &hmac_key, &enc_key);
 
     /*
      * The ctx.credential needs to be marshalled into struct with
      * both size and contents together (to be encrypted as a block)
      */
     TPM2B_MAX_BUFFER marshalled_inner_integrity = TPM2B_EMPTY_INIT;
-    marshalled_inner_integrity.size = ctx.credential.size + sizeof(ctx.credential.size);
+    marshalled_inner_integrity.size = ctx.credential.size
+            + sizeof(ctx.credential.size);
     UINT16 credSize = ctx.credential.size;
     if (!tpm2_util_is_big_endian()) {
         credSize = tpm2_util_endian_swap_16(credSize);
     }
     memcpy(marshalled_inner_integrity.buffer, &credSize, sizeof(credSize));
-    memcpy(&marshalled_inner_integrity.buffer[2], ctx.credential.buffer, ctx.credential.size);
+    memcpy(&marshalled_inner_integrity.buffer[2], ctx.credential.buffer,
+            ctx.credential.size);
 
     /*
      * Perform inner encryption (encIdentity) and outer HMAC (outerHMAC)
      */
     TPM2B_DIGEST outer_hmac = TPM2B_EMPTY_INIT;
     TPM2B_MAX_BUFFER encrypted_sensitive = TPM2B_EMPTY_INIT;
-    tpm2_identity_util_calculate_outer_integrity(
-            name_alg,
-            &ctx.object_name,
-            &marshalled_inner_integrity,
-            &hmac_key,
-            &enc_key,
+    tpm2_identity_util_calculate_outer_integrity(name_alg, &ctx.object_name,
+            &marshalled_inner_integrity, &hmac_key, &enc_key,
             &ctx.public.publicArea.parameters.rsaDetail.symmetric,
-            &encrypted_sensitive,
-            &outer_hmac);
+            &encrypted_sensitive, &outer_hmac);
 
     /*
      * Package up the info to save
@@ -159,35 +150,38 @@ static tool_rc make_external_credential_and_save(void) {
         outer_hmac_size = tpm2_util_endian_swap_16(outer_hmac_size);
     }
     int offset = 0;
-    memcpy(cred_blob.credential + offset, &outer_hmac_size, sizeof(outer_hmac.size));offset += sizeof(outer_hmac.size);
-    memcpy(cred_blob.credential + offset, outer_hmac.buffer, outer_hmac.size);offset += outer_hmac.size;
+    memcpy(cred_blob.credential + offset, &outer_hmac_size,
+            sizeof(outer_hmac.size));
+    offset += sizeof(outer_hmac.size);
+    memcpy(cred_blob.credential + offset, outer_hmac.buffer, outer_hmac.size);
+    offset += outer_hmac.size;
     //NOTE: do NOT include the encrypted_sensitive size, since it is encrypted with the blob!
-    memcpy(cred_blob.credential + offset, encrypted_sensitive.buffer, encrypted_sensitive.size);
+    memcpy(cred_blob.credential + offset, encrypted_sensitive.buffer,
+            encrypted_sensitive.size);
 
-    cred_blob.size = outer_hmac.size + encrypted_sensitive.size + sizeof(outer_hmac.size);
+    cred_blob.size = outer_hmac.size + encrypted_sensitive.size
+            + sizeof(outer_hmac.size);
 
-    return write_cred_and_secret(ctx.out_file_path, &cred_blob, &encrypted_seed)
-            ? tool_rc_success : tool_rc_general_error;
+    return write_cred_and_secret(ctx.out_file_path, &cred_blob,
+            &encrypted_seed) ? tool_rc_success : tool_rc_general_error;
 }
 
-static tool_rc make_credential_and_save(ESYS_CONTEXT *ectx)
-{
+static tool_rc make_credential_and_save(ESYS_CONTEXT *ectx) {
     TPM2B_ID_OBJECT *cred_blob;
     TPM2B_ENCRYPTED_SECRET *secret;
     ESYS_TR tr_handle = ESYS_TR_NONE;
     UINT32 rval;
 
-    rval = Esys_LoadExternal(ectx, ESYS_TR_NONE, ESYS_TR_NONE,
-                        ESYS_TR_NONE, NULL, &ctx.public, TPM2_RH_NULL,
-                        &tr_handle);
+    rval = Esys_LoadExternal(ectx, ESYS_TR_NONE, ESYS_TR_NONE, ESYS_TR_NONE,
+            NULL, &ctx.public, TPM2_RH_NULL, &tr_handle);
     if (rval != TPM2_RC_SUCCESS) {
         LOG_PERR(Esys_LoadExternal, rval);
         return tool_rc_from_tpm(rval);
     }
 
-    rval = Esys_MakeCredential(ectx, tr_handle, ESYS_TR_NONE,
-                        ESYS_TR_NONE, ESYS_TR_NONE, &ctx.credential,
-                        &ctx.object_name, &cred_blob, &secret);
+    rval = Esys_MakeCredential(ectx, tr_handle, ESYS_TR_NONE, ESYS_TR_NONE,
+            ESYS_TR_NONE, &ctx.credential, &ctx.object_name, &cred_blob,
+            &secret);
     if (rval != TPM2_RC_SUCCESS) {
         LOG_PERR(Esys_MakeCredential, rval);
         return tool_rc_from_tpm(rval);
@@ -214,11 +208,12 @@ static bool on_option(char key, char *value) {
             return false;
         }
         ctx.flags.e = 1;
-    } break;
+    }
+        break;
     case 's':
         ctx.credential.size = BUFFER_SIZE(TPM2B_DIGEST, buffer);
         if (!files_load_bytes_from_path(value, ctx.credential.buffer,
-                                        &ctx.credential.size)) {
+                &ctx.credential.size)) {
             return false;
         }
         ctx.flags.s = 1;
@@ -227,12 +222,13 @@ static bool on_option(char key, char *value) {
         ctx.object_name.size = BUFFER_SIZE(TPM2B_NAME, name);
         int q;
         if ((q = tpm2_util_hex_to_byte_structure(value, &ctx.object_name.size,
-                                            ctx.object_name.name)) != 0) {
+                ctx.object_name.name)) != 0) {
             LOG_ERR("FAILED: %d", q);
             return false;
         }
         ctx.flags.n = 1;
-    } break;
+    }
+        break;
     case 'o':
         ctx.out_file_path = value;
         ctx.flags.o = 1;
@@ -252,7 +248,7 @@ bool tpm2_tool_onstart(tpm2_options **opts) {
     };
 
     *opts = tpm2_options_new("e:s:n:o:", ARRAY_LEN(topts), topts, on_option,
-                             NULL, TPM2_OPTIONS_OPTIONAL_SAPI);
+        NULL, TPM2_OPTIONS_OPTIONAL_SAPI);
 
     return *opts != NULL;
 }
@@ -267,6 +263,7 @@ tool_rc tpm2_tool_onrun(ESYS_CONTEXT *ectx, tpm2_option_flags flags) {
     }
 
     // Run it outside of a TPM
-    return ectx ? make_credential_and_save(ectx) :
-            make_external_credential_and_save();
+    return ectx ?
+            make_credential_and_save(ectx) :
+                make_external_credential_and_save();
 }
