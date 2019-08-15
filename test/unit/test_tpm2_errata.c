d106389b31dd6b8b2c283ae9c26880d657d26ccd
Imran Desai <imran.desai@intel.com>
2019-08-15 16:15:00 -0700

    test_tpm2_errata.c: Apply code formatter for appropriate white spacing
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- test/unit/test_tpm2_errata.c
+++ test/unit/test_tpm2_errata.c
@@ -10,7 +10,8 @@
 #include "tpm2_errata.h"
 #include "tpm2_util.h"
 
-static inline void setcaps(UINT32 level, UINT32 rev, UINT32 day, UINT32 year, TSS2_RC rc) {
+static inline void setcaps(UINT32 level, UINT32 rev, UINT32 day, UINT32 year,
+        TSS2_RC rc) {
 
     will_return(__wrap_Esys_GetCapability, level);
     will_return(__wrap_Esys_GetCapability, rev);
@@ -20,10 +21,10 @@ static inline void setcaps(UINT32 level, UINT32 rev, UINT32 day, UINT32 year, TS
 
 }
 
-TSS2_RC __wrap_Esys_GetCapability(ESYS_CONTEXT *context,
-        ESYS_TR session1, ESYS_TR session2, ESYS_TR session3,
-        TPM2_CAP capability, UINT32 property, UINT32 propertyCount,
-        TPMI_YES_NO *moreData, TPMS_CAPABILITY_DATA **capabilityData) {
+TSS2_RC __wrap_Esys_GetCapability(ESYS_CONTEXT *context, ESYS_TR session1,
+        ESYS_TR session2, ESYS_TR session3, TPM2_CAP capability,
+        UINT32 property, UINT32 propertyCount, TPMI_YES_NO *moreData,
+        TPMS_CAPABILITY_DATA **capabilityData) {
 
     UNUSED(context);
     UNUSED(session1);
@@ -39,21 +40,22 @@ TSS2_RC __wrap_Esys_GetCapability(ESYS_CONTEXT *context,
 
     *capabilityData = calloc(1, sizeof(**capabilityData));
     (*capabilityData)->capability = capability;
-    TPML_TAGGED_TPM_PROPERTY *properties = &(*capabilityData)->data.tpmProperties;
+    TPML_TAGGED_TPM_PROPERTY *properties =
+            &(*capabilityData)->data.tpmProperties;
 
     properties->count = 4;
 
     properties->tpmProperty[0].property = TPM2_PT_LEVEL;
-    properties->tpmProperty[0].value    = (UINT32) mock();
+    properties->tpmProperty[0].value = (UINT32) mock();
 
     properties->tpmProperty[1].property = TPM2_PT_REVISION;
-    properties->tpmProperty[1].value    = (UINT32) mock();
+    properties->tpmProperty[1].value = (UINT32) mock();
 
     properties->tpmProperty[2].property = TPM2_PT_DAY_OF_YEAR;
-    properties->tpmProperty[2].value    = (UINT32) mock();
+    properties->tpmProperty[2].value = (UINT32) mock();
 
     properties->tpmProperty[3].property = TPM2_PT_YEAR;
-    properties->tpmProperty[3].value    = (UINT32) mock();
+    properties->tpmProperty[3].value = (UINT32) mock();
 
     TSS2_RC rc = (int) mock(); /* dequeue second value */
     if (rc != TSS2_RC_SUCCESS)
@@ -67,17 +69,17 @@ TSS2_RC __wrap_Esys_GetCapability(ESYS_CONTEXT *context,
     } \
 }
 
-
 static void test_tpm2_errata_no_init_and_apply(void **state) {
     UNUSED(state);
 
     TPM2B_PUBLIC in_public = TPM2B_PUBLIC_INIT(TPMA_OBJECT_SIGN_ENCRYPT);
 
     tpm2_errata_fixup(SPEC_116_ERRATA_2_7,
-                      &in_public.publicArea.objectAttributes);
+            &in_public.publicArea.objectAttributes);
 
-    assert_int_equal(in_public.publicArea.objectAttributes & TPMA_OBJECT_SIGN_ENCRYPT,
-                    TPMA_OBJECT_SIGN_ENCRYPT);
+    assert_int_equal(
+            in_public.publicArea.objectAttributes & TPMA_OBJECT_SIGN_ENCRYPT,
+            TPMA_OBJECT_SIGN_ENCRYPT);
 }
 
 static void test_tpm2_errata_bad_init_and_apply(void **state) {
@@ -86,14 +88,14 @@ static void test_tpm2_errata_bad_init_and_apply(void **state) {
     setcaps(00, 116, 303, 2014, TPM2_RC_FAILURE);
     tpm2_errata_init((ESYS_CONTEXT *) 0xDEADBEEF);
 
-
     TPM2B_PUBLIC in_public = TPM2B_PUBLIC_INIT(TPMA_OBJECT_SIGN_ENCRYPT);
 
     tpm2_errata_fixup(SPEC_116_ERRATA_2_7,
-                      &in_public.publicArea.objectAttributes);
+            &in_public.publicArea.objectAttributes);
 
-    assert_int_equal(in_public.publicArea.objectAttributes & TPMA_OBJECT_SIGN_ENCRYPT,
-                    TPMA_OBJECT_SIGN_ENCRYPT);
+    assert_int_equal(
+            in_public.publicArea.objectAttributes & TPMA_OBJECT_SIGN_ENCRYPT,
+            TPMA_OBJECT_SIGN_ENCRYPT);
 }
 
 static void test_tpm2_errata_init_good_and_apply(void **state) {
@@ -105,10 +107,11 @@ static void test_tpm2_errata_init_good_and_apply(void **state) {
     TPM2B_PUBLIC in_public = TPM2B_PUBLIC_INIT(TPMA_OBJECT_SIGN_ENCRYPT);
 
     tpm2_errata_fixup(SPEC_116_ERRATA_2_7,
-                      &in_public.publicArea.objectAttributes);
+            &in_public.publicArea.objectAttributes);
 
-    assert_int_equal(in_public.publicArea.objectAttributes & TPMA_OBJECT_SIGN_ENCRYPT,
-                     0);
+    assert_int_equal(
+            in_public.publicArea.objectAttributes & TPMA_OBJECT_SIGN_ENCRYPT,
+            0);
 }
 
 static void test_tpm2_errata_init_good_and_no_match(void **state) {
@@ -121,10 +124,11 @@ static void test_tpm2_errata_init_good_and_no_match(void **state) {
     TPM2B_PUBLIC in_public = TPM2B_PUBLIC_INIT(TPMA_OBJECT_SIGN_ENCRYPT);
 
     tpm2_errata_fixup(SPEC_116_ERRATA_2_7,
-                      &in_public.publicArea.objectAttributes);
+            &in_public.publicArea.objectAttributes);
 
-    assert_int_equal(in_public.publicArea.objectAttributes & TPMA_OBJECT_SIGN_ENCRYPT,
-                    TPMA_OBJECT_SIGN_ENCRYPT);
+    assert_int_equal(
+            in_public.publicArea.objectAttributes & TPMA_OBJECT_SIGN_ENCRYPT,
+            TPMA_OBJECT_SIGN_ENCRYPT);
 }
 
 static void test_tpm2_errata_init_no_match_and_apply(void **state) {
@@ -138,10 +142,11 @@ static void test_tpm2_errata_init_no_match_and_apply(void **state) {
     TPM2B_PUBLIC in_public = TPM2B_PUBLIC_INIT(TPMA_OBJECT_SIGN_ENCRYPT);
 
     tpm2_errata_fixup(SPEC_116_ERRATA_2_7,
-                      &in_public.publicArea.objectAttributes);
+            &in_public.publicArea.objectAttributes);
 
-    assert_int_equal(in_public.publicArea.objectAttributes & TPMA_OBJECT_SIGN_ENCRYPT,
-                    TPMA_OBJECT_SIGN_ENCRYPT);
+    assert_int_equal(
+            in_public.publicArea.objectAttributes & TPMA_OBJECT_SIGN_ENCRYPT,
+            TPMA_OBJECT_SIGN_ENCRYPT);
 }
 
 /* link required symbol, but tpm2_tool.c declares it AND main, which
