c4f257e56c24e9e72d4f89adec87c13ec1f2d93e
Imran Desai <imran.desai@intel.com>
2019-08-15 11:55:57 -0700

    tpm2_verifysignature.c: Apply code formatter for appropriate white spacing
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- tools/tpm2_verifysignature.c
+++ tools/tpm2_verifysignature.c
@@ -47,9 +47,8 @@ static tool_rc verify_signature(ESYS_CONTEXT *context) {
     TPMT_TK_VERIFIED *validation;
 
     TSS2_RC rval = Esys_VerifySignature(context,
-                        ctx.key_context_object.tr_handle,
-                        ESYS_TR_NONE, ESYS_TR_NONE, ESYS_TR_NONE,
-                        ctx.msgHash, &ctx.signature, &validation);
+            ctx.key_context_object.tr_handle, ESYS_TR_NONE, ESYS_TR_NONE,
+            ESYS_TR_NONE, ctx.msgHash, &ctx.signature, &validation);
     if (rval != TPM2_RC_SUCCESS) {
         LOG_PERR(Esys_VerifySignature, rval);
         rc = tool_rc_from_tpm(rval);
@@ -63,7 +62,7 @@ static tool_rc verify_signature(ESYS_CONTEXT *context) {
     if (ctx.out_file_path) {
         if (validation->hierarchy == TPM2_RH_NULL) {
             LOG_WARN("The NULL hierarchy doesn't produce a validation ticket,"
-                     " not outputting ticket");
+                    " not outputting ticket");
         } else {
             if (!files_save_ticket(validation, ctx.out_file_path)) {
                 rc = tool_rc_general_error;
@@ -109,21 +108,19 @@ static tool_rc init(ESYS_CONTEXT *context) {
 
     /* check flags for mismatches */
     if (ctx.flags.digest && (ctx.flags.msg || ctx.flags.halg)) {
-        LOG_ERR(
-                "Cannot specify --digest (-D) and ( --msg (-m) or --halg (-g) )");
+        LOG_ERR("Cannot specify --digest (-D) and ( --msg (-m) or --halg (-g) )");
         return tool_rc_option_error;
     }
 
     if (!(ctx.context_arg && ctx.flags.sig)) {
-        LOG_ERR(
-                "--key-context (-c) and --sig (-s) are required");
+        LOG_ERR("--key-context (-c) and --sig (-s) are required");
         return tool_rc_option_error;
     }
 
     TPM2B *msg = NULL;
 
     tool_rc tmp_rc = tpm2_util_object_load(context, ctx.context_arg,
-        &ctx.key_context_object, TPM2_HANDLE_ALL_W_NV);
+            &ctx.key_context_object, TPM2_HANDLE_ALL_W_NV);
     if (tmp_rc != tool_rc_success) {
         return tmp_rc;
     }
@@ -138,8 +135,10 @@ static tool_rc init(ESYS_CONTEXT *context) {
 
     if (ctx.flags.sig) {
 
-        tpm2_convert_sig_fmt fmt = ctx.flags.fmt ? signature_format_plain : signature_format_tss;
-        bool res = tpm2_convert_sig_load(ctx.sig_file_path, fmt, ctx.format, ctx.halg, &ctx.signature);
+        tpm2_convert_sig_fmt fmt =
+                ctx.flags.fmt ? signature_format_plain : signature_format_tss;
+        bool res = tpm2_convert_sig_load(ctx.sig_file_path, fmt, ctx.format,
+                ctx.halg, &ctx.signature);
         if (!res) {
             goto err;
         }
@@ -153,11 +152,12 @@ static tool_rc init(ESYS_CONTEXT *context) {
              * we'll add it here to silence any complainers (such as static
              * analysers).
              */
-            LOG_ERR("No digest set and no message file to compute from, cannot compute message hash!");
+            LOG_ERR("No digest set and no message file to compute from, cannot "
+                    "compute message hash!");
             goto err;
         }
-        tmp_rc = tpm2_hash_compute_data(context, ctx.halg,
-                TPM2_RH_NULL, msg->buffer, msg->size, &ctx.msgHash, NULL);
+        tmp_rc = tpm2_hash_compute_data(context, ctx.halg, TPM2_RH_NULL,
+                msg->buffer, msg->size, &ctx.msgHash, NULL);
         if (tmp_rc != tool_rc_success) {
             rc = tmp_rc;
             LOG_ERR("Compute message hash failed!");
@@ -174,53 +174,55 @@ err:
 
 static bool on_option(char key, char *value) {
 
-	switch (key) {
-	case 'c':
-	    ctx.context_arg = value;
-	    break;
-	case 'g': {
-		ctx.halg = tpm2_alg_util_from_optarg(value, tpm2_alg_util_flags_hash);
-		if (ctx.halg == TPM2_ALG_ERROR) {
-			LOG_ERR("Unable to convert algorithm, got: \"%s\"", value);
-			return false;
-		}
-		ctx.flags.halg = 1;
-	}
-		break;
-	case 'm': {
-		ctx.msg_file_path = value;
-		ctx.flags.msg = 1;
-	}
-		break;
-	case 'd': {
+    switch (key) {
+    case 'c':
+        ctx.context_arg = value;
+        break;
+    case 'g': {
+        ctx.halg = tpm2_alg_util_from_optarg(value, tpm2_alg_util_flags_hash);
+        if (ctx.halg == TPM2_ALG_ERROR) {
+            LOG_ERR("Unable to convert algorithm, got: \"%s\"", value);
+            return false;
+        }
+        ctx.flags.halg = 1;
+    }
+        break;
+    case 'm': {
+        ctx.msg_file_path = value;
+        ctx.flags.msg = 1;
+    }
+        break;
+    case 'd': {
         ctx.msgHash = malloc(sizeof(TPM2B_DIGEST));
-	    ctx.msgHash->size = sizeof(ctx.msgHash->buffer);
-		if (!files_load_bytes_from_path(value, ctx.msgHash->buffer, &ctx.msgHash->size)) {
-			LOG_ERR("Could not load digest from file!");
-			return false;
-		}
-		ctx.flags.digest = 1;
-	}
-		break;
-	case 'f': {
-		ctx.format = tpm2_alg_util_from_optarg(value, tpm2_alg_util_flags_sig);
-		if (ctx.format == TPM2_ALG_ERROR) {
-		    LOG_ERR("Unknown signing scheme, got: \"%s\"", value);
-		    return false;
-		}
+        ctx.msgHash->size = sizeof(ctx.msgHash->buffer);
+        if (!files_load_bytes_from_path(value, ctx.msgHash->buffer,
+                &ctx.msgHash->size)) {
+            LOG_ERR("Could not load digest from file!");
+            return false;
+        }
+        ctx.flags.digest = 1;
+    }
+        break;
+    case 'f': {
+        ctx.format = tpm2_alg_util_from_optarg(value, tpm2_alg_util_flags_sig);
+        if (ctx.format == TPM2_ALG_ERROR) {
+            LOG_ERR("Unknown signing scheme, got: \"%s\"", value);
+            return false;
+        }
 
-		ctx.flags.fmt = 1;
-	} break;
-	case 's':
-		ctx.sig_file_path = value;
-		ctx.flags.sig = 1;
-		break;
-	case 't':
-		ctx.out_file_path = value;
-		ctx.flags.ticket = 1;
-		break;
-		/* no default */
-	}
+        ctx.flags.fmt = 1;
+    }
+        break;
+    case 's':
+        ctx.sig_file_path = value;
+        ctx.flags.sig = 1;
+        break;
+    case 't':
+        ctx.out_file_path = value;
+        ctx.flags.ticket = 1;
+        break;
+        /* no default */
+    }
 
     return true;
 }
@@ -239,14 +241,14 @@ bool tpm2_tool_onstart(tpm2_options **opts) {
 
 
     *opts = tpm2_options_new("g:m:d:f:s:t:c:", ARRAY_LEN(topts), topts,
-                             on_option, NULL, 0);
+            on_option, NULL, 0);
 
     return *opts != NULL;
 }
 
 tool_rc tpm2_tool_onrun(ESYS_CONTEXT *context, tpm2_option_flags flags) {
 
-	UNUSED(flags);
+    UNUSED(flags);
 
     /* initialize and process */
     tool_rc rc = init(context);
