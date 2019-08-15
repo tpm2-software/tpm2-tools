74c21565e3136cb109341cd4cbfeb65db334ee87
Imran Desai <imran.desai@intel.com>
2019-08-15 12:40:40 -0700

    tpm2_policylocality.c: Apply code formatter for appropriate white spacing
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- tools/tpm2_policylocality.c
+++ tools/tpm2_policylocality.c
@@ -10,11 +10,11 @@
 
 typedef struct tpm2_policylocality_ctx tpm2_policylocality_ctx;
 struct tpm2_policylocality_ctx {
-   const char *session_path;
-   TPMA_LOCALITY locality;
-   const char *out_policy_dgst_path;
-   TPM2B_DIGEST *policy_digest;
-   tpm2_session *session;
+    const char *session_path;
+    TPMA_LOCALITY locality;
+    const char *out_policy_dgst_path;
+    TPM2B_DIGEST *policy_digest;
+    tpm2_session *session;
 };
 
 static tpm2_policylocality_ctx ctx;
@@ -42,7 +42,7 @@ bool is_input_option_args_valid(void) {
     return true;
 }
 
-bool on_arg (int argc, char **argv) {
+bool on_arg(int argc, char **argv) {
 
     if (argc > 1) {
         LOG_ERR("Specify only the TPM2 locality.");
@@ -56,13 +56,13 @@ bool on_arg (int argc, char **argv) {
 
     if (strcmp(argv[0], "zero")) {
         ctx.locality = TPMA_LOCALITY_TPM2_LOC_ZERO;
-    } else if(strcmp(argv[0], "one")) {
+    } else if (strcmp(argv[0], "one")) {
         ctx.locality = TPMA_LOCALITY_TPM2_LOC_ONE;
-    } else if(strcmp(argv[0], "two")) {
+    } else if (strcmp(argv[0], "two")) {
         ctx.locality = TPMA_LOCALITY_TPM2_LOC_TWO;
-    } else if(strcmp(argv[0], "three")) {
+    } else if (strcmp(argv[0], "three")) {
         ctx.locality = TPMA_LOCALITY_TPM2_LOC_THREE;
-    } else if(strcmp(argv[0], "four")) {
+    } else if (strcmp(argv[0], "four")) {
         ctx.locality = TPMA_LOCALITY_TPM2_LOC_FOUR;
     } else {
         bool result = tpm2_util_string_to_uint8(argv[0], &ctx.locality);
@@ -79,12 +79,12 @@ bool on_arg (int argc, char **argv) {
 bool tpm2_tool_onstart(tpm2_options **opts) {
 
     static struct option topts[] = {
-        { "session",            required_argument,  NULL,   'S' },
-        { "policy",             required_argument,  NULL,   'L' },
+        { "session", required_argument,  NULL, 'S' },
+        { "policy",  required_argument,  NULL, 'L' },
     };
 
-    *opts = tpm2_options_new("S:L:", ARRAY_LEN(topts), topts, on_option,
-                             on_arg, 0);
+    *opts = tpm2_options_new("S:L:", ARRAY_LEN(topts), topts, on_option, on_arg,
+            0);
 
     return *opts != NULL;
 }
@@ -98,13 +98,13 @@ tool_rc tpm2_tool_onrun(ESYS_CONTEXT *ectx, tpm2_option_flags flags) {
         return tool_rc_option_error;
     }
 
-    tool_rc rc = tpm2_session_restore(ectx, ctx.session_path, false, &ctx.session);
+    tool_rc rc = tpm2_session_restore(ectx, ctx.session_path, false,
+            &ctx.session);
     if (rc != tool_rc_success) {
         return rc;
     }
 
-    rc = tpm2_policy_build_policylocality(ectx, ctx.session,
-        ctx.locality);
+    rc = tpm2_policy_build_policylocality(ectx, ctx.session, ctx.locality);
     if (rc != tool_rc_success) {
         LOG_ERR("Could not build TPM policy_locality");
         return rc;
