63f417d1b1db6201585d1c89de947c63bfd28343
Imran Desai <imran.desai@intel.com>
2019-08-15 12:44:02 -0700

    tpm2_policypcr.c: Apply code formatter for appropriate white spacing
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- tools/tpm2_policypcr.c
+++ tools/tpm2_policypcr.c
@@ -11,12 +11,12 @@
 
 typedef struct tpm2_policypcr_ctx tpm2_policypcr_ctx;
 struct tpm2_policypcr_ctx {
-   const char *session_path;
-   const char *raw_pcrs_file;
-   TPML_PCR_SELECTION pcr_selection;
-   const char *policy_out_path;
-   TPM2B_DIGEST *policy_digest;
-   tpm2_session *session;
+    const char *session_path;
+    const char *raw_pcrs_file;
+    TPML_PCR_SELECTION pcr_selection;
+    const char *policy_out_path;
+    TPM2B_DIGEST *policy_digest;
+    tpm2_session *session;
 };
 
 static tpm2_policypcr_ctx ctx;
@@ -36,10 +36,11 @@ static bool on_option(char key, char *value) {
             LOG_ERR("Could not parse PCR selections");
             return false;
         }
-    } break;
+    }
+        break;
     case 'S':
         ctx.session_path = value;
-    break;
+        break;
     }
     return true;
 }
@@ -47,14 +48,14 @@ static bool on_option(char key, char *value) {
 bool tpm2_tool_onstart(tpm2_options **opts) {
 
     static struct option topts[] = {
-        { "policy",             required_argument,  NULL,   'L' },
-        { "pcr",                required_argument,  NULL,   'f' },
-        { "pcr-list",           required_argument,  NULL,   'l' },
-        { "session",            required_argument,  NULL,   'S' },
+        { "policy",   required_argument,  NULL, 'L' },
+        { "pcr",      required_argument,  NULL, 'f' },
+        { "pcr-list", required_argument,  NULL, 'l' },
+        { "session",  required_argument,  NULL, 'S' },
     };
 
     *opts = tpm2_options_new("L:f:l:S:", ARRAY_LEN(topts), topts, on_option,
-                             NULL, 0);
+    NULL, 0);
 
     return *opts != NULL;
 }
@@ -78,14 +79,13 @@ tool_rc tpm2_tool_onrun(ESYS_CONTEXT *ectx, tpm2_option_flags flags) {
         return tool_rc_general_error;
     }
 
-    tool_rc rc = tpm2_session_restore(ectx, ctx.session_path, false, &ctx.session);
+    tool_rc rc = tpm2_session_restore(ectx, ctx.session_path, false,
+            &ctx.session);
     if (rc != tool_rc_success) {
         return rc;
     }
 
-
-    rc  = tpm2_policy_build_pcr(ectx, ctx.session,
-            ctx.raw_pcrs_file,
+    rc = tpm2_policy_build_pcr(ectx, ctx.session, ctx.raw_pcrs_file,
             &ctx.pcr_selection);
     if (rc != tool_rc_success) {
         LOG_ERR("Could not build pcr policy");
