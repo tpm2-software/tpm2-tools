2a6cd98c32fddce6f34e840350b2be529def0db6
Imran Desai <imran.desai@intel.com>
2019-08-15 07:05:28 -0700

    tpm2_nvreadpublic.c: Apply code formatter for appropriate white spacing
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- tools/tpm2_nvreadpublic.c
+++ tools/tpm2_nvreadpublic.c
@@ -14,30 +14,29 @@ static void print_nv_public(TPM2B_NV_PUBLIC *nv_public) {
         LOG_ERR("Could not convert attributes to string form");
     }
 
-    const char *alg = tpm2_alg_util_algtostr(nv_public->nvPublic.nameAlg, tpm2_alg_util_flags_hash);
+    const char *alg = tpm2_alg_util_algtostr(nv_public->nvPublic.nameAlg,
+            tpm2_alg_util_flags_hash);
     if (!alg) {
         LOG_ERR("Could not convert algorithm to string form");
     }
 
     tpm2_tool_output("  hash algorithm:\n");
     tpm2_tool_output("    friendly: %s\n", alg);
-    tpm2_tool_output("    value: 0x%X\n",
-            nv_public->nvPublic.nameAlg);
+    tpm2_tool_output("    value: 0x%X\n", nv_public->nvPublic.nameAlg);
 
     tpm2_tool_output("  attributes:\n");
     tpm2_tool_output("    friendly: %s\n", attrs);
     tpm2_tool_output("    value: 0x%X\n",
             tpm2_util_ntoh_32(nv_public->nvPublic.attributes));
 
-    tpm2_tool_output("  size: %d\n",
-               nv_public->nvPublic.dataSize);
+    tpm2_tool_output("  size: %d\n", nv_public->nvPublic.dataSize);
 
     if (nv_public->nvPublic.authPolicy.size) {
         tpm2_tool_output("  authorization policy: ");
 
         UINT16 i;
-        for (i=0; i<nv_public->nvPublic.authPolicy.size; i++) {
-            tpm2_tool_output("%02X", nv_public->nvPublic.authPolicy.buffer[i] );
+        for (i = 0; i < nv_public->nvPublic.authPolicy.size; i++) {
+            tpm2_tool_output("%02X", nv_public->nvPublic.authPolicy.buffer[i]);
         }
         tpm2_tool_output("\n");
     }
@@ -49,11 +48,9 @@ static tool_rc nv_readpublic(ESYS_CONTEXT *context) {
 
     TPMS_CAPABILITY_DATA *capabilityData;
     UINT32 property = tpm2_util_hton_32(TPM2_HT_NV_INDEX);
-    TSS2_RC rval = Esys_GetCapability(context,
-                                      ESYS_TR_NONE, ESYS_TR_NONE, ESYS_TR_NONE,
-                                      TPM2_CAP_HANDLES, property,
-                                      TPM2_PT_NV_INDEX_MAX,
-                                      NULL, &capabilityData);
+    TSS2_RC rval = Esys_GetCapability(context, ESYS_TR_NONE, ESYS_TR_NONE,
+            ESYS_TR_NONE, TPM2_CAP_HANDLES, property, TPM2_PT_NV_INDEX_MAX,
+            NULL, &capabilityData);
     if (rval != TPM2_RC_SUCCESS) {
         LOG_PERR(Esys_GetCapability, rval);
         return tool_rc_from_tpm(rval);
@@ -83,8 +80,7 @@ static tool_rc nv_readpublic(ESYS_CONTEXT *context) {
 
 bool tpm2_tool_onstart(tpm2_options **opts) {
 
-    *opts = tpm2_options_new(NULL, 0, NULL, NULL, NULL,
-            0);
+    *opts = tpm2_options_new(NULL, 0, NULL, NULL, NULL, 0);
 
     return *opts != NULL;
 }
