30b923a72e0db0e2900c096b16d818b920563b1d
Imran Desai <imran.desai@intel.com>
2019-08-15 23:15:19 -0700

    lib/tpm2_ctx_mgmt: Apply code formatter for appropriate white spacing
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- lib/tpm2_ctx_mgmt.c
+++ lib/tpm2_ctx_mgmt.c
@@ -3,12 +3,8 @@
 #include "tpm2_auth_util.h"
 #include "tpm2_ctx_mgmt.h"
 
-tool_rc tpm2_ctx_mgmt_evictcontrol(
-        ESYS_CONTEXT *ectx,
-        ESYS_TR auth,
-        tpm2_session *sess,
-        ESYS_TR objhandle,
-        TPMI_DH_PERSISTENT phandle,
+tool_rc tpm2_ctx_mgmt_evictcontrol(ESYS_CONTEXT *ectx, ESYS_TR auth,
+        tpm2_session *sess, ESYS_TR objhandle, TPMI_DH_PERSISTENT phandle,
         ESYS_TR *out_tr) {
 
     ESYS_TR shandle1 = ESYS_TR_NONE;
@@ -19,15 +15,8 @@ tool_rc tpm2_ctx_mgmt_evictcontrol(
 
     ESYS_TR outHandle;
 
-    TSS2_RC rval = Esys_EvictControl(
-        ectx,
-        auth,
-        objhandle,
-        shandle1,
-        ESYS_TR_NONE,
-        ESYS_TR_NONE,
-        phandle,
-        &outHandle);
+    TSS2_RC rval = Esys_EvictControl(ectx, auth, objhandle, shandle1,
+            ESYS_TR_NONE, ESYS_TR_NONE, phandle, &outHandle);
     if (rval != TSS2_RC_SUCCESS) {
         LOG_PERR(Esys_EvictControl, rval);
         return tool_rc_from_tpm(rval);
