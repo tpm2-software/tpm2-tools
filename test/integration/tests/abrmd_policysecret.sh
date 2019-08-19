f1f5ee07da8ee48f31b735c36ff7efd5f047a854
Imran Desai <imran.desai@intel.com>
2019-08-19 09:49:49 -0700

    abrmd_policysecret.sh: fixed white spaces and remove spurious quotations
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- test/integration/tests/abrmd_policysecret.sh
+++ test/integration/tests/abrmd_policysecret.sh
@@ -3,7 +3,7 @@
 source helpers.sh
 
 TPM_RH_OWNER=0x40000001
-SEALED_SECRET="SEALED-SECRET"
+SEALED_SECRET=SEALED-SECRET
 session_ctx=session.ctx
 o_policy_digest=policy.digest
 primary_ctx=prim.ctx
@@ -12,7 +12,7 @@ seal_key_priv=sealing_key.priv
 seal_key_ctx=sealing_key.ctx
 
 cleanup() {
-    rm -f  $session_ctx $o_policy_digest $primary_ctx $seal_key_pub $seal_key_priv\
+    rm -f $session_ctx $o_policy_digest $primary_ctx $seal_key_pub $seal_key_priv\
     $seal_key_ctx
 
     tpm2_flushcontext $session_ctx 2>/dev/null || true
@@ -40,14 +40,15 @@ tpm2_flushcontext $session_ctx
 rm $session_ctx
 
 # Create and Load Object
-tpm2_createprimary -Q -C o  -c $primary_ctx -P ownerauth
-tpm2_create -Q -g sha256 -u $seal_key_pub -r $seal_key_priv -C $primary_ctx\
-  -L $o_policy_digest -i- <<< $SEALED_SECRET
+tpm2_createprimary -Q -C o -c $primary_ctx -P ownerauth
+tpm2_create -Q -g sha256 -u $seal_key_pub -r $seal_key_priv -C $primary_ctx \
+-L $o_policy_digest -i- <<< $SEALED_SECRET
 tpm2_load -C $primary_ctx -u $seal_key_pub -r $seal_key_priv -c $seal_key_ctx
 
 # Satisfy policy and unseal data
 tpm2_startauthsession --policy-session -S $session_ctx
-echo -n "ownerauth" | tpm2_policysecret -S $session_ctx -c $TPM_RH_OWNER -L $o_policy_digest file:-
+echo -n "ownerauth" | tpm2_policysecret -S $session_ctx -c $TPM_RH_OWNER \
+-L $o_policy_digest file:-
 unsealed=`tpm2_unseal -p"session:$session_ctx" -c $seal_key_ctx`
 tpm2_flushcontext $session_ctx
 rm $session_ctx
@@ -55,7 +56,7 @@ rm $session_ctx
 test "$unsealed" == "$SEALED_SECRET"
 
 if [ $? != 0 ]; then
-  echo "Failed policysecret integration test where in ref object password was set."
+  echo "Failed policysecret integration test where ref object password was set."
 fi
 
 #Test the policy with auth reference object password not set
@@ -71,7 +72,7 @@ rm $session_ctx
 test "$unsealed" == "$SEALED_SECRET"
 
 if [ $? != 0 ]; then
-  echo "Failed policysecret integration test where in ref object is passwordless."
+  echo "Failed policysecret integration test for passwordless reference object."
 fi
 
 exit 0
