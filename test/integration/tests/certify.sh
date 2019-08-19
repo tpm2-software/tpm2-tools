2ab66d3e691e98fa62fdb4694f90ec1fe2b2b7b6
Imran Desai <imran.desai@intel.com>
2019-08-19 10:02:23 -0700

    certify.sh: fixed white spaces and remove spurious quotations
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- test/integration/tests/certify.sh
+++ test/integration/tests/certify.sh
@@ -4,7 +4,7 @@ source helpers.sh
 
 cleanup() {
     rm -f primary.ctx certify.ctx certify.pub certify.priv certify.name \
-          attest.out sig.out &>/dev/null
+    attest.out sig.out &>/dev/null
 
     if [ "$1" != "no-shut-down" ]; then
         shut_down
@@ -20,9 +20,10 @@ tpm2_clear -Q
 
 tpm2_createprimary -Q -C e -g sha256 -G rsa -c primary.ctx
 
-tpm2_create -Q -g sha256 -G rsa -u certify.pub -r certify.priv  -C primary.ctx
+tpm2_create -Q -g sha256 -G rsa -u certify.pub -r certify.priv -C primary.ctx
 
-tpm2_load -Q -C primary.ctx -u certify.pub -r certify.priv -n certify.name -c certify.ctx
+tpm2_load -Q -C primary.ctx -u certify.pub -r certify.priv -n certify.name \
+-c certify.ctx
 
 tpm2_certify -Q -C primary.ctx -c certify.ctx -g sha256 -o attest.out -s sig.out
 
