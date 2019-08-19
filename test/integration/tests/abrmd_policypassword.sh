2a762dd6810383f8bdc612851f342dee99d66b4a
Imran Desai <imran.desai@intel.com>
2019-08-19 09:47:31 -0700

    abrmd_policypassword.sh: fixed white spaces
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- test/integration/tests/abrmd_policypassword.sh
+++ test/integration/tests/abrmd_policypassword.sh
@@ -15,8 +15,8 @@ decrypted_txt=dec.txt
 testpswd=testpswd
 
 cleanup() {
-    rm -f  $policypassword $session_ctx $o_policy_digest $primary_key_ctx $key_ctx\
-    $key_pub $key_priv $plain_txt $encrypted_txt $decrypted_txt
+    rm -f $policypassword $session_ctx $o_policy_digest $primary_key_ctx \
+    $key_ctx $key_pub $key_priv $plain_txt $encrypted_txt $decrypted_txt
 
     tpm2_flushcontext $session_ctx 2>/dev/null || true
 
@@ -40,7 +40,7 @@ rm $session_ctx
 tpm2_createprimary -C o -c $primary_key_ctx
 
 tpm2_create -g sha256 -G aes -u $key_pub -r $key_priv -C $primary_key_ctx \
-  -L $policypassword -p $testpswd
+-L $policypassword -p $testpswd
 
 tpm2_load -C $primary_key_ctx -u $key_pub -r $key_priv -c $key_ctx
 tpm2_encryptdecrypt -c $key_ctx -o $encrypted_txt -p $testpswd $plain_txt
@@ -48,7 +48,7 @@ tpm2_encryptdecrypt -c $key_ctx -o $encrypted_txt -p $testpswd $plain_txt
 tpm2_startauthsession --policy-session -S $session_ctx
 tpm2_policypassword -S $session_ctx -L $policypassword
 tpm2_encryptdecrypt -c $key_ctx -o $decrypted_txt -d \
-  -p session:$session_ctx+$testpswd $encrypted_txt
+-p session:$session_ctx+$testpswd $encrypted_txt
 tpm2_flushcontext $session_ctx
 rm $session_ctx
 
