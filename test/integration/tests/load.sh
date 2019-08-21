efa6268c84c6af635c1ede85c1f5bf4302899530
Imran Desai <imran.desai@intel.com>
2019-08-21 11:09:36 -0700

    load.sh: fixed white spaces
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- test/integration/tests/load.sh
+++ test/integration/tests/load.sh
@@ -14,8 +14,10 @@ alg_load=sha1
 file_primary_key_ctx=context.p_"$alg_primary_obj"_"$alg_primary_key"
 file_load_key_pub=opu_"$alg_create_obj"_"$alg_create_key"
 file_load_key_priv=opr_"$alg_create_obj"_"$alg_create_key"
-file_load_key_name=name.load_"$alg_primary_obj"_"$alg_primary_key"-"$alg_create_obj"_"$alg_create_key"
-file_load_key_ctx=ctx_load_out_"$alg_primary_obj"_"$alg_primary_key"-"$alg_create_obj"_"$alg_create_key"
+file_load_key_name=name.load_"$alg_primary_obj"_"$alg_primary_key"-\
+"$alg_create_obj"_"$alg_create_key"
+file_load_key_ctx=ctx_load_out_"$alg_primary_obj"_"$alg_primary_key"-\
+"$alg_create_obj"_"$alg_create_key"
 file_load_output=load_"$file_load_key_ctx"
 
 Handle_parent=0x81010018
@@ -23,7 +25,8 @@ Handle_ek_load=0x81010017
 
 cleanup() {
 
-  rm -f $file_load_key_pub $file_load_key_priv $file_load_key_name $file_load_key_ctx
+  rm -f $file_load_key_pub $file_load_key_priv $file_load_key_name \
+  $file_load_key_ctx
 
   tpm2_evictcontrol -Q -Co -c $Handle_parent 2>/dev/null || true
 
@@ -43,11 +46,14 @@ tpm2_clear
 
 #####file test
 
-tpm2_createprimary -Q -C e -g $alg_primary_obj -G $alg_primary_key -c $file_primary_key_ctx
+tpm2_createprimary -Q -C e -g $alg_primary_obj -G $alg_primary_key \
+-c $file_primary_key_ctx
 
-tpm2_create -Q -g $alg_create_obj -G $alg_create_key -u $file_load_key_pub -r $file_load_key_priv  -C $file_primary_key_ctx
+tpm2_create -Q -g $alg_create_obj -G $alg_create_key -u $file_load_key_pub \
+-r $file_load_key_priv -C $file_primary_key_ctx
 
-tpm2_load -Q -C $file_primary_key_ctx  -u $file_load_key_pub  -r $file_load_key_priv -n $file_load_key_name -c $file_load_key_ctx
+tpm2_load -Q -C $file_primary_key_ctx -u $file_load_key_pub \
+-r $file_load_key_priv -n $file_load_key_name -c $file_load_key_ctx
 
 #####handle test
 
@@ -55,8 +61,10 @@ cleanup "keep_ctx" "no-shut-down"
 
 tpm2_evictcontrol -Q -C o -c $file_primary_key_ctx $Handle_parent
 
-tpm2_create -Q -C $Handle_parent   -g $alg_create_obj  -G $alg_create_key -u $file_load_key_pub  -r  $file_load_key_priv
+tpm2_create -Q -C $Handle_parent -g $alg_create_obj -G $alg_create_key \
+-u $file_load_key_pub  -r $file_load_key_priv
 
-tpm2_load -Q -C $Handle_parent   -u $file_load_key_pub  -r $file_load_key_priv -n $file_load_key_name -c $file_load_key_ctx
+tpm2_load -Q -C $Handle_parent -u $file_load_key_pub -r $file_load_key_priv \
+-n $file_load_key_name -c $file_load_key_ctx
 
 exit 0
