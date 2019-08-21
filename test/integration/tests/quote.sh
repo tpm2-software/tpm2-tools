379a2234ea8b7aa9d1c6fc937c522207cd398163
Imran Desai <imran.desai@intel.com>
2019-08-21 11:26:07 -0700

    quote.sh: fixed white spaces
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- test/integration/tests/quote.sh
+++ test/integration/tests/quote.sh
@@ -13,8 +13,10 @@ alg_quote1=0x000b
 file_primary_key_ctx=context.p_"$alg_primary_obj"_"$alg_primary_key"
 file_quote_key_pub=opu_"$alg_create_obj"_"$alg_create_key"
 file_quote_key_priv=opr_"$alg_create_obj"_"$alg_create_key"
-file_quote_key_name=name.load_"$alg_primary_obj"_"$alg_primary_key"-"$alg_create_obj"_"$alg_create_key"
-file_quote_key_ctx=ctx_load_out_"$alg_primary_obj"_"$alg_primary_key"-"$alg_create_obj"_"$alg_create_key"
+file_quote_key_name=name.load_"$alg_primary_obj"_"$alg_primary_key"-\
+"$alg_create_obj"_"$alg_create_key"
+file_quote_key_ctx=ctx_load_out_"$alg_primary_obj"_"$alg_primary_key"-\
+"$alg_create_obj"_"$alg_create_key"
 
 Handle_ak_quote=0x81010016
 Handle_ek_quote=0x81010017
@@ -55,24 +57,32 @@ cleanup "no-shut-down"
 
 tpm2_clear
 
-tpm2_createprimary -Q -C e -g $alg_primary_obj -G $alg_primary_key -c $file_primary_key_ctx
+tpm2_createprimary -Q -C e -g $alg_primary_obj -G $alg_primary_key \
+-c $file_primary_key_ctx
 
-tpm2_create -Q -g $alg_create_obj -G $alg_create_key -u $file_quote_key_pub -r $file_quote_key_priv  -C $file_primary_key_ctx
+tpm2_create -Q -g $alg_create_obj -G $alg_create_key -u $file_quote_key_pub \
+-r $file_quote_key_priv -C $file_primary_key_ctx
 
-tpm2_load -Q -C $file_primary_key_ctx  -u $file_quote_key_pub  -r $file_quote_key_priv -n $file_quote_key_name -c $file_quote_key_ctx
+tpm2_load -Q -C $file_primary_key_ctx -u $file_quote_key_pub \
+-r $file_quote_key_priv -n $file_quote_key_name -c $file_quote_key_ctx
 
-tpm2_quote -c $file_quote_key_ctx  -l $alg_quote:16,17,18 -q $nonce -m $toss_out -s $toss_out -o $toss_out -g $alg_primary_obj > $out
+tpm2_quote -c $file_quote_key_ctx -l $alg_quote:16,17,18 -q $nonce \
+-m $toss_out -s $toss_out -o $toss_out -g $alg_primary_obj > $out
 
 yaml_verify $out
 
-tpm2_quote -Q -c $file_quote_key_ctx  -l $alg_quote:16,17,18+$alg_quote1:16,17,18 -q $nonce -m $toss_out -s $toss_out -o $toss_out -g $alg_primary_obj
+tpm2_quote -Q -c $file_quote_key_ctx \
+-l $alg_quote:16,17,18+$alg_quote1:16,17,18 -q $nonce -m $toss_out \
+-s $toss_out -o $toss_out -g $alg_primary_obj
 
 #####handle testing
 tpm2_evictcontrol -Q -C o -c $file_quote_key_ctx $Handle_ak_quote
 
-tpm2_quote -Q -c $Handle_ak_quote -l $alg_quote:16,17,18 -q $nonce -m $toss_out -s $toss_out -o $toss_out -g $alg_primary_obj
+tpm2_quote -Q -c $Handle_ak_quote -l $alg_quote:16,17,18 -q $nonce \
+-m $toss_out -s $toss_out -o $toss_out -g $alg_primary_obj
 
-tpm2_quote -Q -c $Handle_ak_quote  -l $alg_quote:16,17,18+$alg_quote1:16,17,18 -q $nonce -m $toss_out -s $toss_out -o $toss_out -g $alg_primary_obj
+tpm2_quote -Q -c $Handle_ak_quote -l $alg_quote:16,17,18+$alg_quote1:16,17,18 \
+-q $nonce -m $toss_out -s $toss_out -o $toss_out -g $alg_primary_obj
 
 #####AK
 tpm2_createek -Q -c $Handle_ek_quote -G 0x01 -p ek.pub2
@@ -80,6 +90,7 @@ tpm2_createek -Q -c $Handle_ek_quote -G 0x01 -p ek.pub2
 tpm2_createak -Q -C $Handle_ek_quote -c $ak2_ctx -u ak.pub2 -n ak.name_2
 tpm2_evictcontrol -Q -C o -c $ak2_ctx $Handle_ak_quote2
 
-tpm2_quote -Q -c $Handle_ak_quote -l $alg_quote:16,17,18 -q $nonce -m $toss_out -s $toss_out -o $toss_out -g $alg_primary_obj
+tpm2_quote -Q -c $Handle_ak_quote -l $alg_quote:16,17,18 -q $nonce \
+-m $toss_out -s $toss_out -o $toss_out -g $alg_primary_obj
 
 exit 0
