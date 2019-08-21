0fb668de2a8af4faa02c87361e6981c426383ebc
Imran Desai <imran.desai@intel.com>
2019-08-21 11:28:16 -0700

    readpublic.sh: fixed white spaces
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- test/integration/tests/readpublic.sh
+++ test/integration/tests/readpublic.sh
@@ -10,8 +10,10 @@ alg_create_key=hmac
 file_primary_key_ctx=context.p_"$alg_primary_obj"_"$alg_primary_key"
 file_readpub_key_pub=opu_"$alg_create_obj"_"$alg_create_key"
 file_readpub_key_priv=opr_"$alg_create_obj"_"$alg_create_key"
-file_readpub_key_name=name.load_"$alg_primary_obj"_"$alg_primary_key"-"$alg_create_obj"_"$alg_create_key"
-file_readpub_key_ctx=ctx_load_out_"$alg_primary_obj"_"$alg_primary_key"-"$alg_create_obj"_"$alg_create_key"
+file_readpub_key_name=name.load_"$alg_primary_obj"_"$alg_primary_key"-\
+"$alg_create_obj"_"$alg_create_key"
+file_readpub_key_ctx=ctx_load_out_"$alg_primary_obj"_"$alg_primary_key"-\
+"$alg_create_obj"_"$alg_create_key"
 file_readpub_output=readpub_"$file_readpub_key_ctx"
 
 Handle_readpub=0x81010014
@@ -34,11 +36,14 @@ cleanup "no-shut-down"
 
 tpm2_clear
 
-tpm2_createprimary -Q -C e -g $alg_primary_obj -G $alg_primary_key -c $file_primary_key_ctx
+tpm2_createprimary -Q -C e -g $alg_primary_obj -G $alg_primary_key \
+-c $file_primary_key_ctx
 
-tpm2_create -Q -g $alg_create_obj -G $alg_create_key -u $file_readpub_key_pub -r $file_readpub_key_priv  -C $file_primary_key_ctx
+tpm2_create -Q -g $alg_create_obj -G $alg_create_key -u $file_readpub_key_pub \
+-r $file_readpub_key_priv -C $file_primary_key_ctx
 
-tpm2_load -Q -C $file_primary_key_ctx  -u $file_readpub_key_pub  -r $file_readpub_key_priv -n $file_readpub_key_name -c $file_readpub_key_ctx
+tpm2_load -Q -C $file_primary_key_ctx -u $file_readpub_key_pub \
+-r $file_readpub_key_priv -n $file_readpub_key_name -c $file_readpub_key_ctx
 
 tpm2_readpublic -Q -c $file_readpub_key_ctx -o $file_readpub_output
 
