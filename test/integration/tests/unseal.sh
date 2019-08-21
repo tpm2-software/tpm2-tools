43f1b9259c01754999bbe2cd0cb155b0769cfcd0
Imran Desai <imran.desai@intel.com>
2019-08-21 11:54:29 -0700

    unseal.sh: fixed white spaces
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- test/integration/tests/unseal.sh
+++ test/integration/tests/unseal.sh
@@ -12,8 +12,10 @@ file_policy=policy.data
 file_primary_key_ctx=context.p_"$alg_primary_obj"_"$alg_primary_key"
 file_unseal_key_pub=opu_"$alg_create_obj"
 file_unseal_key_priv=opr_"$alg_create_obj"
-file_unseal_key_ctx=ctx_load_out_"$alg_primary_obj"_"$alg_primary_key"-"$alg_create_obj"
-file_unseal_key_name=name.load_"$alg_primary_obj"_"$alg_primary_key"-"$alg_create_obj"
+file_unseal_key_ctx=ctx_load_out_"$alg_primary_obj"_"$alg_primary_key"-\
+"$alg_create_obj"
+file_unseal_key_name=name.load_"$alg_primary_obj"_"$alg_primary_key"-\
+"$alg_create_obj"
 file_unseal_output_data=usl_"$file_unseal_key_ctx"
 
 secret="12345678"
@@ -37,11 +39,14 @@ echo $secret > $file_input_data
 
 tpm2_clear
 
-tpm2_createprimary -Q -C e -g $alg_primary_obj -G $alg_primary_key -c $file_primary_key_ctx
+tpm2_createprimary -Q -C e -g $alg_primary_obj -G $alg_primary_key \
+-c $file_primary_key_ctx
 
-tpm2_create -Q -g $alg_create_obj -u $file_unseal_key_pub -r $file_unseal_key_priv -i $file_input_data -C $file_primary_key_ctx
+tpm2_create -Q -g $alg_create_obj -u $file_unseal_key_pub \
+-r $file_unseal_key_priv -i $file_input_data -C $file_primary_key_ctx
 
-tpm2_load -Q -C $file_primary_key_ctx  -u $file_unseal_key_pub  -r $file_unseal_key_priv -n $file_unseal_key_name -c $file_unseal_key_ctx
+tpm2_load -Q -C $file_primary_key_ctx -u $file_unseal_key_pub \
+-r $file_unseal_key_priv -n $file_unseal_key_name -c $file_unseal_key_ctx
 
 tpm2_unseal -Q -c $file_unseal_key_ctx -o $file_unseal_output_data
 
@@ -49,11 +54,14 @@ cmp -s $file_unseal_output_data $file_input_data
 
 # Test -i using stdin via pipe
 
-rm $file_unseal_key_pub $file_unseal_key_priv $file_unseal_key_name $file_unseal_key_ctx
+rm $file_unseal_key_pub $file_unseal_key_priv $file_unseal_key_name \
+$file_unseal_key_ctx
 
-cat $file_input_data | tpm2_create -Q -g $alg_create_obj -u $file_unseal_key_pub -r $file_unseal_key_priv -i- -C $file_primary_key_ctx
+cat $file_input_data | tpm2_create -Q -g $alg_create_obj \
+-u $file_unseal_key_pub -r $file_unseal_key_priv -i- -C $file_primary_key_ctx
 
-tpm2_load -Q -C $file_primary_key_ctx  -u $file_unseal_key_pub  -r $file_unseal_key_priv -n $file_unseal_key_name -c $file_unseal_key_ctx
+tpm2_load -Q -C $file_primary_key_ctx -u $file_unseal_key_pub \
+-r $file_unseal_key_priv -n $file_unseal_key_name -c $file_unseal_key_ctx
 
 tpm2_unseal -Q -c $file_unseal_key_ctx -o $file_unseal_output_data
 
@@ -61,18 +69,23 @@ cmp -s $file_unseal_output_data $file_input_data
 
 # Test using a PCR policy for auth and use file based stdin for -i
 
-rm $file_unseal_key_pub $file_unseal_key_priv $file_unseal_key_name $file_unseal_key_ctx
+rm $file_unseal_key_pub $file_unseal_key_priv $file_unseal_key_name \
+$file_unseal_key_ctx
 
 tpm2_pcrread -Q -o $file_pcr_value $pcr_specification
 
-tpm2_createpolicy -Q --policy-pcr -l $pcr_specification -f $file_pcr_value -L $file_policy
+tpm2_createpolicy -Q --policy-pcr -l $pcr_specification -f $file_pcr_value \
+-L $file_policy
 
-tpm2_create -Q -g $alg_create_obj -u $file_unseal_key_pub -r $file_unseal_key_priv -i- -C $file_primary_key_ctx -L $file_policy \
-  -a 'fixedtpm|fixedparent' <<< $secret
+tpm2_create -Q -g $alg_create_obj -u $file_unseal_key_pub \
+-r $file_unseal_key_priv -i- -C $file_primary_key_ctx -L $file_policy \
+-a 'fixedtpm|fixedparent' <<< $secret
 
-tpm2_load -Q -C $file_primary_key_ctx  -u $file_unseal_key_pub  -r $file_unseal_key_priv -n $file_unseal_key_name -c $file_unseal_key_ctx
+tpm2_load -Q -C $file_primary_key_ctx -u $file_unseal_key_pub \
+-r $file_unseal_key_priv -n $file_unseal_key_name -c $file_unseal_key_ctx
 
-unsealed=`tpm2_unseal -V --object-context $file_unseal_key_ctx -p pcr:$pcr_specification=$file_pcr_value`
+unsealed=`tpm2_unseal -V --object-context $file_unseal_key_ctx \
+-p pcr:$pcr_specification=$file_pcr_value`
 
 test "$unsealed" == "$secret"
 
@@ -100,15 +113,20 @@ fi
 
 trap onerror ERR
 
-rm $file_unseal_key_pub $file_unseal_key_priv $file_unseal_key_name $file_unseal_key_ctx
+rm $file_unseal_key_pub $file_unseal_key_priv $file_unseal_key_name \
+$file_unseal_key_ctx
 
 tpm2_pcrread -Q -o $file_pcr_value $pcr_specification
 
-tpm2_createpolicy -Q --policy-pcr -l $pcr_specification -f $file_pcr_value -L $file_policy
+tpm2_createpolicy -Q --policy-pcr -l $pcr_specification -f $file_pcr_value \
+-L $file_policy
 
-tpm2_create -Q -g $alg_create_obj -u $file_unseal_key_pub -r $file_unseal_key_priv -i- -C $file_primary_key_ctx -L $file_policy -p secretpass <<< $secret
+tpm2_create -Q -g $alg_create_obj -u $file_unseal_key_pub \
+-r $file_unseal_key_priv -i- -C $file_primary_key_ctx -L $file_policy \
+-p secretpass <<< $secret
 
-tpm2_load -Q -C $file_primary_key_ctx  -u $file_unseal_key_pub  -r $file_unseal_key_priv -n $file_unseal_key_name -c $file_unseal_key_ctx
+tpm2_load -Q -C $file_primary_key_ctx -u $file_unseal_key_pub \
+-r $file_unseal_key_priv -n $file_unseal_key_name -c $file_unseal_key_ctx
 
 unsealed=`tpm2_unseal -c $file_unseal_key_ctx -p secretpass`
 
