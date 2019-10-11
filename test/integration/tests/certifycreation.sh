7a86472a5849f80dae9fbc34bb12f274bdb3cf53
Imran Desai <imran.desai@intel.com>
2019-10-11 15:33:07 -0700

    certifycreation.sh: Add test for tpm2_certifycreation
    Signed-off-by: Imran Desai <imran.desai@intel.com>

---
+++ test/integration/tests/certifycreation.sh
@@ -0,0 +1,35 @@
+# SPDX-License-Identifier: BSD-3-Clause
+
+source helpers.sh
+
+cleanup() {
+    rm -f  primary.ctx creation.data creation.digest creation.ticket rsa.pub \
+    rsa.priv signature.bin attestation.bin sslpub.pem
+
+    if [ "$1" != "no-shut-down" ]; then
+        shut_down
+    fi
+}
+trap cleanup EXIT
+
+start_up
+
+cleanup "no-shut-down"
+
+tpm2_clear -Q
+
+tpm2_createprimary -C o -c primary.ctx --creation-data creation.data \
+-d creation.digest -t creation.ticket -Q
+
+tpm2_create -G rsa -u rsa.pub -r rsa.priv -C primary.ctx -c signing_key.ctx -Q
+
+tpm2_readpublic -c signing_key.ctx -f pem -o sslpub.pem
+
+tpm2_certifycreation -C signing_key.ctx -c primary.ctx -d creation.digest \
+-t creation.ticket -g sha256 -o signature.bin --attestation attestation.bin \
+-f plain -s rsassa
+
+dd if=attestation.bin bs=1 skip=2 | \
+openssl dgst -verify sslpub.pem -keyform pem -sha256 -signature signature.bin
+
+exit 0
