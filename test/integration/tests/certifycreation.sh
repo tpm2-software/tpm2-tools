767ef4c9cb66176b484b293f6a05e0c1b5c99f03
Imran Desai <imran.desai@intel.com>
2019-10-11 17:34:58 -0700

    certifycreation.sh: Add test for specifying qualification data
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- test/integration/tests/certifycreation.sh
+++ test/integration/tests/certifycreation.sh
@@ -4,7 +4,7 @@ source helpers.sh
 
 cleanup() {
     rm -f  primary.ctx creation.data creation.digest creation.ticket rsa.pub \
-    rsa.priv signature.bin attestation.bin sslpub.pem
+    rsa.priv signature.bin attestation.bin sslpub.pem qual.dat
 
     if [ "$1" != "no-shut-down" ]; then
         shut_down
@@ -28,6 +28,18 @@ tpm2_readpublic -c signing_key.ctx -f pem -o sslpub.pem
 tpm2_certifycreation -C signing_key.ctx -c primary.ctx -d creation.digest \
 -t creation.ticket -g sha256 -o signature.bin --attestation attestation.bin \
 -f plain -s rsassa
+
+dd if=attestation.bin bs=1 skip=2 | \
+openssl dgst -verify sslpub.pem -keyform pem -sha256 -signature signature.bin
+
+#
+# Test with qualifier data
+#
+dd if=/dev/urandom of=qual.dat bs=1 count=32
+
+tpm2_certifycreation -C signing_key.ctx -c primary.ctx -d creation.digest \
+-t creation.ticket -g sha256 -o signature.bin --attestation attestation.bin \
+-f plain -s rsassa -q qual.dat
 
 dd if=attestation.bin bs=1 skip=2 | \
 openssl dgst -verify sslpub.pem -keyform pem -sha256 -signature signature.bin
