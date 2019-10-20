e78a65680c974fe3b41b6e135678490184a80806
Imran Desai <imran.desai@intel.com>
2019-10-20 12:13:54 -0700

    nvcertify.sh: Add test to validate signatures with qualification data
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- test/integration/tests/nvcertify.sh
+++ test/integration/tests/nvcertify.sh
@@ -4,7 +4,7 @@ source helpers.sh
 
 cleanup() {
     rm -f  primary.ctx signing_key.pub signing_key.priv signature.bin attestation.bin \
-    sslpub.pem signing_key.ctx
+    sslpub.pem signing_key.ctx qual.dat
 
     if [ "$1" != "no-shut-down" ]; then
         shut_down
@@ -34,5 +34,21 @@ tpm2_nvcertify -C signing_key.ctx -g sha256 -f plain -s rsassa \
 
 dd if=attestation.bin bs=1 skip=2 | \
 openssl dgst -verify sslpub.pem -keyform pem -sha256 -signature signature.bin
+
+#
+# Test with qualifier data
+#
+dd if=/dev/urandom of=qual.dat bs=1 count=32
+
+tpm2_nvcertify -C signing_key.ctx -g sha256 -f plain -s rsassa \
+-o signature.bin --attestation attestation.bin --size 32 -q qual.dat 1
+
+dd if=attestation.bin bs=1 skip=2 | \
+openssl dgst -verify sslpub.pem -keyform pem -sha256 -signature signature.bin
+
+#
+# Test if qualifier data was present in the attestation
+#
+xxd -p attestation.bin | tr -d '\n' | grep `xxd -p qual.dat | tr -d '\n'`
 
 exit 0
