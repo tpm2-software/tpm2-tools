c8361990c33e30e86e26af4545c16eec69766a30
Imran Desai <imran.desai@intel.com>
2019-11-04 14:02:51 -0700

    abrmd_policytemplate.sh: Add integration test with tpm2_create
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- test/integration/tests/abrmd_policytemplate.sh
+++ test/integration/tests/abrmd_policytemplate.sh
@@ -3,7 +3,7 @@
 source helpers.sh
 
 cleanup() {
-    rm -f prim.ctx template.data template.hash policy.template
+    rm -f prim.ctx template.data template.hash policy.template key.pub key.priv
     tpm2_flushcontext session.ctx 2>/dev/null || true
     if [ "${1}" != "no-shutdown" ]; then
         shut_down
@@ -55,7 +55,61 @@ trap - ERR
 
 tpm2_createprimary -C o -G ecc -c prim2.ctx -P session:session.ctx
 if [ $? == 0 ];then
-  echo "ERROR: Expected tpm2_reateprimary should fail!"
+  echo "ERROR: Expected tpm2_createprimary should fail!"
+  exit 1
+fi
+
+trap onerror ERR
+
+tpm2_flushcontext session.ctx
+
+
+
+#
+# Restrict the object type created under a primary key
+#
+
+tpm2_clear
+
+## Create a key template
+tpm2_createprimary -C o -c prim.ctx -Q
+tpm2_create -C prim.ctx -u key.pub -r key.priv --template-data template.data -Q
+
+cat template.data | openssl dgst -sha256 -binary -out template.hash
+
+rm -f prim.ctx key.pub key.priv template.data
+
+## Create the policytemplate
+tpm2_startauthsession -S session.ctx -g sha256
+
+tpm2_policytemplate -S session.ctx -L policy.template \
+--template-hash template.hash
+
+tpm2_flushcontext session.ctx
+
+## Set the primary key auth policy to create keys of specific template
+tpm2_createprimary -C o -c prim.ctx -L policy.template -Q
+
+## Satisfy the policy and create a key
+tpm2_startauthsession -S session.ctx -g sha256 --policy-session
+
+tpm2_policytemplate -S session.ctx --template-hash template.hash
+
+tpm2_create -C prim.ctx -u key.pub -r key.priv -P session:session.ctx -Q
+
+tpm2_flushcontext session.ctx
+
+## Attempt to create a key with a different template
+
+tpm2_startauthsession -S session.ctx -g sha256 --policy-session
+
+tpm2_policytemplate -S session.ctx --template-hash template.hash
+
+trap - ERR
+
+tpm2_create -C prim.ctx -G ecc -u key.pub -r key.priv -P session:session.ctx
+if [ $? == 0 ];then
+  echo "ERROR: Expected tpm2_createprimary should fail!"
   exit 1
 fi
 
