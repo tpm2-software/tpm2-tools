e29fb431f936d684d572ccbc1de01266b41bb61e
pengchen-zpc <pengchen.zpc@alibaba-inc.com>
2019-11-12 14:08:09 +0800

    pcr.c: Add unit tests for pcr_parse_selections
    Signed-off-by: pengchen-zpc <pengchen.zpc@alibaba-inc.com>

--- test/unit/test_pcr.c
+++ test/unit/test_pcr.c
@@ -30,6 +30,42 @@ static void test_pcr_alg_nice_names(void **state) {
 
     assert_memory_equal(&friendly_pcr_selections, &raw_pcr_selections,
             sizeof(raw_pcr_selections));
+
+    // select from PCR bank sm3_256
+    TPML_PCR_SELECTION friendly_pcr_selections_sm3 =
+    TPML_PCR_SELECTION_EMPTY_INIT;
+
+    bool result_sm3 = pcr_parse_selections("sm3_256:16,17,18+0x12:16,17,18",
+            &friendly_pcr_selections_sm3);
+    assert_true(result_sm3);
+
+    TPML_PCR_SELECTION raw_pcr_selections_sm3 =
+    TPML_PCR_SELECTION_EMPTY_INIT;
+
+    result_sm3 = pcr_parse_selections("0x12:16,17,18+0x12:16,17,18",
+            &raw_pcr_selections_sm3);
+    assert_true(result_sm3);
+
+    assert_memory_equal(&friendly_pcr_selections_sm3, &raw_pcr_selections_sm3,
+            sizeof(raw_pcr_selections_sm3));
+
+    // select from PCR bank sha3_256
+    TPML_PCR_SELECTION friendly_pcr_selections_sha3_256 =
+    TPML_PCR_SELECTION_EMPTY_INIT;
+
+    bool result_sha3 = pcr_parse_selections("sha3_256:16,17,18+0x27:16,17,18",
+            &friendly_pcr_selections_sha3_256);
+    assert_true(result_sha3);
+
+    TPML_PCR_SELECTION raw_pcr_selections_sha3_256 =
+    TPML_PCR_SELECTION_EMPTY_INIT;
+
+    result_sha3 = pcr_parse_selections("0x27:16,17,18+0x27:16,17,18",
+            &raw_pcr_selections_sha3_256);
+    assert_true(result_sha3);
+
+    assert_memory_equal(&friendly_pcr_selections_sha3_256, &raw_pcr_selections_sha3_256,
+            sizeof(raw_pcr_selections_sha3_256));
 }
 
 /* link required symbol, but tpm2_tool.c declares it AND main, which
