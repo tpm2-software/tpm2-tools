25791957777d97584162fffe27ddf72a1539385b
Imran Desai <imran.desai@intel.com>
2020-02-03 11:30:21 -0700

    tpm2_nvdefine: Add an option to output cpHash for the command
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- lib/tpm2.h
+++ lib/tpm2.h
@@ -279,7 +279,7 @@ tool_rc tpm2_import(ESYS_CONTEXT *esys_context, tpm2_loaded_object *parent_obj,
 
 tool_rc tpm2_nv_definespace(ESYS_CONTEXT *esys_context,
         tpm2_loaded_object *auth_hierarchy_obj, const TPM2B_AUTH *auth,
-        const TPM2B_NV_PUBLIC *public_info);
+        const TPM2B_NV_PUBLIC *public_info, TPM2B_DIGEST *cp_hash);
 
 tool_rc tpm2_nvextend(ESYS_CONTEXT *esys_context,
         tpm2_loaded_object *auth_hierarchy_obj, TPM2_HANDLE nv_index,
