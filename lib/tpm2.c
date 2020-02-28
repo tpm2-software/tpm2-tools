f541d5e1efe37ee99ac9efd29912651a07f9e163
Imran Desai <imran.desai@intel.com>
2020-02-28 15:51:04 -0700

    tpm2_setclock: Add an option to output cpHash for the command
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- lib/tpm2.c
+++ lib/tpm2.c
@@ -3600,7 +3600,8 @@ tool_rc tpm2_readclock(ESYS_CONTEXT *ectx, TPMS_TIME_INFO **current_time) {
     return tool_rc_success;
 }
 
-tool_rc tpm2_setclock(ESYS_CONTEXT *ectx, tpm2_loaded_object *object, UINT64 new_time) {
+tool_rc tpm2_setclock(ESYS_CONTEXT *ectx, tpm2_loaded_object *object,
+UINT64 new_time, TPM2B_DIGEST *cp_hash) {
 
     ESYS_TR shandle1 = ESYS_TR_NONE;
     tool_rc rc = tpm2_auth_util_get_shandle(ectx,
@@ -3608,6 +3609,44 @@ tool_rc tpm2_setclock(ESYS_CONTEXT *ectx, tpm2_loaded_object *object, UINT64 new
     if (rc != tool_rc_success) {
         LOG_ERR("Couldn't get shandle for lockout hierarchy");
         return rc;
+    }
+
+    if (cp_hash) {
+        /*
+         * Need sys_context to be able to calculate CpHash
+         */
+        TSS2_SYS_CONTEXT *sys_context = NULL;
+        rc = tpm2_getsapicontext(ectx, &sys_context);
+        if(rc != tool_rc_success) {
+            LOG_ERR("Failed to acquire SAPI context.");
+            return rc;
+        }
+
+        TSS2_RC rval = Tss2_Sys_ClockSet_Prepare(sys_context,
+            object->handle, new_time);
+        if (rval != TPM2_RC_SUCCESS) {
+            LOG_PERR(Tss2_Sys_ClockSet_Prepare, rval);
+            return tool_rc_general_error;
+        }
+
+        TPM2B_NAME *name1 = NULL;
+        rc = tpm2_tr_get_name(ectx, object->tr_handle,
+            &name1);
+        if (rc != tool_rc_success) {
+            goto tpm2_setclock_free_name1;
+        }
+
+        cp_hash->size = tpm2_alg_util_get_hash_size(
+            tpm2_session_get_authhash(object->session));
+        rc = tpm2_sapi_getcphash(sys_context, name1, NULL, NULL,
+            tpm2_session_get_authhash(object->session), cp_hash);
+
+        /*
+         * Exit here without making the ESYS call since we just need the cpHash
+         */
+tpm2_setclock_free_name1:
+        Esys_Free(name1);
+        goto tpm2_setclock_skip_esapi_call;
     }
 
     TSS2_RC rval = Esys_ClockSet(ectx,
@@ -3619,7 +3658,8 @@ tool_rc tpm2_setclock(ESYS_CONTEXT *ectx, tpm2_loaded_object *object, UINT64 new
         return tool_rc_from_tpm(rval);
     }
 
-    return tool_rc_success;
+tpm2_setclock_skip_esapi_call:
+    return rc;
 }
 
 tool_rc tpm2_clockrateadjust(ESYS_CONTEXT *ectx, tpm2_loaded_object *object,
