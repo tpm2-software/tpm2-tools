b95308f6120c938e27475851e9978f1eab10ebf0
Trammell Hudson <hudson@trmm.net>
2020-06-09 11:11:00 +0200

    tpm2_eventlog: updat etext to use callback struct
    Signed-off-by: Trammell Hudson <hudson@trmm.net>

--- test/unit/test_tpm2_eventlog.c
+++ test/unit/test_tpm2_eventlog.c
@@ -24,16 +24,18 @@ static bool foreach_digest2_test_callback(TCG_DIGEST2 const *digest, size_t size
 static void test_foreach_digest2_null(void **state){
 
     (void)state;
+    tpm2_eventlog_ctx_t ctx = {};
 
-    assert_false(foreach_digest2(NULL, 0, sizeof(TCG_DIGEST2), NULL, NULL));
+    assert_false(foreach_digest2(&ctx, 0, NULL, 0, sizeof(TCG_DIGEST2)));
 }
 static void test_foreach_digest2_size(void **state) {
 
     (void)state;
     uint8_t buf [sizeof(TCG_DIGEST2) - 1] = { 0, };
     TCG_DIGEST2 *digest = (TCG_DIGEST2*)buf;
+    tpm2_eventlog_ctx_t ctx = { .digest2_cb = foreach_digest2_test_callback };
 
-    assert_false(foreach_digest2(digest, 1, sizeof(TCG_DIGEST2) - 1, foreach_digest2_test_callback, NULL));
+    assert_false(foreach_digest2(&ctx, 0, digest, 1, sizeof(TCG_DIGEST2) - 1));
 }
 static void test_foreach_digest2(void **state) {
 
@@ -42,7 +44,9 @@ static void test_foreach_digest2(void **state) {
     TCG_DIGEST2* digest = (TCG_DIGEST2*)buf;
 
     will_return(foreach_digest2_test_callback, true);
-    assert_true(foreach_digest2(digest, 1, TCG_DIGEST2_SHA1_SIZE, foreach_digest2_test_callback, NULL));
+
+    tpm2_eventlog_ctx_t ctx = { .digest2_cb = foreach_digest2_test_callback };
+    assert_true(foreach_digest2(&ctx, 0, digest, 1, TCG_DIGEST2_SHA1_SIZE));
 }
 static void test_foreach_digest2_cbnull(void **state){
 
@@ -50,7 +54,8 @@ static void test_foreach_digest2_cbnull(void **state){
     uint8_t buf [TCG_DIGEST2_SHA1_SIZE] = { 0, };
     TCG_DIGEST2* digest = (TCG_DIGEST2*)buf;
 
-    assert_true(foreach_digest2(digest, 1, TCG_DIGEST2_SHA1_SIZE, NULL, NULL));
+    tpm2_eventlog_ctx_t ctx = {};
+    assert_true(foreach_digest2(&ctx, 0, digest, 1, TCG_DIGEST2_SHA1_SIZE));
 }
 static void test_foreach_digest2_cbfail(void **state){
 
@@ -59,7 +64,9 @@ static void test_foreach_digest2_cbfail(void **state){
     TCG_DIGEST2* digest = (TCG_DIGEST2*)buf;
 
     will_return(foreach_digest2_test_callback, false);
-    assert_false(foreach_digest2(digest, 1, TCG_DIGEST2_SHA1_SIZE, foreach_digest2_test_callback, NULL));
+
+    tpm2_eventlog_ctx_t ctx = { .digest2_cb = foreach_digest2_test_callback };
+    assert_false(foreach_digest2(&ctx, 0, digest, 1, TCG_DIGEST2_SHA1_SIZE));
 }
 static void test_digest2_accumulator_callback(void **state) {
 
@@ -146,12 +153,6 @@ static void test_parse_event2_badeventbuf(void **state){
 
     assert_false(parse_event2(eventhdr, sizeof(buf), &size_event, &size_digest));
 }
-static void test_foreach_event2_null(void **state){
-
-    (void)state;
-
-    assert_false(foreach_event2(NULL, 0, NULL, NULL, NULL, NULL));
-}
 static void test_foreach_event2(void **state){
 
     (void)state;
@@ -167,9 +168,13 @@ static void test_foreach_event2(void **state){
     will_return(test_event2hdr_callback, true);
     will_return(foreach_digest2_test_callback, true);
     will_return(test_event2_callback, true);
-    assert_true(foreach_event2(eventhdr, sizeof(buf), test_event2hdr_callback,
-                               foreach_digest2_test_callback,
-                               test_event2_callback, NULL));
+
+    tpm2_eventlog_ctx_t ctx = {
+        .digest2_cb = foreach_digest2_test_callback,
+        .event2_cb = test_event2_callback,
+        .event2hdr_cb = test_event2hdr_callback,
+    };
+    assert_true(foreach_event2(&ctx, eventhdr, sizeof(buf)));
 }
 static void test_foreach_event2_event2hdr_fail(void **state){
 
@@ -184,8 +189,11 @@ static void test_foreach_event2_event2hdr_fail(void **state){
     event->EventSize = 1;
 
     will_return(test_event2hdr_callback, false);
-    assert_false(foreach_event2(eventhdr, sizeof(buf), test_event2hdr_callback,
-                                NULL, NULL, NULL));
+
+    tpm2_eventlog_ctx_t ctx = {
+        .event2hdr_cb = test_event2hdr_callback,
+    };
+    assert_false(foreach_event2(&ctx, eventhdr, sizeof(buf)));
 }
 static void test_foreach_event2_digest2_fail(void **state){
 
@@ -201,9 +209,12 @@ static void test_foreach_event2_digest2_fail(void **state){
 
     will_return(test_event2hdr_callback, true);
     will_return(foreach_digest2_test_callback, false);
-    assert_false(foreach_event2(eventhdr, sizeof(buf),
-                                test_event2hdr_callback,
-                                foreach_digest2_test_callback, NULL, NULL));
+
+    tpm2_eventlog_ctx_t ctx = {
+        .digest2_cb = foreach_digest2_test_callback,
+        .event2hdr_cb = test_event2hdr_callback,
+    };
+    assert_false(foreach_event2(&ctx, eventhdr, sizeof(buf)));
 }
 static void test_foreach_event2_parse_event2body_fail(void **state){
 
@@ -221,9 +232,12 @@ static void test_foreach_event2_parse_event2body_fail(void **state){
 
     will_return(test_event2hdr_callback, true);
     will_return(foreach_digest2_test_callback, true);
-    assert_false(foreach_event2(eventhdr, sizeof(buf),
-                                test_event2hdr_callback,
-                                foreach_digest2_test_callback, NULL, NULL));
+
+    tpm2_eventlog_ctx_t ctx = {
+        .digest2_cb = foreach_digest2_test_callback,
+        .event2hdr_cb = test_event2hdr_callback,
+    };
+    assert_false(foreach_event2(&ctx, eventhdr, sizeof(buf)));
 }
 static void test_foreach_event2_event2body_fail(void **state){
 
@@ -240,10 +254,13 @@ static void test_foreach_event2_event2body_fail(void **state){
     will_return(test_event2hdr_callback, true);
     will_return(foreach_digest2_test_callback, true);
     will_return(test_event2_callback, false);
-    assert_false(foreach_event2(eventhdr, sizeof(buf),
-                                test_event2hdr_callback,
-                                foreach_digest2_test_callback,
-                                test_event2_callback, NULL));
+
+    tpm2_eventlog_ctx_t ctx = {
+        .event2hdr_cb = test_event2hdr_callback,
+        .digest2_cb = foreach_digest2_test_callback,
+        .event2_cb = test_event2_callback,
+    };
+    assert_false(foreach_event2(&ctx, eventhdr, sizeof(buf)));
 }
 static void test_parse_event2body_uefivar_badsize(void **state){
 
@@ -457,7 +474,6 @@ int main(void) {
         cmocka_unit_test(test_parse_event2_baddigest),
         cmocka_unit_test(test_parse_event2_badeventsize),
         cmocka_unit_test(test_parse_event2_badeventbuf),
-        cmocka_unit_test(test_foreach_event2_null),
         cmocka_unit_test(test_foreach_event2),
         cmocka_unit_test(test_foreach_event2_event2hdr_fail),
         cmocka_unit_test(test_foreach_event2_event2body_fail),
