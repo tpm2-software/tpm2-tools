079e7f8f098dae1161f3ab4a118e1ca9f991aded
Imran Desai <imran.desai@intel.com>
2020-07-24 11:41:55 -0700

    tpm2_getekcertificate: Add capability to output INTC EK cert from NV in PEM
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- test/integration/tests/getekcertificate.sh
+++ test/integration/tests/getekcertificate.sh
@@ -92,30 +92,48 @@ define_ek_cert_nv_index() {
     tpm2 nvwrite -C p -i $1 $2
 }
 
-## ECC only
+## ECC only INTC certificate from NV index
 define_ek_cert_nv_index ecc_ek_cert.bin $ECC_EK_CERT_NV_INDEX
 
-tpm2 getekcertificate -o nv_ecc_ek_cert.bin
+tpm2 getekcertificate -o nv_ecc_ek_cert.pem
 
-diff nv_ecc_ek_cert.bin ecc_ek_cert.bin
+sed 's/-/+/g;s/_/\//g;s/%3D/=/g;s/^{.*certificate":"//g;s/"}$//g;' \
+ecc_ek_cert.bin | base64 --decode > ecc_test.der
 
-## RSA only
+openssl x509 -inform PEM -outform DER  -in nv_ecc_ek_cert.pem \
+-out nv_ecc_ek_cert.der
+
+diff nv_ecc_ek_cert.der ecc_test.der
+
+## RSA only INTC certificate from NV index
 tpm2 nvundefine -C p $ECC_EK_CERT_NV_INDEX
 
 define_ek_cert_nv_index rsa_ek_cert.bin $RSA_EK_CERT_NV_INDEX
 
-tpm2 getekcertificate -o nv_rsa_ek_cert.bin
+tpm2 getekcertificate -o nv_rsa_ek_cert.pem
 
-diff nv_rsa_ek_cert.bin rsa_ek_cert.bin
+sed 's/-/+/g;s/_/\//g;s/%3D/=/g;s/^{.*certificate":"//g;s/"}$//g;' \
+rsa_ek_cert.bin | base64 --decode > rsa_test.der
 
-## RSA & ECC
+openssl x509 -inform PEM -outform DER  -in nv_rsa_ek_cert.pem \
+-out nv_rsa_ek_cert.der
+
+diff nv_rsa_ek_cert.der rsa_test.der
+
+## RSA & ECC INTC certificates from NV index
 
 define_ek_cert_nv_index ecc_ek_cert.bin $ECC_EK_CERT_NV_INDEX
 
-tpm2 getekcertificate -o nv_rsa_ek_cert.bin -o nv_ecc_ek_cert.bin
+tpm2 getekcertificate -o nv_rsa_ek_cert.pem -o nv_ecc_ek_cert.pem
 
-diff nv_ecc_ek_cert.bin ecc_ek_cert.bin
+openssl x509 -inform PEM -outform DER  -in nv_ecc_ek_cert.pem \
+-out nv_ecc_ek_cert.der
 
-diff nv_rsa_ek_cert.bin rsa_ek_cert.bin
+openssl x509 -inform PEM -outform DER  -in nv_rsa_ek_cert.pem \
+-out nv_rsa_ek_cert.der
+
+diff nv_ecc_ek_cert.der ecc_test.der
+
+diff nv_rsa_ek_cert.der rsa_test.der
 
 exit 0
