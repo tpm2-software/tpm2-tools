912da05b842b1f9d63d29040a3e6486c5e7d1d4d
Imran Desai <imran.desai@intel.com>
2020-07-30 11:43:00 -0700

    activatecredential.sh: Rearrange to clarify separate tests
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- test/integration/tests/activecredential.sh
+++ test/integration/tests/activecredential.sh
@@ -27,6 +27,23 @@ tpm2 createek -Q -c 0x81010009 -G rsa -u ek.pub
 tpm2 createak -C 0x81010009 -c ak.ctx -G rsa -g sha256 -s rsassa -u ak.pub \
 -n ak.name -p akpass> ak.out
 
+file_size=`ls -l ak.name | awk {'print $5'}`
+loaded_key_name=`cat ak.name | xxd -p -c $file_size` # Use -c in xxd so there is no line wrapping
+
+tpm2 readpublic -c 0x81010009 -o ek.pem -f pem -Q
+
+echo "12345678" | tpm2 makecredential -Q -u ek.pem -s - -n $loaded_key_name \
+-o mkcred.out -G rsa
+
+# Test the secret data matches after credential activation process
+tpm2 startauthsession --policy-session -S session.ctx
+tpm2 policysecret -S session.ctx -c e
+tpm2 activatecredential -Q -c ak.ctx -C 0x81010009 -i mkcred.out \
+-o actcred.out -p akpass -P"session:session.ctx"
+tpm2 flushcontext session.ctx
+
+diff actcred.out secret.data
+
 # Capture the yaml output and verify that its the same as the name output
 loaded_key_name_yaml=`python << pyscript
 from __future__ import print_function
@@ -38,23 +55,6 @@ with open('ak.out', 'r') as f:
     print(doc['loaded-key']['name'])
 pyscript`
 
-# Use -c in xxd so there is no line wrapping
-file_size=`ls -l ak.name | awk {'print $5'}`
-loaded_key_name=`cat ak.name | xxd -p -c $file_size`
-
 test "$loaded_key_name_yaml" == "$loaded_key_name"
-
-tpm2 readpublic -c 0x81010009 -o ek.pem -f pem -Q
-
-echo "12345678" | tpm2 makecredential -Q -u ek.pem -s - -n $loaded_key_name \
--o mkcred.out -G rsa
-
-tpm2 startauthsession --policy-session -S session.ctx
-tpm2 policysecret -S session.ctx -c e
-tpm2 activatecredential -Q -c ak.ctx -C 0x81010009 -i mkcred.out \
--o actcred.out -p akpass -P"session:session.ctx"
-tpm2 flushcontext session.ctx
-
-diff actcred.out secret.data
 
 exit 0
