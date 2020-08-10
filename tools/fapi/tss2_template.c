tpm2-software/tpm2-tools

commit 98176e9f1e8c4f866a071cb650ee4d873d72cb23

tss2-tools: set umask to 0117
get rid of:
 * owner execute (1)
 * group execute (1)
 * other write + read + execute (7)

actual mode bits are determined by requested mask & ~umask. All files
created should be 660 aka: rw- rw- ---

0777 & ~117=660

Signed-off-by: William Roberts <william.c.roberts@intel.com>

tools/fapi/tss2_template.c
@@ -16,6 +16,9 @@
 #include <stdarg.h>
 #include <errno.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+
 #include "tools/fapi/tss2_template.h"
 #include "lib/config.h"
 
@@ -397,6 +400,14 @@ static const tss2_tool *tss2_tool_lookup(int *argc, char ***argv)
  * specify which FAPI function to call.
  */
 int main(int argc, char *argv[]) {
+
+    /* get rid of:
+     *   owner execute (1)
+     *   group execute (1)
+     *   other write + read + execute (7)
+     */
+    umask(0117);
+
     const tss2_tool * const tool = tss2_tool_lookup(&argc, &argv);
     if (!tool) {
         LOG_ERR("%s: unknown tool. Available tss2 commands:\n", argv[0]);

commit 7063e23199810ed5b6da1a10b2a73e9695c65da8

tss2-tools: retain executable permission for user and group in umask
The umask affects files and directories alike, see man umask(2). Creating FAPI
objects entails creating new directories as well, which will not be searchable,
leading to failures like

ERROR:fapi:src/tss2-fapi/ifapi_helpers.c:986:create_dirs() mkdir not possible: -1 /tmp/tpm2_test_umRcf1/tss2_fapi.6tZ4O8/keystore_system/P_RSA/HE/
ERROR:fapi:src/tss2-fapi/ifapi_helpers.c:1013:ifapi_create_dirs() ErrorCode (0x0006000b) Create directories for /P_RSA/HE/EK
ERROR:fapi:src/tss2-fapi/ifapi_keystore.c:710:ifapi_keystore_store_async() ErrorCode (0x0006000b) Directory /P_RSA/HE/EK could not be created.
ERROR:fapi:src/tss2-fapi/api/Fapi_Provision.c:835:Fapi_Provision_Finish() ErrorCode (0x0006000b) Could not open: HE/EK
ERROR:fapi:src/tss2-fapi/api/Fapi_Provision.c:168:Fapi_Provision() ErrorCode (0x0006000b) Provision
Fapi_Provision(0x6000B) - fapi:A parameter has a bad value

To avoid this issue, the umask needs to retain the executable permission for
user and group. This has no implications for files created by the FAPI since
these are created using fopen(), which uses mode 0666 (so no executable
permission), see man fopen(3).

Fixes: 98176e9f1e8c4f866a071cb650ee4d873d72cb23
Signed-off-by: Jonas Witschel <diabonas@gmx.de>

tools/fapi/tss2_template.c
@@ -402,11 +402,9 @@ static const tss2_tool *tss2_tool_lookup(int *argc, char ***argv)
 int main(int argc, char *argv[]) {
 
     /* get rid of:
-     *   owner execute (1)
-     *   group execute (1)
      *   other write + read + execute (7)
      */
-    umask(0117);
+    umask(0007);
 
     const tss2_tool * const tool = tss2_tool_lookup(&argc, &argv);
     if (!tool) {