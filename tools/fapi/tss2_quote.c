afbb5225a02ae858df13ad714de6507be237e93f
William Roberts <william.c.roberts@intel.com>
2020-08-21 12:01:24 -0500

    tools/fapi/tss2_quote: fix unused value in printf
    Switch from LOG_ERR to LOG_PERR so the TSS2_RC value is used.
    Fixes:
    *** CID 1496688:  API usage errors  (PRINTF_ARGS)
    /tools/fapi/tss2_quote.c: 150 in tss2_tool_onrun()
    144         uint8_t *qualifyingData = NULL;
    145         size_t qualifyingDataSize = 0;
    146         if (ctx.qualifyingData) {
    147             r = open_read_and_close (ctx.qualifyingData,
    148                 (void*)&qualifyingData, &qualifyingDataSize);
    149             if (r) {
    >>>     CID 1496688:  API usage errors  (PRINTF_ARGS)
    >>>     This argument was not used by the format string: "r".
    150               LOG_ERR ("open_read_and_close qualifyingData", r);
    151               free (ctx.pcrList);
    152               return 1;
    153             }
    154         }
    155
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- tools/fapi/tss2_quote.c
+++ tools/fapi/tss2_quote.c
@@ -147,7 +147,7 @@ int tss2_tool_onrun (FAPI_CONTEXT *fctx) {
         r = open_read_and_close (ctx.qualifyingData,
             (void*)&qualifyingData, &qualifyingDataSize);
         if (r) {
-          LOG_ERR ("open_read_and_close qualifyingData", r);
+          LOG_PERR ("open_read_and_close qualifyingData", r);
           free (ctx.pcrList);
           return 1;
         }
