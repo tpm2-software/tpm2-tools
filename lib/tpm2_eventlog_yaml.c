a406d294207fef0868b6683c3ab4dc69025a742b
Hai Huang <huang195@gmail.com>
2020-12-09 09:29:35 -0500

    keep backward compatibility
    Signed-off-by: Hai Huang <huang195@gmail.com>

--- lib/tpm2_eventlog_yaml.c
+++ lib/tpm2_eventlog_yaml.c
@@ -210,9 +210,8 @@ static bool yaml_uefi_post_code(const TCG_EVENT2* const event) {
                          blob->BlobLength);
     } else { // otherwise, we treat it as an ASCII string
         const char* const data = (const char *) event->Event;
-        tpm2_tool_output("    Event:\n"
-                         "      Action: |-\n"
-                         "        %.*s\n", (int) len, data);
+        tpm2_tool_output("    Event: |-\n"
+                         "      %.*s\n", (int) len, data);
 
     }
     return true;
@@ -256,9 +255,8 @@ bool yaml_uefi_platfwblob(UEFI_PLATFORM_FIRMWARE_BLOB *data) {
 /* TCG PC Client PFP section 9.4.4 */
 bool yaml_uefi_action(UINT8 const *action, size_t size) {
 
-    tpm2_tool_output("    Event:\n"
-                     "      Action: |-\n"
-                     "        %.*s\n", (int) size, action);
+    tpm2_tool_output("    Event: |-\n"
+                     "      %.*s\n", (int) size, action);
 
     return true;
 }
