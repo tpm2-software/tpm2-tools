2c56eba816c8c4f5a461f67e2821baaabb979a51
Imran Desai <imran.desai@intel.com>
2021-04-16 11:46:06 -0700

    lib/tpm2_util.c: Fix potential buffer over-run
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- lib/tpm2_util.c
+++ lib/tpm2_util.c
@@ -1034,6 +1034,11 @@ bool tpm2_pem_encoded_key_to_fingerprint(const char *pem_encoded_key,
     char *token = strtok(str, "\n");
     while ( token != NULL ) {
         if (!strstr(token, "-----")) {
+            bool is_base64_overrun = (strlen(base64) + strlen(token)) > 1024 ?
+                true : false;
+            if (is_base64_overrun) {
+                return false;
+            }
             strcat(base64, token);
         }
         token = strtok(NULL, "\n");
