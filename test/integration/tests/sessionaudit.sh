5565a82a186771f64c80348990fa9883f8668244
Imran Desai <imran.desai@intel.com>
2021-05-26 14:30:10 -0700

    tools/tpm2_nvsetbits: specify non-auth sessions for audit/enc
    Fixes #2518
    
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- test/integration/tests/sessionaudit.sh
+++ test/integration/tests/sessionaudit.sh
@@ -356,6 +356,36 @@ diff \
 <( tail -c 32 att.data )
 
 #
+# Get audit digest: TPM command TPM2_CC_nvsetbits in an audit session
+#
+tpm2 clear -Q
+
+tpm2 createprimary -Q -C e -g sha256 -G rsa -c prim.ctx
+
+tpm2 create -Q -C prim.ctx -c signing_key.ctx -u signing_key.pub \
+-r signing_key.priv
+
+echo "plaintext" | \
+tpm2 create -C prim.ctx -c key.ctx -u key.pub -r key.priv -i-
+
+tpm2 nvdefine 1 -a "authwrite|ownerread|nt=bits"
+
+tpm2 startauthsession -S session.ctx --audit-session
+
+tpm2 nvsetbits 1 -i 1 --cphash cp.hash --rphash rp.hash -S session.ctx
+
+tpm2 getsessionauditdigest -c signing_key.ctx -m att.data -s att.sig \
+-S session.ctx
+
+dd if=/dev/zero bs=1 count=32 status=none of=zero.bin
+dd if=cp.hash skip=2 bs=1 count=32 status=none of=cphash.bin
+dd if=rp.hash skip=2 bs=1 count=32 status=none of=rphash.bin
+
+diff \
+<( cat zero.bin cphash.bin rphash.bin | openssl dgst -sha256 -binary ) \
+<( tail -c 32 att.data )
+
+#
 # End
 #
 exit 0
