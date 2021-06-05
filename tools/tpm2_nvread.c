caa315e6c3c440d7c035baa4ffce4459db93a253
Imran Desai <imran.desai@intel.com>
2021-06-04 18:01:44 -0700

    tools/tpm2_nvread: Redo tool layout to standard format
    Fixes #2419
    
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- tools/tpm2_nvread.c
+++ tools/tpm2_nvread.c
@@ -9,6 +9,9 @@
 
 typedef struct tpm_nvread_ctx tpm_nvread_ctx;
 struct tpm_nvread_ctx {
+    /*
+     * Inputs
+     */
     struct {
         const char *ctx_path;
         const char *auth_str;
@@ -16,54 +19,138 @@ struct tpm_nvread_ctx {
     } auth_hierarchy;
 
     TPM2_HANDLE nv_index;
-
     UINT32 size_to_read;
     UINT32 offset;
-    char *output_file;
 
+    /*
+     * Outputs
+     */
+    char *output_file;
+    UINT8* data_buffer;
+    UINT16 bytes_written;
+
+    /*
+     * Parameter hashes
+     */
     char *cp_hash_path;
+    TPM2B_DIGEST *cphash;
+    TPM2B_DIGEST cp_hash;
+    bool is_command_dispatch;
 };
 
 static tpm_nvread_ctx ctx;
 
-static tool_rc nv_read(ESYS_CONTEXT *ectx, tpm2_option_flags flags,
-    TPM2B_DIGEST *cp_hash) {
+static tool_rc nv_read(ESYS_CONTEXT *ectx) {
 
-    UINT8* data_buffer = NULL;
-    UINT16 bytes_written = 0;
-    tool_rc rc = tpm2_util_nv_read(ectx, ctx.nv_index, ctx.size_to_read,
-            ctx.offset, &ctx.auth_hierarchy.object, &data_buffer, &bytes_written,
-            cp_hash);
-    if (rc != tool_rc_success || cp_hash != NULL) {
-        goto out;
+    return tpm2_util_nv_read(ectx, ctx.nv_index, ctx.size_to_read,
+        ctx.offset, &ctx.auth_hierarchy.object, &ctx.data_buffer,
+        &ctx.bytes_written, ctx.cphash);
+}
+
+static tool_rc process_output(ESYS_CONTEXT *ectx, tpm2_option_flags flags) {
+
+    UNUSED(ectx);
+    /*
+     * 1. Outputs that do not require TPM2_CC_<command> dispatch
+     */
+    bool is_file_op_success = true;
+    if (ctx.cp_hash_path) {
+        is_file_op_success = files_save_digest(&ctx.cp_hash, ctx.cp_hash_path);
+
+        if (!is_file_op_success) {
+            return tool_rc_general_error;
+        }
     }
 
-    /* dump data_buffer to output file, if specified */
+    if (!ctx.is_command_dispatch) {
+        return tool_rc_success;
+    }
+
+    /*
+     * 2. Outputs generated after TPM2_CC_<command> dispatch
+     */
+
+    /* dump ctx.data_buffer to output file, if specified */
+    tool_rc rc = tool_rc_success;
     if (ctx.output_file) {
-        if (!files_save_bytes_to_file(ctx.output_file, data_buffer,
-                bytes_written)) {
+        if (!files_save_bytes_to_file(ctx.output_file, ctx.data_buffer,
+                ctx.bytes_written)) {
             rc = tool_rc_general_error;
             goto out;
         }
         /* else use stdout if quiet is not specified */
     } else if (!flags.quiet) {
-        if (!files_write_bytes(stdout, data_buffer, bytes_written)) {
+        if (!files_write_bytes(stdout, ctx.data_buffer, ctx.bytes_written)) {
             rc = tool_rc_general_error;
             goto out;
         }
     }
 
 out:
-    if (data_buffer) {
-        free(data_buffer);
+    return rc;
+}
+
+static tool_rc process_inputs(ESYS_CONTEXT *ectx) {
+
+    /*
+     * 1. Object and auth initializations
+     */
+
+    /*
+     * 1.a Add the new-auth values to be set for the object.
+     */
+
+    /*
+     * 1.b Add object names and their auth sessions
+     */
+
+    /* Object #1 */
+    tool_rc rc = tpm2_util_object_load_auth(ectx, ctx.auth_hierarchy.ctx_path,
+            ctx.auth_hierarchy.auth_str, &ctx.auth_hierarchy.object, false,
+            TPM2_HANDLE_FLAGS_NV | TPM2_HANDLE_FLAGS_O | TPM2_HANDLE_FLAGS_P);
+    if (rc != tool_rc_success) {
+        LOG_ERR("Invalid handle authorization");
+        return rc;
     }
+
+    /*
+     * 2. Restore auxiliary sessions
+     */
+
+    /*
+     * 3. Command specific initializations
+     */
+
+    /*
+     * 4. Configuration for calculating the pHash
+     */
+
+    /*
+     * 4.a Determine pHash length and alg
+     */
+    ctx.cphash = ctx.cp_hash_path ? &ctx.cp_hash : 0;
+
+    /*
+     * 4.b Determine if TPM2_CC_<command> is to be dispatched
+     */
+    ctx.is_command_dispatch = ctx.cp_hash_path ? false : true;
 
     return rc;
 }
 
+static tool_rc check_options(void) {
+
+    if (!ctx.size_to_read) {
+        LOG_WARN("Reading full size of the NV index");
+    }
+
+    return tool_rc_success;
+}
+
 static bool on_arg(int argc, char **argv) {
-    /* If the user doesn't specify an authorization hierarchy use the index
-     * passed to -x/--index for the authorization index.
+    /*
+     * If the user doesn't specify an authorization hierarchy use the index
+     * for the authorization index.
      */
     if (!ctx.auth_hierarchy.ctx_path) {
         ctx.auth_hierarchy.ctx_path = argv[0];
@@ -127,39 +214,62 @@ static bool tpm2_tool_onstart(tpm2_options **opts) {
 
 static tool_rc tpm2_tool_onrun(ESYS_CONTEXT *ectx, tpm2_option_flags flags) {
 
-    UNUSED(flags);
-
-    tool_rc rc = tpm2_util_object_load_auth(ectx, ctx.auth_hierarchy.ctx_path,
-            ctx.auth_hierarchy.auth_str, &ctx.auth_hierarchy.object, false,
-            TPM2_HANDLE_FLAGS_NV | TPM2_HANDLE_FLAGS_O | TPM2_HANDLE_FLAGS_P);
+    /*
+     * 1. Process options
+     */
+    tool_rc rc = check_options();
     if (rc != tool_rc_success) {
-        LOG_ERR("Invalid handle authorization");
         return rc;
     }
 
-    if (!ctx.cp_hash_path) {
-        return nv_read(ectx, flags, NULL);
-    }
-
-    TPM2B_DIGEST cp_hash = { .size = 0 };
-    rc = nv_read(ectx, flags, &cp_hash);
+    /*
+     * 2. Process inputs
+     */
+    rc = process_inputs(ectx);
     if (rc != tool_rc_success) {
-        LOG_ERR("CpHash calculation failed!");
         return rc;
     }
 
-    bool result = files_save_digest(&cp_hash, ctx.cp_hash_path);
-    if (!result) {
-        rc = tool_rc_general_error;
+    /*
+     * 3. TPM2_CC_<command> call
+     */
+    rc = nv_read(ectx);
+    if (rc != tool_rc_success) {
+        return rc;
     }
+
+    /*
+     * 4. Process outputs
+     */
+    return process_output(ectx, flags);
+}
+
+static tool_rc tpm2_tool_onstop(ESYS_CONTEXT *ectx) {
+
+    UNUSED(ectx);
+    /*
+     * 1. Free objects
+     */
+    if (ctx.data_buffer) {
+        free(ctx.data_buffer);
+    }
+
+    /*
+     * 2. Close authorization sessions
+     */
+    tool_rc rc = tool_rc_success;
+    tool_rc tmp_rc = tpm2_session_close(&ctx.auth_hierarchy.object.session);
+    if (tmp_rc != tool_rc_success) {
+        rc = tmp_rc;
+    }
+
+    /*
+     * 3. Close auxiliary sessions
+     */
 
     return rc;
 }
 
-static tool_rc tpm2_tool_onstop(ESYS_CONTEXT *ectx) {
-    UNUSED(ectx);
-    return tpm2_session_close(&ctx.auth_hierarchy.object.session);
-}
-
 // Register this tool with tpm2_tool.c
-TPM2_TOOL_REGISTER("nvread", tpm2_tool_onstart, tpm2_tool_onrun, tpm2_tool_onstop, NULL)
+TPM2_TOOL_REGISTER("nvread", tpm2_tool_onstart, tpm2_tool_onrun,
+    tpm2_tool_onstop, NULL)
