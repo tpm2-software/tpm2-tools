7de60c942373c5b5b617daf84c8f202e7ce5e01c
William Roberts <william.c.roberts@intel.com>
2021-09-01 17:26:15 -0500

    [RFC] tools: add tpm2_policy tool for invoking libpolicy
    UPDATE:
    Working:
    Initial test working:
    If you get the tpm2_policy tool from the tss pr, this should work:
    tpm2 policy /path/to/tpm2-tss/test/data/fapi/policy/pol_pcr16_0.json
    
    Their are still many todo's to go through, but let's figure out the
    interface into the library and go from there.
    
    Create a tpm2_policy tool that can read the FAPI JSON style policies
    and:
    
    1. Instantiate them -> This process fills in anything missing in the
    template. TODO: How does this get handled, do we need to tweak any
    of the callbacks?
    
    2. Calculate them -> This process produces a list of hashes... TODO:
       Why? Is this a list of all the subordinate policies or can the json file
       have N policies where N > 1?
    
    3. Execute them -> Execute the policy on a session. TODO: Who is
    supposed to start the policy session as it seems to be 0?
    
    Their are a lot of TODO items in this code. I'm looking for how we want
    to use this, different tools, like tpm2_policyinit tpm2_policycalc and
    tpm2_policyexec? Or an all in-one tool. Currently note that ONLY the
    Execute needs an ESYS context, but instantiate should be filling stuff
    in so it likely needs a context or the callbacks handled?
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

---
+++ tools/misc/tpm2_policy.c
@@ -0,0 +1,165 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <tss2/tss2_policy.h>
+
+#include "log.h"
+#include "tpm2_tool.h"
+#include "tpm2_util.h"
+
+typedef struct tpm2_policy_ctx tpm2_policy_ctx;
+struct tpm2_policy_ctx {
+    ifapi_policyeval_INST_CB cb;
+    const char *policy_file;
+};
+
+static tpm2_policy_ctx ctx;
+
+static bool on_arg(int argc, char *argv[]) {
+
+    if (argc != 1) {
+        LOG_ERR("Expected single file path argument");
+        return false;
+    }
+
+    ctx.policy_file = argv[0];
+
+    return true;
+}
+
+static bool tpm2_tool_onstart(tpm2_options **opts) {
+//    static const struct option topts[] = {
+//        { "type",   required_argument, NULL, 't' },
+//        { "format", required_argument, NULL, 'f' },
+//    };
+
+    *opts = tpm2_options_new(NULL, 0, NULL, NULL, on_arg,
+            0);
+
+    return *opts != NULL;
+}
+
+static tool_rc tpm2_tool_onrun(ESYS_CONTEXT *ectx, tpm2_option_flags flags) {
+    UNUSED(flags);
+
+    TPMS_POLICY *policy_ctx;
+
+    TSS2_RC rc = Tss2_PolicyInstantiate(
+        ctx.policy_file,
+        &ctx.cb,
+        &policy_ctx);
+    if (rc) {
+        LOG_ERR("Instantiate failed");
+        return tool_rc_general_error;
+    }
+
+    rc = Tss2_PolicyCalculate(
+            policy_ctx->policy, /* could we just pass context and drop these? */
+            &policy_ctx->policyDigests, /* same as above */
+            TPM2_ALG_SHA256,
+            32, /* this could be computed from alg... */
+            0); /* I can't figure out what this is for, looks like some kind of recursion in the tss2 lib */
+    if (rc) {
+        LOG_ERR("Calculate failed");
+        return tool_rc_general_error;
+    }
+
+    /* Why doesn't calculate give us the aggregate hash? */
+    printf("hash: ");
+    tpm2_util_hexdump2(stdout, policy_ctx->policyDigests.digests[0].digest.sha256, 32);
+    printf("\n");
+
+    /* TODO TAKE USER INPUTS */
+    TPM2B_SENSITIVE_CREATE inSensitivePrimary = { 0 };
+
+    TPM2B_PUBLIC inPublic = {
+        .size = 0,
+        .publicArea = {
+            .type = TPM2_ALG_RSA,
+            .nameAlg = TPM2_ALG_SHA256,
+            .objectAttributes = (TPMA_OBJECT_USERWITHAUTH |
+                                 TPMA_OBJECT_RESTRICTED |
+                                 TPMA_OBJECT_DECRYPT |
+                                 TPMA_OBJECT_FIXEDTPM |
+                                 TPMA_OBJECT_FIXEDPARENT |
+                                 TPMA_OBJECT_SENSITIVEDATAORIGIN),
+            .authPolicy = {
+                 .size = 0,
+             },
+            .parameters.rsaDetail = {
+                 .symmetric = {
+                     .algorithm = TPM2_ALG_AES,
+                     .keyBits.aes = 128,
+                     .mode.aes = TPM2_ALG_CFB},
+                 .scheme = {
+                      .scheme = TPM2_ALG_NULL
+                  },
+                 .keyBits = 2048,
+                 .exponent = 0,
+             },
+            .unique.rsa = {
+                 .size = 0,
+                 .buffer = {},
+             },
+        },
+    };
+
+    TPMT_SYM_DEF symmetric = {.algorithm = TPM2_ALG_AES,
+                              .keyBits = {.aes = 128},
+                              .mode = {.aes = TPM2_ALG_CFB}
+    };
+
+    TPM2B_NONCE nonceCaller = {
+        .size = 32,
+        .buffer = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11, 12, 13, 14, 15, 16, 17,
+                    18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32}
+    };
+
+    TPM2B_DATA outsideInfo = { 0 };
+    TPML_PCR_SELECTION creationPCR = { 0 };
+    ESYS_TR primaryHandle = ESYS_TR_NONE;
+    TPM2B_PUBLIC *outPublic = NULL;
+    TPM2B_CREATION_DATA *creationData = NULL;
+    TPM2B_DIGEST *creationHash = NULL;
+    TPMT_TK_CREATION *creationTicket = NULL;
+    ESYS_TR session = ESYS_TR_NONE;
+
+    rc = Esys_CreatePrimary(ectx, ESYS_TR_RH_OWNER, ESYS_TR_PASSWORD,
+                           ESYS_TR_NONE, ESYS_TR_NONE, &inSensitivePrimary, &inPublic,
+                           &outsideInfo, &creationPCR, &primaryHandle,
+                           &outPublic, &creationData, &creationHash,
+                           &creationTicket);
+    if (rc) {
+        LOG_ERR("CreatePrimary failed");
+        return tool_rc_general_error;
+    }
+
+    rc = Esys_StartAuthSession(ectx,
+            primaryHandle, // tpm key
+            ESYS_TR_NONE,  // bind key
+            ESYS_TR_NONE, ESYS_TR_NONE, ESYS_TR_NONE, // sessions
+            &nonceCaller, TPM2_SE_POLICY, &symmetric, TPM2_ALG_SHA256, &session);
+    if (rc) {
+        LOG_ERR("StartAuthSession failed");
+        return tool_rc_general_error;
+    }
+
+    rc = Tss2_PolicyExecute(
+        TPM2_ALG_SHA256,
+        policy_ctx,
+        ectx,
+        session);
+    if (rc) {
+        LOG_ERR("Execute failed");
+        return tool_rc_general_error;
+    }
+
+    return tool_rc_success;
+}
+
+// Register this tool with tpm2_tool.c
+TPM2_TOOL_REGISTER("policy", tpm2_tool_onstart, tpm2_tool_onrun, NULL, NULL)
