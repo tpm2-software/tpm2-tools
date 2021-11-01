0c3eb6537b11b9f7ff76c4b57e38e18f3d804b23
Imran Desai <imran.desai@intel.com>
2021-11-01 15:41:30 -0700

    tools/tpm2_nvwritelock: enable support to specify aux/non-auth sessions
    Fixes #2521
    
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- test/integration/tests/sessionaudit.sh
+++ test/integration/tests/sessionaudit.sh
@@ -509,6 +509,66 @@ diff \
 <( tail -c 32 att.data )
 
 #
+# Get audit digest: TPM command TPM2_CC_NV_WriteLock in an audit session
+#
+tpm2 clear -Q
+
+tpm2 createprimary -Q -C e -g sha256 -G rsa -c prim.ctx
+
+tpm2 create -Q -C prim.ctx -c signing_key.ctx -u signing_key.pub \
+-r signing_key.priv
+
+tpm2 nvdefine 1 -C o -s 32 -a "authread|authwrite|writedefine"
+
+tpm2 startauthsession -S session.ctx --audit-session
+
+tpm2 nvwritelock 1 -C 0x01000001 --cphash cp.hash --rphash rp.hash \
+-S session.ctx
+
+tpm2 getsessionauditdigest -c signing_key.ctx -m att.data -s att.sig \
+-S session.ctx
+
+tpm2 flushcontext session.ctx
+
+dd if=/dev/zero bs=1 count=32 status=none of=zero.bin
+dd if=cp.hash skip=2 bs=1 count=32 status=none of=cphash.bin
+dd if=rp.hash skip=2 bs=1 count=32 status=none of=rphash.bin
+
+diff \
+<( cat zero.bin cphash.bin rphash.bin | openssl dgst -sha256 -binary ) \
+<( tail -c 32 att.data )
+
+#
+# Get audit digest: TPM command TPM2_CC_NV_GlobalWriteLock in an audit session
+#
+tpm2 clear -Q
+
+tpm2 createprimary -Q -C e -g sha256 -G rsa -c prim.ctx
+
+tpm2 create -Q -C prim.ctx -c signing_key.ctx -u signing_key.pub \
+-r signing_key.priv
+
+tpm2 nvdefine 1 -C o -s 32 -a "authread|authwrite|globallock"
+
+tpm2 startauthsession -S session.ctx --audit-session
+
+tpm2 nvwritelock --global -C o --cphash cp.hash --rphash rp.hash \
+-S session.ctx
+
+tpm2 getsessionauditdigest -c signing_key.ctx -m att.data -s att.sig \
+-S session.ctx
+
+tpm2 flushcontext session.ctx
+
+dd if=/dev/zero bs=1 count=32 status=none of=zero.bin
+dd if=cp.hash skip=2 bs=1 count=32 status=none of=cphash.bin
+dd if=rp.hash skip=2 bs=1 count=32 status=none of=rphash.bin
+
+diff \
+<( cat zero.bin cphash.bin rphash.bin | openssl dgst -sha256 -binary ) \
+<( tail -c 32 att.data )
+
+#
 # End
 #
 exit 0
