tpm2-software/tpm2-tools

commit 6468c73d7e8fe5d138f54a6d49ef9e0c28deabe7

tools/tpm2_nvread: fix reads > TPM2_PT_NV_BUFFER_MAX with policy session
Fixes #2875

Policy authorization session must be re-instantiated on multiple
iterations of nv read if the read size exceeds the allowed operating
size. To avoid this, only a single iteration is allowed when using
policy authorization session.

Re-instantiation is avoided because compounded policies are not
possible to be replayed to arrive at the same policy digest.

Signed-off-by: Imran Desai <imran.desai@intel.com>

lib/tpm2_nv_util.h
@@ -228,7 +228,18 @@ static inline tool_rc tpm2_util_nv_read(ESYS_CONTEXT *ectx,
         "Make seperate tpm2_nvread calls with proper offsets specified to "
         "specify all NV index data to be read");
 
-        rc =-tool_rc_option_error;
+        rc = tool_rc_option_error;
+        goto out;
+    }
+
+    bool is_auth_a_policy_session = is_nvread_dispatched &&
+        tpm2_session_get_type(auth_hierarchy_obj->session) == TPM2_SE_POLICY;
+    if (is_auth_a_policy_session && is_nv_index_data_larger_than_max_read) {
+        LOG_ERR("Cannot continue as the policy auth session must be "
+                "reinstantiated for multiple iterations of NV read. "
+                "Specify a max read size of %lu or specify an offset and max "
+                "read size of %lu", max_data_size, max_data_size);
+        rc = tool_rc_option_error;
         goto out;
     }
 

commit d83f3cea5eb6d1d85fc06147a8ee69d0f7f88499

Use %u instead of %lu format specifier for uint16
max_data_size is uint16, therefore GCC 11.1.0 gives warnings/errors like

./lib/tpm2_nv_util.h: In function ‘tpm2_util_nv_read’:
./lib/tpm2_nv_util.h:238:17: error: format ‘%lu’ expects argument of type ‘long unsigned int’, but argument 5 has type ‘int’ [-Werror=format=]
  238 |         LOG_ERR("Cannot continue as the policy auth session must be "
      |                 ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
......
  241 |                 "read size of %lu", max_data_size, max_data_size);
      |                                     ~~~~~~~~~~~~~
      |                                     |
      |                                     int

Fixes: 6468c73d7e8fe5d138f54a6d49ef9e0c28deabe7 ("tools/tpm2_nvread: fix reads > TPM2_PT_NV_BUFFER_MAX with policy session")
Fixes: 4d7456dee8111190faf1d5c9d91b0602d653a342 ("tools/tpm2_nvwrite: fix writes > TPM2_PT_NV_BUFFER_MAX with policyauth")

Signed-off-by: Jonas Witschel <git@diabonas.de>

lib/tpm2_nv_util.h
@@ -237,8 +237,8 @@ static inline tool_rc tpm2_util_nv_read(ESYS_CONTEXT *ectx,
     if (is_auth_a_policy_session && is_nv_index_data_larger_than_max_read) {
         LOG_ERR("Cannot continue as the policy auth session must be "
                 "reinstantiated for multiple iterations of NV read. "
-                "Specify a max read size of %lu or specify an offset and max "
-                "read size of %lu", max_data_size, max_data_size);
+                "Specify a max read size of %u or specify an offset and max "
+                "read size of %u", max_data_size, max_data_size);
         rc = tool_rc_option_error;
         goto out;
     }

tools/tpm2_nvwrite.c
@@ -203,8 +203,8 @@ static tool_rc process_inputs(ESYS_CONTEXT *ectx) {
     if (is_auth_a_policy_session && ctx.data_size > ctx.max_data_size) {
          LOG_ERR("Cannot continue as the policy auth session must be "
                  "reinstantiated for multiple iterations of NV write. "
-                 "Specify a max write size of %lu or specify an offset and max "
-                 "write size of %lu", ctx.max_data_size, ctx.max_data_size);
+                 "Specify a max write size of %u or specify an offset and max "
+                 "write size of %u", ctx.max_data_size, ctx.max_data_size);
          return tool_rc_option_error;
     }
 