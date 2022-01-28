d74a1b97cebc96df174fac9baad942ceff7dc50c
Imran Desai <imran.desai@intel.com>
2022-01-28 11:42:10 -0700

    tools/tpm2_clearcontrol: calculate pHash alg from session & phash path
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- tools/tpm2_clearcontrol.c
+++ tools/tpm2_clearcontrol.c
@@ -7,6 +7,7 @@
 #include "tpm2.h"
 #include "tpm2_tool.h"
 
+#define MAX_SESSIONS 3
 typedef struct clearcontrol_ctx clearcontrol_ctx;
 struct clearcontrol_ctx {
     /*
@@ -27,15 +28,16 @@ struct clearcontrol_ctx {
     /*
      * Parameter hashes
      */
-    char *cp_hash_path;
-    TPM2B_DIGEST *cphash;
+    const char *cp_hash_path;
     TPM2B_DIGEST cp_hash;
     bool is_command_dispatch;
+    TPMI_ALG_HASH parameter_hash_algorithm;
 };
 
 static clearcontrol_ctx ctx = {
     .auth_hierarchy.ctx_path = "p",
     .disable_clear = 0,
+    .parameter_hash_algorithm = TPM2_ALG_ERROR,
 };
 
 static tool_rc clearcontrol(ESYS_CONTEXT *ectx) {
@@ -46,7 +48,7 @@ static tool_rc clearcontrol(ESYS_CONTEXT *ectx) {
                 "TPM2_RH_PLATFORM" : "TPM2_RH_LOCKOUT");
 
     return tpm2_clearcontrol(ectx, &ctx.auth_hierarchy.object,
-        ctx.disable_clear, ctx.cphash);
+        ctx.disable_clear, &ctx.cp_hash, ctx.parameter_hash_algorithm);
 }
 
 static tool_rc process_output(ESYS_CONTEXT *ectx) {
@@ -116,11 +118,20 @@ static tool_rc process_inputs(ESYS_CONTEXT *ectx) {
     /*
      * 4. Configuration for calculating the pHash
      */
-    ctx.cphash = ctx.cp_hash_path ? &ctx.cp_hash : 0;
 
     /*
      * 4.a Determine pHash length and alg
      */
+    tpm2_session *all_sessions[MAX_SESSIONS] = {
+        ctx.auth_hierarchy.object.session,
+        0,
+        0
+    };
+
+    const char **cphash_path = ctx.cp_hash_path ? &ctx.cp_hash_path : 0;
+
+    ctx.parameter_hash_algorithm = tpm2_util_calculate_phash_algorithm(ectx,
+        cphash_path, &ctx.cp_hash, 0, 0, all_sessions);
 
     /*
      * 4.b Determine if TPM2_CC_<command> is to be dispatched
