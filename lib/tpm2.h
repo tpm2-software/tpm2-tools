f5720002b04f6ab0aa2d11d3b318e294ca996ade
Imran Desai <imran.desai@intel.com>
2022-01-28 15:41:10 -0700

    tools/tpm2_clockrateadjust: calc pHash alg from session input
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- lib/tpm2.h
+++ lib/tpm2.h
@@ -255,7 +255,8 @@ tool_rc tpm2_clearcontrol(ESYS_CONTEXT *esys_context,
     TPM2B_DIGEST *cp_hash, TPMI_ALG_HASH parameter_hash_algorithm);
 
 tool_rc tpm2_clockrateadjust(ESYS_CONTEXT *ectx, tpm2_loaded_object *object,
-        TPM2_CLOCK_ADJUST rate_adjust, TPM2B_DIGEST *cp_hash);
+    TPM2_CLOCK_ADJUST rate_adjust, TPM2B_DIGEST *cp_hash,
+    TPMI_ALG_HASH parameter_hash_algorithm);
 
 tool_rc tpm2_dictionarylockout_reset(ESYS_CONTEXT *esys_context,
     tpm2_loaded_object *auth_hierarchy, TPM2B_DIGEST *cp_hash,
