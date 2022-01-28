3551c7e493a4c0f281b516230bd66d727615de45
Imran Desai <imran.desai@intel.com>
2022-01-28 13:52:31 -0700

    tools/tpm2_clockrateadjust: Redo tool layout to standard format
    Fixes #2389
    
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- tools/tpm2_clockrateadjust.c
+++ tools/tpm2_clockrateadjust.c
@@ -9,16 +9,155 @@
 
 typedef struct tpm2_setclock_ctx tpm2_setclock_ctx;
 struct tpm2_setclock_ctx {
+    /*
+     * Inputs
+     */
+    struct {
+        const char *ctx_path;
+        const char *auth_str;
+        tpm2_loaded_object object;
+    } auth_hierarchy;
+
+    const char *adjustment;
     TPM2_CLOCK_ADJUST adjust;
-    const char *auth_hierarchy;
-    tpm2_loaded_object object;
-    const char *auth_value;
+    bool is_slower;
+
+    /*
+     * Outputs
+     */
+
+    /*
+     * Parameter hashes
+     */
     char *cp_hash_path;
+    TPM2B_DIGEST *cphash;
+    TPM2B_DIGEST cp_hash;
+    bool is_command_dispatch;
 };
 
 static tpm2_setclock_ctx ctx = {
-        .auth_hierarchy = "o" /* default to owner hierarchy */
+        .auth_hierarchy.ctx_path = "o" /* default to owner hierarchy */
 };
+
+
+static tool_rc clockrateadjust(ESYS_CONTEXT *ectx) {
+
+    return tpm2_clockrateadjust(ectx, &ctx.auth_hierarchy.object, ctx.adjust,
+        ctx.cphash);
+}
+
+
+static tool_rc process_output(ESYS_CONTEXT *ectx) {
+
+    UNUSED(ectx);
+    /*
+     * 1. Outputs that do not require TPM2_CC_<command> dispatch
+     */
+    bool is_file_op_success = true;
+    if (ctx.cp_hash_path) {
+        is_file_op_success = files_save_digest(&ctx.cp_hash, ctx.cp_hash_path);
+
+        if (!is_file_op_success) {
+            return tool_rc_general_error;
+        }
+    }
+
+    tool_rc rc = tool_rc_success;
+    if (!ctx.is_command_dispatch) {
+        return rc;
+    }
+
+    /*
+     * 2. Outputs generated after TPM2_CC_<command> dispatch
+     */
+
+    return rc;
+}
+
+
+static tool_rc process_inputs(ESYS_CONTEXT *ectx) {
+
+    /*
+     * 1. Object and auth initializations
+     */
+
+    /*
+     * 1.a Add the new-auth values to be set for the object.
+     */
+
+    /*
+     * 1.b Add object names and their auth sessions
+     */
+
+    /* Object #1 */
+    tool_rc rc = tpm2_util_object_load_auth(ectx, ctx.auth_hierarchy.ctx_path,
+            ctx.auth_hierarchy.auth_str, &ctx.auth_hierarchy.object, false,
+            TPM2_HANDLE_FLAGS_O | TPM2_HANDLE_FLAGS_P);
+    if (rc != tool_rc_success) {
+        LOG_ERR("Invalid lockout authorization");
+        return rc;
+    }
+
+    /*
+     * 2. Restore auxiliary sessions
+     */
+
+    /*
+     * 3. Command specific initializations
+     */
+    if (ctx.is_slower) {
+        ctx.adjust -= strlen(ctx.adjustment);
+    } else {
+        ctx.adjust += strlen(ctx.adjustment);
+    }
+
+    /*
+     * 4. Configuration for calculating the pHash
+     */
+    ctx.cphash = ctx.cp_hash_path ? &ctx.cp_hash : 0;
+
+    /*
+     * 4.a Determine pHash length and alg
+     */
+
+    /*
+     * 4.b Determine if TPM2_CC_<command> is to be dispatched
+     */
+    ctx.is_command_dispatch = ctx.cp_hash_path ? false : true;
+
+    return rc;
+}
+
+static tool_rc check_options(ESYS_CONTEXT *ectx) {
+
+    UNUSED(ectx);
+
+    size_t len = strlen(ctx.adjustment);
+    if (len > 3) {
+        LOG_ERR("Expected at most 3 adjustment characters");
+        return tool_rc_option_error;
+    }
+
+    static const char slower[] = "sss";
+    static const char faster[] = "fff";
+    ctx.is_slower = ctx.adjustment[0] == 's';
+    const char *compare = ctx.is_slower ? slower : faster;
+
+    bool is_equal = !strncmp(ctx.adjustment, compare, len);
+    if (!is_equal) {
+        LOG_ERR("Adjustment specifier should be consistent, either all "
+                "'s' or all 'f' characters got: \"%s\"", ctx.adjustment);
+        return tool_rc_option_error;
+    }
+
+    if (!ctx.is_command_dispatch) {
+        LOG_WARN("Generating cpHash. Exiting without executing clear.");
+    }
+
+    LOG_INFO("adjust value: %d", ctx.adjust);
+
+    return tool_rc_success;
+}
 
 static bool on_arg(int argc, char **argv) {
 
@@ -27,32 +166,7 @@ static bool on_arg(int argc, char **argv) {
         return false;
     }
 
-    const char *adjustment = argv[0];
-
-    size_t len = strlen(adjustment);
-    if (len > 3) {
-        LOG_ERR("Expected at most 3 adjustment characters");
-        return false;
-    }
-
-    static const char slower[] = "sss";
-    static const char faster[] = "fff";
-
-    bool is_slower = adjustment[0] == 's';
-    const char *compare = is_slower ? slower : faster;
-
-    bool is_equal = !strncmp(adjustment, compare, len);
-    if (!is_equal) {
-        LOG_ERR("Adjustment specifier should be consistent, either all "
-                "'s' or all 'f' characters got: \"%s\"", adjustment);
-        return false;
-    }
-
-    if (is_slower) {
-        ctx.adjust -= len;
-    } else {
-        ctx.adjust += len;
-    }
+    ctx.adjustment = argv[0];
 
     return true;
 }
@@ -61,10 +175,10 @@ static bool on_option(char key, char *value) {
 
     switch (key) {
     case 'c':
-        ctx.auth_hierarchy = value;
+        ctx.auth_hierarchy.ctx_path = value;
         break;
     case 'p':
-        ctx.auth_value = value;
+        ctx.auth_hierarchy.auth_str = value;
         break;
     case 0:
         ctx.cp_hash_path = value;
@@ -78,52 +192,71 @@ static bool on_option(char key, char *value) {
 static bool tpm2_tool_onstart(tpm2_options **opts) {
 
     const struct option topts[] = {
-        { "hierarchy", required_argument, NULL, 'c' },
-        { "auth",      required_argument, NULL, 'p' },
-        { "cphash",    required_argument, NULL,  0  },
+        { "hierarchy", required_argument, 0, 'c' },
+        { "auth",      required_argument, 0, 'p' },
+        { "cphash",    required_argument, 0,  0  },
     };
 
     *opts = tpm2_options_new("c:p:", ARRAY_LEN(topts), topts, on_option,
     on_arg, 0);
 
-    return *opts != NULL;
+    return *opts != 0;
 }
 
 static tool_rc tpm2_tool_onrun(ESYS_CONTEXT *ectx, tpm2_option_flags flags) {
 
     UNUSED(flags);
 
-    tool_rc rc = tpm2_util_object_load_auth(ectx, ctx.auth_hierarchy,
-            ctx.auth_value, &ctx.object, false,
-            TPM2_HANDLE_FLAGS_P|TPM2_HANDLE_FLAGS_O);
+    /*
+     * 1. Process options
+     */
+    tool_rc rc = check_options(ectx);
     if (rc != tool_rc_success) {
         return rc;
     }
 
-    LOG_INFO("adjust value: %d", ctx.adjust);
-
-    if (ctx.cp_hash_path) {
-        TPM2B_DIGEST cp_hash = { .size = 0 };
-        LOG_WARN("Calculating cpHash. Exiting without setting clock.");
-        rc = tpm2_clockrateadjust(ectx, &ctx.object, ctx.adjust, &cp_hash);
-        if (rc != tool_rc_success) {
-            return rc;
-        }
-        bool result = files_save_digest(&cp_hash, ctx.cp_hash_path);
-        if (!result) {
-            rc = tool_rc_general_error;
-        }
+    /*
+     * 2. Process inputs
+     */
+    rc = process_inputs(ectx);
+    if (rc != tool_rc_success) {
         return rc;
     }
 
-    return tpm2_clockrateadjust(ectx, &ctx.object, ctx.adjust, NULL);
+    /*
+     * 3. TPM2_CC_<command> call
+     */
+    rc = clockrateadjust(ectx);
+    if (rc != tool_rc_success) {
+        return rc;
+    }
+
+    /*
+     * 4. Process outputs
+     */
+    return process_output(ectx);
 }
 
 static tool_rc tpm2_tool_onstop(ESYS_CONTEXT *ectx) {
+
     UNUSED(ectx);
 
-    return tpm2_session_close(&ctx.object.session);
+    /*
+     * 1. Free objects
+     */
+
+    /*
+     * 2. Close authorization sessions
+     */
+    tool_rc rc = tpm2_session_close(&ctx.auth_hierarchy.object.session);
+
+    /*
+     * 3. Close auxiliary sessions
+     */
+
+    return rc;
 }
 
 // Register this tool with tpm2_tool.c
-TPM2_TOOL_REGISTER("clockrateadjust", tpm2_tool_onstart, tpm2_tool_onrun, tpm2_tool_onstop, NULL)
+TPM2_TOOL_REGISTER("clockrateadjust", tpm2_tool_onstart, tpm2_tool_onrun,
+    tpm2_tool_onstop, 0)
