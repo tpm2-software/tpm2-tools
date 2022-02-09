15b79be44a7adc50d7c22c661532b15408218e24
Imran Desai <imran.desai@intel.com>
2022-02-09 15:28:43 -0700

    tools/tpm2_hmac: calc phash alg from session input & path
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- tools/tpm2_hmac.c
+++ tools/tpm2_hmac.c
@@ -13,6 +13,7 @@
 #include "tpm2_alg_util.h"
 #include "tpm2_tool.h"
 
+#define MAX_SESSIONS 3
 typedef struct tpm_hmac_ctx tpm_hmac_ctx;
 struct tpm_hmac_ctx {
     /*
@@ -42,13 +43,15 @@ struct tpm_hmac_ctx {
     /*
      * Parameter hashes
      */
-    char *cp_hash_path;
-    TPM2B_DIGEST *cphash;
+    const char *cp_hash_path;
     TPM2B_DIGEST cp_hash;
     bool is_command_dispatch;
+    TPMI_ALG_HASH parameter_hash_algorithm;
 };
 
-static tpm_hmac_ctx ctx;
+static tpm_hmac_ctx ctx = {
+    .parameter_hash_algorithm = TPM2_ALG_ERROR,
+};
 
 static tool_rc hmac(ESYS_CONTEXT *ectx) {
 
@@ -81,7 +84,7 @@ static tool_rc hmac(ESYS_CONTEXT *ectx) {
          * hash algorithm for the HMAC
          */
         return tpm2_hmac(ectx, &ctx.hmac_key.object, ctx.halg, &buffer,
-            &ctx.hmac_out, ctx.cphash);
+            &ctx.hmac_out, &ctx.cp_hash, ctx.parameter_hash_algorithm);
     }
 
     if (ctx.cp_hash_path) {
@@ -275,11 +278,20 @@ static tool_rc process_inputs(ESYS_CONTEXT *ectx) {
     /*
      * 4. Configuration for calculating the pHash
      */
-    ctx.cphash = ctx.cp_hash_path ? &ctx.cp_hash : 0;
 
     /*
      * 4.a Determine pHash length and alg
      */
+    tpm2_session *all_sessions[MAX_SESSIONS] = {
+        ctx.hmac_key.object.session,
+        0,
+        0
+    };
+
+    const char **cphash_path = ctx.cp_hash_path ? &ctx.cp_hash_path : 0;
+
+    ctx.parameter_hash_algorithm = tpm2_util_calculate_phash_algorithm(ectx,
+        cphash_path, &ctx.cp_hash, 0, 0, all_sessions);
 
     /*
      * 4.b Determine if TPM2_CC_<command> is to be dispatched
