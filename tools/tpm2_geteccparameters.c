0b6276b003135837e339ffc96137f2549faa79cb
Imran Desai <imran.desai@intel.com>
2022-06-03 06:26:29 -0700

    tools/tpm2_geteccparameters: redo tools layout
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- tools/tpm2_geteccparameters.c
+++ tools/tpm2_geteccparameters.c
@@ -9,13 +9,100 @@
 
 typedef struct tpm_geteccparameters_ctx tpm_geteccparameters_ctx;
 struct tpm_geteccparameters_ctx {
+    /*
+     * Inputs
+     */
     TPMI_ECC_CURVE curve_id;
+
+    /*
+     * Outputs
+     */
+    TPMS_ALGORITHM_DETAIL_ECC *parameters;
     const char *ecc_parameters_path;
 };
 
 static tpm_geteccparameters_ctx ctx = {
     .curve_id = TPM2_ECC_NONE,
 };
+
+static tool_rc geteccparameters(ESYS_CONTEXT *ectx) {
+
+    return tpm2_geteccparameters(ectx, ctx.curve_id, &ctx.parameters);
+}
+
+static tool_rc process_outputs(ESYS_CONTEXT *ectx) {
+
+    UNUSED(ectx);
+
+    /*
+     * 1. Outputs that do not require TPM2_CC_<command> dispatch
+     */
+
+    /*
+     * 2. Outputs generated after TPM2_CC_<command> dispatch
+     */
+    bool is_file_op_success = files_save_ecc_details(ctx.parameters,
+        ctx.ecc_parameters_path);
+    if (!is_file_op_success) {
+        LOG_ERR("Failed to write out the ECC pub key");
+        return tool_rc_general_error;
+    }
+
+    return tool_rc_success;
+}
+
+static tool_rc process_inputs(ESYS_CONTEXT *ectx) {
+
+    UNUSED(ectx);
+    /*
+     * 1. Object and auth initializations
+     */
+
+    /*
+     * 1.a Add the new-auth values to be set for the object.
+     */
+
+    /*
+     * 1.b Add object names and their auth sessions
+     */
+
+    /*
+     * 2. Restore auxiliary sessions
+     */
+
+    /*
+     * 3. Command specific initializations
+     */
+
+    /*
+     * 4. Configuration for calculating the pHash
+     */
+
+    /*
+     * 4.a Determine pHash length and alg
+     */
+
+    /*
+     * 4.b Determine if TPM2_CC_<command> is to be dispatched
+     */
+
+    return tool_rc_success;
+}
+
+static tool_rc check_options(void) {
+
+    if (!ctx.ecc_parameters_path) {
+        LOG_ERR("Invalid path specified for saving the ECC parameters.");
+        return tool_rc_option_error;
+    }
+
+    if (ctx.curve_id == TPM2_ECC_NONE) {
+        LOG_ERR("Invalid/ unspecified ECC curve");
+        return tool_rc_option_error;
+    }
+
+    return tool_rc_success;
+}
 
 static bool on_option(char key, char *value) {
 
@@ -58,57 +145,68 @@ static bool on_args(int argc, char **argv) {
 static bool tpm2_tool_onstart(tpm2_options **opts) {
 
     static struct option topts[] = {
-      { "output",  required_argument, NULL, 'o' },
+      { "output",  required_argument, 0, 'o' },
     };
 
     *opts = tpm2_options_new("o:", ARRAY_LEN(topts), topts,
             on_option, on_args, 0);
 
-    return *opts != NULL;
-}
-
-static tool_rc check_options(void) {
-
-    if (!ctx.ecc_parameters_path) {
-        LOG_ERR("Invalid path specified for saving the ECC parameters.");
-        return tool_rc_option_error;
-    }
-
-    if (ctx.curve_id == TPM2_ECC_NONE) {
-        LOG_ERR("Invalid/ unspecified ECC curve");
-        return tool_rc_option_error;
-    }
-
-    return tool_rc_success;
+    return *opts != 0;
 }
 
 static tool_rc tpm2_tool_onrun(ESYS_CONTEXT *ectx, tpm2_option_flags flags) {
 
     UNUSED(flags);
-    UNUSED(ectx);
 
-    // Check input options and arguments
+    /*
+     * 1. Process options
+     */
     tool_rc rc = check_options();
     if (rc != tool_rc_success) {
         return rc;
     }
 
-    // ESAPI call
-    TPMS_ALGORITHM_DETAIL_ECC *parameters;
-    rc = tpm2_geteccparameters(ectx, ctx.curve_id, &parameters);
+    /*
+     * 2. Process inputs
+     */
+    rc = process_inputs(ectx);
     if (rc != tool_rc_success) {
         return rc;
     }
 
-    // Process outputs
-    bool result = files_save_ecc_details(parameters, ctx.ecc_parameters_path);
-    if (!result) {
-        LOG_ERR("Failed to write out the ECC pub key");
-        return tool_rc_general_error;
+    /*
+     * 3. TPM2_CC_<command> call
+     */
+    rc = geteccparameters(ectx);
+    if (rc != tool_rc_success) {
+        return rc;
     }
+
+    /*
+     * 4. Process outputs
+     */
+    return process_outputs(ectx);
+}
+
+static tool_rc tpm2_tool_onstop(ESYS_CONTEXT *ectx) {
+
+    UNUSED(ectx);
+
+    /*
+     * 1. Free objects
+     */
+
+    /*
+     * 2. Close authorization sessions
+     */
+
+    /*
+     * 3. Close auxiliary sessions
+     */
 
     return tool_rc_success;
 }
 
 // Register this tool with tpm2_tool.c
-TPM2_TOOL_REGISTER("geteccparameters", tpm2_tool_onstart, tpm2_tool_onrun, NULL, NULL)
+TPM2_TOOL_REGISTER("geteccparameters", tpm2_tool_onstart, tpm2_tool_onrun,
+    tpm2_tool_onstop, 0)
