6d32afa83a30e34c1131c2e2d010265bc954671a
cihula <connor.cihula@intel.com>
2022-07-13 14:16:30 -0700

    tools/tpm2_pcrallocate: add support for calculating cpHash
    Signed-off-by: cihula <connor.cihula@intel.com>

--- lib/tpm2.c
+++ lib/tpm2.c
@@ -3840,16 +3840,56 @@ tpm2_nvwrite_skip_esapi_call:
 
 tool_rc tpm2_pcr_allocate(ESYS_CONTEXT *esys_context,
         tpm2_loaded_object *auth_hierarchy_obj,
-        const TPML_PCR_SELECTION *pcr_allocation) {
+        const TPML_PCR_SELECTION *pcr_allocation, TPM2B_DIGEST *cp_hash,
+        TPMI_ALG_HASH parameter_hash_algorithm) {
 
-    TSS2_RC rval;
     TPMI_YES_NO allocation_success;
     UINT32 max_pcr;
     UINT32 size_needed;
     UINT32 size_available;
 
+    TSS2_RC rval = TSS2_RC_SUCCESS;
+    tool_rc rc = tool_rc_success;
+    if (cp_hash && cp_hash->size) {
+        /*
+         * Need sys_context to be able to calculate CpHash
+         */
+        TSS2_SYS_CONTEXT *sys_context = 0;
+        rc = tpm2_getsapicontext(esys_context, &sys_context);
+        if(rc != tool_rc_success) {
+            LOG_ERR("Failed to acquire Tss2_Sys_PCR_Allocate_Prepare SAPI context.");
+            return rc;
+        }
+
+        TPM2_HANDLE h = auth_hierarchy_obj->handle;
+        TSS2_RC rval = Tss2_Sys_PCR_Allocate_Prepare(
+            sys_context, h, pcr_allocation);
+        if (rval != TPM2_RC_SUCCESS) {
+            LOG_PERR(Tss2_Sys_PCR_Allocate_Prepare, rval);
+            return tool_rc_general_error;
+        }
+
+        TPM2B_NAME *name1 = NULL;
+        rc = tpm2_tr_get_name(esys_context, auth_hierarchy_obj->tr_handle,
+            &name1);
+        if (rc != tool_rc_success) {
+            goto tpm2_pcrallocate_free_name1;
+        }
+
+        rc = tpm2_sapi_getcphash(sys_context, name1, NULL, NULL,
+            parameter_hash_algorithm, cp_hash);
+
+tpm2_pcrallocate_free_name1:
+        Esys_Free(name1);
+
+        /*
+         * Exit here without making the ESYS call since we just need the cpHash
+         */
+        goto tpm2_pcrallocate_skip_esapi_call;
+    }
+
     ESYS_TR auth_hierarchy_obj_session_handle = ESYS_TR_NONE;
-    tool_rc rc = tpm2_auth_util_get_shandle(esys_context, ESYS_TR_RH_PLATFORM,
+    rc = tpm2_auth_util_get_shandle(esys_context, ESYS_TR_RH_PLATFORM,
             auth_hierarchy_obj->session, &auth_hierarchy_obj_session_handle);
     if (rc != tool_rc_success) {
         LOG_ERR("Couldn't get shandle for lockout hierarchy");
@@ -3873,7 +3913,8 @@ tool_rc tpm2_pcr_allocate(ESYS_CONTEXT *esys_context,
         return tool_rc_general_error;
     }
 
-    return tool_rc_success;
+tpm2_pcrallocate_skip_esapi_call:
+    return rc;
 }
 
 tool_rc tpm2_sign(ESYS_CONTEXT *esys_context, tpm2_loaded_object *signingkey_obj,
