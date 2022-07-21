2ba612382595fa5d7e1d81ca93e823e12c6af6e7
Imran Desai <imran.desai@intel.com>
2022-07-21 09:42:14 -0700

    test/certify.sh: add test verifying cerifyinfo signature with openssl
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- test/integration/tests/certify.sh
+++ test/integration/tests/certify.sh
@@ -20,11 +20,18 @@ tpm2 clear -Q
 
 tpm2 createprimary -Q -C e -g sha256 -G rsa -c primary.ctx
 
-tpm2 create -Q -g sha256 -G rsa -u certify.pub -r certify.priv -C primary.ctx
+tpm2 create -Q -g sha256 -G rsa:rsassa -u certify.pub -r certify.priv \
+    -C primary.ctx
 
 tpm2 load -Q -C primary.ctx -u certify.pub -r certify.priv -n certify.name \
 -c certify.ctx
 
-tpm2 certify -Q -c primary.ctx -C certify.ctx -g sha256 -o attest.out -s sig.out
+tpm2 certify -Q -c primary.ctx -C certify.ctx -g sha256 -o attest.out \
+    -f plain -s sig.out
+
+# Verify the signatures with openssl
+tpm2 readpublic -Q -c certify.ctx -f pem -o certify.pem
+openssl dgst -verify certify.pem -keyform pem -sha256 \
+    -signature sig.out attest.out
 
 exit 0
