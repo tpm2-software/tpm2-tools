09facdc38380260ff3e5d9bb338a3f18c1d8e503
Daniel P. Berrangé <berrange@redhat.com>
2022-09-29 11:37:33 -0400

    test: track expected YAML output for eventlog
    In addition to checking whether the YAML output is parsable,
    compare it against the expected output, and expected stderr
    warning messages. This will detect any unexpected changes
    in eventlog output format.
    
    If there is ever an intentional change in format, the desired
    data files can be re-created using:
    
      TEST_REGENERATE_OUTPUT=1  make check V=1 TESTS=test/integration/tests/eventlog.sh
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- test/integration/tests/eventlog.sh
+++ test/integration/tests/eventlog.sh
@@ -10,36 +10,91 @@ yaml_validate() {
 }
 
 expect_fail() {
-    $@
+    tpm2 eventlog $@
     if [ $? -eq 0 ]; then
         echo "failing test case passed"
         exit 1;
     fi
 }
+
 expect_pass() {
-    $@ | yaml_validate
+    evlog=$1
+    shift
+
+    base=$(basename $evlog)
+    tpm2 eventlog $@ $evlog 1> $base.out 2> $base.err
+
+    ret=0
+
+    yaml_validate < $base.out
     if [ $? -ne 0 ]; then
-        echo "passing test case failed"
-        exit 1;
+        echo "YAML parsing failed"
+        ret=1
+    fi
+
+    diff $evlog.yaml $base.out
+    if [ $? -ne 0 ]; then
+        echo "YAML output changed"
+
+        if test "$TEST_REGENERATE_OUTPUT" = "1"
+        then
+            cp $base.out $evlog.yaml
+        else
+            ret=1
+        fi
+    fi
+
+    if test -f $evlog.warn
+    then
+        diff $evlog.warn $base.err
+        if [ $? -ne 0 ]; then
+            echo "WARNING output changed"
+
+            if test "$TEST_REGENERATE_OUTPUT" = "1"
+            then
+                cp $base.err $evlog.warn
+            else
+                ret=1
+            fi
+        fi
+    else
+        if test -s $base.err
+        then
+            cat $base.err
+            echo "WARNING output unexpected"
+
+            if test "$TEST_REGENERATE_OUTPUT" = "1"
+            then
+                cp $base.err $evlog.warn
+            else
+                ret=1
+            fi
+        fi
+    fi
+
+    rm $base.out $base.err
+    if test $ret != 0
+    then
+        exit $ret
     fi
 }
 
-expect_fail tpm2 eventlog
-expect_fail tpm2 eventlog foo
-expect_fail tpm2 eventlog foo bar
-expect_fail tpm2 eventlog ${srcdir}/test/integration/fixtures/event-bad.bin
+expect_fail
+expect_fail foo
+expect_fail foo bar
+expect_fail ${srcdir}/test/integration/fixtures/event-bad.bin
 
-expect_pass tpm2 eventlog ${srcdir}/test/integration/fixtures/specid-vendordata.bin
-expect_pass tpm2 eventlog ${srcdir}/test/integration/fixtures/event.bin
-expect_pass tpm2 eventlog ${srcdir}/test/integration/fixtures/event-uefivar.bin
-expect_pass tpm2 eventlog ${srcdir}/test/integration/fixtures/event-uefiaction.bin
-expect_pass tpm2 eventlog ${srcdir}/test/integration/fixtures/event-uefiservices.bin
-expect_pass tpm2 eventlog ${srcdir}/test/integration/fixtures/event-uefi-sha1-log.bin
-expect_pass tpm2 eventlog ${srcdir}/test/integration/fixtures/event-bootorder.bin
-expect_pass tpm2 eventlog ${srcdir}/test/integration/fixtures/event-postcode.bin
+expect_pass ${srcdir}/test/integration/fixtures/specid-vendordata.bin
+expect_pass ${srcdir}/test/integration/fixtures/event.bin
+expect_pass ${srcdir}/test/integration/fixtures/event-uefivar.bin
+expect_pass ${srcdir}/test/integration/fixtures/event-uefiaction.bin
+expect_pass ${srcdir}/test/integration/fixtures/event-uefiservices.bin
+expect_pass ${srcdir}/test/integration/fixtures/event-uefi-sha1-log.bin
+expect_pass ${srcdir}/test/integration/fixtures/event-bootorder.bin
+expect_pass ${srcdir}/test/integration/fixtures/event-postcode.bin
 
 # Make sure that --eventlog-version=2 works on complete TPM2 logs
-expect_pass tpm2 eventlog --eventlog-version=2 ${srcdir}/test/integration/fixtures/event-arch-linux.bin
-expect_pass tpm2 eventlog --eventlog-version=2 ${srcdir}/test/integration/fixtures/event-gce-ubuntu-2104-log.bin
+expect_pass ${srcdir}/test/integration/fixtures/event-arch-linux.bin --eventlog-version=2
+expect_pass ${srcdir}/test/integration/fixtures/event-gce-ubuntu-2104-log.bin --eventlog-version=2
 
 exit $?
