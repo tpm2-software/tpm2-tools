4de2866d340799824b351f0c7e85a377ced26f3d
William Roberts <william.c.roberts@intel.com>
2022-11-17 15:37:22 -0600

    tpm2_getcap: support vendor properties
    Requires tpm2-tss 4.0
    
    Support vendor properties as a vendor agnostic TPM2B buffer. This also
    introduces the -i or --ignore-more option to ignore the moreData byte
    returned from the TPM.
    
    Introduce --ignore-moredata to ignore moreData when it's set to 1 but
    moreData does not follow.
    
    Example output from my NUC:
    ./tools/tpm2 getcap -i --tcti=pcap:device:/dev/tpmrm0 vendor:1
    0000000100000003
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- lib/tpm2_capability.h
+++ lib/tpm2_capability.h
@@ -2,6 +2,7 @@
 
 #ifndef LIB_TPM2_CAPABILITY_H_
 #define LIB_TPM2_CAPABILITY_H_
+#include "config.h"
 
 #include <tss2/tss2_esys.h>
 
@@ -23,6 +24,28 @@
  */
 tool_rc tpm2_capability_get(ESYS_CONTEXT *context, TPM2_CAP capability,
         UINT32 property, UINT32 count, TPMS_CAPABILITY_DATA **capability_data);
+
+/** Same as tpm2_capability_get with some extra options
+ * Invokes GetCapability to retrieve the current value of a capability from the
+ * TPM.
+ * @param context
+ *  Enhanced system api (ESAPI) context
+ * @param capability
+ *  the capability being requested from the TPM
+ * @param property
+ *  property
+ * @param count
+ *  maximum number of values to return
+ * @param ignore_more_data
+ *  Ignore the moreData value and just do one read.
+ * @param capability_data
+ *  capability data structure to populate
+ * @return
+ *  tool_rc indicating status.
+ */
+tool_rc tpm2_capability_get_ex(ESYS_CONTEXT *ectx, TPM2_CAP capability,
+        UINT32 property, UINT32 count, bool ignore_more_data,
+        TPMS_CAPABILITY_DATA **capability_data);
 
 /**
  * Attempts to find a vacant handle in the persistent handle namespace.
