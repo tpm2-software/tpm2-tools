71848e3725968616b8fe41585615426697ec5149
William Roberts <william.c.roberts@intel.com>
2022-11-28 12:20:42 -0600

    tests: fix vendor unit test
    Fix build when variant directory is used.
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- test/unit/vendor_tests.sh
+++ test/unit/vendor_tests.sh
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 set -e
 
-result=$(${abs_srcdir}/tools/tpm2 getcap \
+result=$(${abs_builddir}/tools/tpm2 getcap \
     --tcti='cmd:${abs_srcdir}/test/scripts/echo_tcti.py 8001000000170000000001000001000000000100000003' \
     --ignore-moredata vendor)
 test ${result} == "0000000100000003"
