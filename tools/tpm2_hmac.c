ab13ea10f4892809cdddaedbc420bb2963c88cf0
Bill Roberts <bill.c.roberts@gmail.com>
2023-12-09 13:34:15 -0600

    tpm2_hmac: add yaml support
    Signed-off-by: Bill Roberts <bill.c.roberts@gmail.com>

--- tools/tpm2_hmac.c
+++ tools/tpm2_hmac.c
@@ -377,8 +377,12 @@ static bool tpm2_tool_onstart(tpm2_options **opts) {
 
     ctx.input = stdin;
 
+    /*
+     * Unfortunately this writes to whatever -o is specified and can't be hooked as
+     * normal stdout behavior, leave as is and don't yaml-afy
+     */
     *opts = tpm2_options_new("c:p:o:g:t:", ARRAY_LEN(topts), topts, on_option,
-        on_args, 0);
+        on_args, TPM2_OPTIONS_NO_OUTPUT);
 
     return *opts != 0;
 }
